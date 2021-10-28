171cafbac4c786392ff3466a08bc55b3bf9f6228
Arie Trouw <arie.trouw@xy.compay>
2021-10-28 15:00:39 -0700

    added stats

---
+++ src/archive/payload/stats/get.ts
@@ -0,0 +1,23 @@
+import 'source-map-support/register'
+
+import { assertEx } from '@xyo-network/sdk-xyo-js'
+import lambda from 'aws-lambda'
+
+import { getArchivistPayloadMongoSdk, Result, trapServerError } from '../../../lib'
+
+const getCount = async (archive: string) => {
+  const bwSdk = getArchivistPayloadMongoSdk(archive)
+  return await bwSdk.fetchCount()
+}
+
+export const entryPoint = async (
+  event: lambda.APIGatewayProxyEvent,
+  context: lambda.Context,
+  callback: lambda.APIGatewayProxyCallback
+) => {
+  const archive = assertEx(event.pathParameters?.['archive'], 'Missing archive name')
+  await trapServerError(callback, async () => {
+    const count = await getCount(archive)
+    return Result.Ok(callback, { count })
+  })
+}
