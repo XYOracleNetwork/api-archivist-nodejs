f4a627fd6783d6ca471a22f32b3b0ce5516655e2
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-01-04 14:03:31 -0500

    use mongo envs if present

--- src/lib/getMongoDBValues.ts
+++ src/lib/getMongoDBValues.ts
@@ -2,6 +2,7 @@ import { getEnvFromAws } from '@xylabs/sdk-api-express-ecs'
 
 export const getMongoDBConfig = async () => {
   let env
+
   if (typeof process.env.MONGO_DOMAIN !== undefined) {
     const envsFromAWS = await getEnvFromAws(
       'arn:aws:secretsmanager:us-east-1:434114103920:secret:api-xyo-archivist-aWFucj'
@@ -10,8 +11,12 @@ export const getMongoDBConfig = async () => {
       ...envsFromAWS,
     }
   } else {
-    // @TODO - use real envs from MongoDB
-    throw Error('Please set proper configuration for connecting to MongoDB')
+    env = {
+      MONGO_DATABASE: process.env.MONGO_DATABASE,
+      MONGO_DOMAIN: process.env.MONGO_DOMAIN,
+      MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+      MONGO_USERNAME: process.env.USERNAME,
+    }
   }
 
   return env
