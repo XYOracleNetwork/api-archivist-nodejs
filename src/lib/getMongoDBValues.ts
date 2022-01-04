699cdf3506cb8d39e91c2c9fa7d66673e0c823fa
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-01-04 09:46:13 -0500

    extract function to get mongo config values

---
+++ src/lib/getMongoDBValues.ts
@@ -0,0 +1,18 @@
+import { getEnvFromAws } from '@xylabs/sdk-api-express-ecs'
+
+export const getMongoDBConfig = async () => {
+  let env
+  if (typeof process.env.MONGO_ENVS !== undefined) {
+    const envsFromAWS = await getEnvFromAws(
+      'arn:aws:secretsmanager:us-east-1:434114103920:secret:api-xyo-archivist-aWFucj'
+    )
+    env = {
+      ...envsFromAWS,
+    }
+  } else {
+    // @TODO - use real envs from MongoDB
+    throw Error('Please set proper configuration for connecting to MongoDB')
+  }
+
+  return env
+}
