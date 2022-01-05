04c031bd8df58ca067ef6d30f2b8218a6c058336
Joel Carter <joelbcarter@gmail.com>
2022-01-05 16:03:11 -0600

    Strongly type storage functions

--- src/archive/block/post/storeBoundWitnesses.ts
+++ src/archive/block/post/storeBoundWitnesses.ts
@@ -3,7 +3,7 @@ import { XyoBoundWitness } from '@xyo-network/sdk-xyo-client-js'
 
 import { getArchivistBoundWitnessesMongoSdk } from '../../../lib'
 
-export const storeBoundWitnesses = async (archive: string, boundWitnesses: XyoBoundWitness[]) => {
+export const storeBoundWitnesses = async (archive: string, boundWitnesses: XyoBoundWitness[]): Promise<number> => {
   const sdk = await getArchivistBoundWitnessesMongoSdk(archive)
   const result = await sdk.insertMany(boundWitnesses)
   assertEx(result === boundWitnesses.length, `Boundwitness Storage Failed [${result}/${boundWitnesses.length}]`)
