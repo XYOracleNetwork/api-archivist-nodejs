7f0085ecd8b4a362a0ee726d73a2eb503c93a9e2
Joel Carter <joelbcarter@gmail.com>
2022-01-18 00:29:25 -0600

    Use UUIDv1s to prevent replay attacks

--- src/middleware/auth/routes/wallet/challenge/post.ts
+++ src/middleware/auth/routes/wallet/challenge/post.ts
@@ -2,7 +2,7 @@ import 'source-map-support/register'
 
 import { RequestHandler } from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { v4 } from 'uuid'
+import { v1 } from 'uuid'
 
 export const postWalletChallenge: RequestHandler = (req, res, _next) => {
   const { address } = req.body
@@ -10,9 +10,9 @@ export const postWalletChallenge: RequestHandler = (req, res, _next) => {
     res.sendStatus(StatusCodes.BAD_REQUEST)
     return
   }
-  // TODO: Store these somewhere to verify we actually generated them
-  // to prevent replay attacks. Possibly use time-based UUID with window
-  // validation.
-  const state = v4()
+  // NOTE: So that we don't have to store these somewhere to verify we actually
+  // generated them, we use a UUIDv1 which includes a time we can verify is
+  // within a reasonable window later.
+  const state = v1()
   res.json({ state })
 }
--- src/middleware/auth/strategy/web3Strategy.ts
+++ src/middleware/auth/strategy/web3Strategy.ts
@@ -1,8 +1,43 @@
 import { utils } from 'ethers'
 import { Request } from 'express'
 import passport, { Strategy, StrategyCreated, StrategyCreatedStatic } from 'passport'
+import { validate } from 'uuid'
 
 import { IUserStore, IWeb3User, User } from '../model'
+
+const GREGORIAN_OFFSET = 122192928000000000
+
+const getTimeFromUuid = (uuid_str: string) => {
+  const parts = uuid_str.split('-')
+  const time = [parts[2].substring(1), parts[1], parts[0]].join('')
+  return parseInt(time, 16)
+}
+
+const getDateFromUuid = (uuid_str: string) => {
+  const time = getTimeFromUuid(uuid_str) - GREGORIAN_OFFSET
+  const milliseconds = Math.floor(time / 10000)
+  return new Date(milliseconds)
+}
+
+/**
+ *
+ * @param uuid UUIDv1 string
+ * @returns true if the string was a valid UUIDv1 and generated today
+ */
+const verifyUuid = (uuid: string): boolean => {
+  try {
+    if (validate(uuid)) {
+      const date = getDateFromUuid(uuid)
+      const now = new Date()
+      return (
+        date.getFullYear() === now.getFullYear() && date.getMonth() === now.getMonth() && date.getDay() === now.getDay()
+      )
+    }
+  } catch (error) {
+    return false
+  }
+  return false
+}
 
 const verifyWallet = (message: string, signature: string, address: string) => {
   try {
@@ -30,6 +65,10 @@ class Web3AuthStrategy extends Strategy {
       return
     }
     if (!verifyWallet(message, signature, address)) {
+      this.fail()
+      return
+    }
+    if (!verifyUuid(message)) {
       this.fail()
       return
     }
