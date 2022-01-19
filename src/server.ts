990335524958becb47d8fc7b0ea9b6f3c56f672c
Joel Carter <joelbcarter@gmail.com>
2022-01-19 05:14:58 -0600

    Correct verbiage

--- src/server.ts
+++ src/server.ts
@@ -59,8 +59,8 @@ const server = async (port = 80) => {
   // If an AWS ARN was supplied for Secrets Manager
   const awsEnvSecret = process.env.AWS_ENV_SECRET_ARN
   if (awsEnvSecret) {
-    // Merge the values from there into the current ENV
-    // with AWS taking preference
+    // Merge the values from AWS into the current ENV
+    // with AWS taking precedence
     const awsEnv = await getEnvFromAws(awsEnvSecret)
     Object.assign(process.env, awsEnv)
   }
