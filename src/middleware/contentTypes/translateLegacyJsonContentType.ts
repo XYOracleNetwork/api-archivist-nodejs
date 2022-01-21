39fcd4aad43b830e631b2c3da5bdde73d4253e93
Joel Carter <joelbcarter@gmail.com>
2022-01-20 19:29:19 -0600

    Safe path walk `includes`

--- src/middleware/contentTypes/translateLegacyJsonContentType.ts
+++ src/middleware/contentTypes/translateLegacyJsonContentType.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from 'express'
 
 export const translateLegacyJsonContentTypes: RequestHandler = (req, res, next) => {
   const contentType = req.headers['content-type']
-  if (contentType?.includes('text/json')) {
+  if (contentType?.includes?.('text/json')) {
     req.headers['content-type'] = contentType.replace('text/json', 'application/json')
   }
   next()
