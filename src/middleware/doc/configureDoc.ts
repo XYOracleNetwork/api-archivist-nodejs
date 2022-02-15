86419d0dbbbaf843634464c9bd31888b50347f21
Joel <JoelBCarter@users.noreply.github.com>
2022-02-15 15:07:52 -0600

    Calculate default URI Scheme (#62)

--- src/middleware/doc/configureDoc.ts
+++ src/middleware/doc/configureDoc.ts
@@ -35,7 +35,8 @@ export interface IConfigureDocOptions {
 
 export const configureDoc = async (app: Application, options: IConfigureDocOptions) => {
   app.get(swaggerJsonUrl, (req, res) => res.sendFile(swaggerJsonFile, { root: './' }))
-  const mergedOptions = { ...defaultOptions, ...options }
+  const schemes = options.host.includes('localhost') ? ['http'] : ['https']
+  const mergedOptions = { ...defaultOptions, ...options, schemes }
   await swaggerAutogen()(swaggerJsonFile, endpointsFiles, mergedOptions)
   // eslint-disable-next-line import/no-deprecated
   app.use('/doc', serve, setup(undefined, opts))
