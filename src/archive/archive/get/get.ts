6b9b3d3eeab6f35a8c8d1a9e419b141dbf15575a
JoelBCarter <joelbcarter@gmail.com>
2022-02-18 08:46:27 -0600

    Use archive from locals

--- src/archive/archive/get/get.ts
+++ src/archive/archive/get/get.ts
@@ -1,34 +1,31 @@
-import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
+import { NoReqBody, NoReqQuery } from '@xylabs/sdk-api-express-ecs'
 import { RequestHandler } from 'express'
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 
-import { getArchiveByName, isValidArchiveName } from '../../../lib'
+import { ArchiveLocals } from '../../archiveLocals'
 import { ArchivePathParams } from '../../archivePathParams'
 import { ArchiveResponse } from '../../archiveResponse'
 
-const handler: RequestHandler<ArchivePathParams, ArchiveResponse> = async (req, res, next) => {
+const handler: RequestHandler<ArchivePathParams, ArchiveResponse, NoReqBody, NoReqQuery, ArchiveLocals> = (
+  req,
+  res,
+  next
+) => {
   const { user } = req
   if (!user || !user?.id) {
     next({ message: 'Invalid User', statusCode: StatusCodes.UNAUTHORIZED })
     return
   }
-
-  const archive = req.params.archive?.toLowerCase()
-  if (!isValidArchiveName(archive)) {
-    next({ message: 'Invalid Archive Name', statusCode: StatusCodes.BAD_REQUEST })
-    return
-  }
-
-  const response = await getArchiveByName(archive)
-  if (!response) {
+  const { archive } = res.locals
+  if (!archive) {
     next({ message: ReasonPhrases.NOT_FOUND, statusCode: StatusCodes.NOT_FOUND })
   }
-  if (response?.user === user.id) {
-    res.json(response)
+  if (archive?.user === user.id) {
+    res.json(archive)
     next()
   } else {
     next({ message: ReasonPhrases.FORBIDDEN, statusCode: StatusCodes.FORBIDDEN })
   }
 }
 
-export const getArchive = asyncHandler(handler)
+export const getArchive = handler
