a6473ec0ad4e7c3be6b057570a19da1f376da0f4
JoelBCarter <joelbcarter@gmail.com>
2022-02-19 12:20:46 -0600

    Rough draft of getBoundWitnesses endpoint

--- src/archive/archive/block/get/get.ts
+++ src/archive/archive/block/get/get.ts
@@ -1,40 +1,57 @@
 import { NoReqBody, NoReqQuery } from '@xylabs/sdk-api-express-ecs'
+import { XyoBoundWitness, XyoBoundWitnessWrapper } from '@xyo-network/sdk-xyo-client-js'
 import { RequestHandler } from 'express'
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 
+import { getArchivistBoundWitnessesMongoSdk } from '../../../../lib'
 import { ArchiveLocals } from '../../../archiveLocals'
 import { ArchivePathParams } from '../../../archivePathParams'
-import { ArchiveResponse } from '../../../archiveResponse'
+
+// TODO: Move to SDK lib
+const getBoundWitnesses = async (archive: string, hash: string, limit = 100, orderBy: 'asc' | 'desc' = 'asc') => {
+  const sdk = await getArchivistBoundWitnessesMongoSdk(archive)
+  const sortOrder = orderBy === 'asc' ? 1 : -1
+  return await (
+    await sdk.useCollection((collection) => collection.find().sort({ _id: sortOrder }).limit(limit))
+  ).toArray()
+}
+
+// TODO: Move to shared lib since we use for single hash endpoint too
+const scrubBoundWitnesses = (boundWitnesses: XyoBoundWitness[]) => {
+  return boundWitnesses?.map((boundWitness) => {
+    const bwWrapper = new XyoBoundWitnessWrapper(boundWitness)
+    return bwWrapper.scrubbedFields
+  })
+}
 
 interface GetArchiveBlocksQueryParams extends NoReqQuery {
-  orderBy: 'asc' | 'desc'
   hash: string
-  limit: string
+  limit?: string
+  orderBy?: 'asc' | 'desc'
 }
 
 const handler: RequestHandler<
   ArchivePathParams,
-  ArchiveResponse,
+  Pick<XyoBoundWitness, string>[],
   NoReqBody,
   GetArchiveBlocksQueryParams,
   ArchiveLocals
-> = (req, res, next) => {
-  const { user } = req
-  if (!user || !user?.id) {
-    next({ message: 'Invalid User', statusCode: StatusCodes.UNAUTHORIZED })
-    return
-  }
+> = async (req, res, next) => {
   const { archive } = res.locals
   if (!archive) {
     next({ message: ReasonPhrases.NOT_FOUND, statusCode: StatusCodes.NOT_FOUND })
   }
-  if (archive.user === user.id) {
-    // res.json(archive)
-    // next()
-    next({ message: ReasonPhrases.NOT_IMPLEMENTED, statusCode: StatusCodes.NOT_IMPLEMENTED })
-  } else {
-    next({ message: ReasonPhrases.FORBIDDEN, statusCode: StatusCodes.FORBIDDEN })
-  }
+  // Validate path params
+  const { hash, limit, orderBy } = req.query
+  const parsed = parseInt(limit || '100', 10)
+  const parsedLimit = isNaN(parsed) || parsed > 100 ? 100 : parsed
+  const parsedOrderBy = orderBy === 'asc' ? 'asc' : 'desc'
+
+  // Get boundWitnesses
+  const boundWitnesses = (await getBoundWitnesses(archive.archive, hash, parsedLimit, parsedOrderBy)) ?? []
+  const response = scrubBoundWitnesses(boundWitnesses)
+  res.json(response)
+  next()
 }
 
 export const getArchiveBlocks = handler
