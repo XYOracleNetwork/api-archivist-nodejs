8a517c38e03ba6b350aef2007bf727e84510ccbf
JoelBCarter <joelbcarter@gmail.com>
2022-02-19 21:03:35 -0600

    Update wording

--- src/middleware/standardResponses/standardErrors.spec.ts
+++ src/middleware/standardResponses/standardErrors.spec.ts
@@ -12,7 +12,7 @@ describe('standardErrors', () => {
     await claimArchive(token, archive)
   })
   it('Returns errors in the standard format', async () => {
-    const data = 'no JSON' as unknown as XyoBoundWitness
+    const data = 'definitely not JSON' as unknown as XyoBoundWitness
     const response = await getArchivist().post(`/archive/${archive}/block`).send(data)
     const errors = response?.body?.errors
     expect(errors).toBeTruthy()
