0ee63609b33f630417df81e76249f6b53d1337eb
JoelBCarter <joelbcarter@gmail.com>
2022-02-22 20:10:36 -0600

    Refactor endpoints for parity

--- src/archive/archive/block/get/get.ts
+++ src/archive/archive/block/get/get.ts
@@ -13,28 +13,47 @@ const defaultLimit = 10
 const maxLimit = 100
 
 export interface GetArchiveBlocksQueryParams extends NoReqQuery {
-  hash: string
+  hash?: string
   limit?: string
   order?: SortDirection
+  timestamp?: string
 }
 
 // TODO: Move to SDK lib
 const getBoundWitnesses = async (
   archive: string,
-  hash: string,
+  hash?: string,
+  timestamp?: number,
   limit = defaultLimit,
   sortOrder: SortDirection = 'asc'
-) => {
+): Promise<XyoBoundWitness[] | null> => {
   const sdk = await getArchivistBoundWitnessesMongoSdk(archive)
-  const boundWitnesses = await sdk.findOne({ _hash: hash })
-  if (!boundWitnesses || !boundWitnesses._timestamp) {
+  // If no hash/timestamp was supplied, just return from the start/end of the archive
+  if (!hash && !timestamp) {
+    return sortOrder === 'asc' ? sdk.findAfter(0, limit) : sdk.findBefore(Date.now(), limit)
+  }
+  // If a filter was supplied, find that block
+  const findOneFilter: { _hash?: string; _timestamp?: number } = {}
+  if (hash) findOneFilter._hash = hash
+  if (timestamp) findOneFilter._timestamp = timestamp
+  const boundWitnesses = await sdk.findOne(findOneFilter)
+  if (!boundWitnesses) {
     return null
   }
+
+  // Find all blocks starting from that block
+  const _idPredicate = sortOrder === 'asc' ? { $gte: boundWitnesses._id } : { $lte: boundWitnesses._id }
   const _timestampPredicate =
     sortOrder === 'asc' ? { $gte: boundWitnesses._timestamp } : { $lte: boundWitnesses._timestamp }
   return await (
     await sdk.useCollection((col) =>
-      col.find({ _archive: archive, _timestamp: _timestampPredicate }).sort({ _timestamp: sortOrder }).limit(limit)
+      col
+        // Order of keys really matters here since we're relying on the archive/timestamp
+        // index so we disable eslint from sorting them alphabetically
+        // eslint-disable-next-line sort-keys-fix/sort-keys-fix
+        .find({ _archive: archive, _timestamp: _timestampPredicate, _id: _idPredicate })
+        .sort({ _timestamp: sortOrder })
+        .limit(limit)
     )
   ).toArray()
 }
@@ -50,14 +69,15 @@ const handler: RequestHandler<
   if (!archive) {
     next({ message: ReasonPhrases.NOT_FOUND, statusCode: StatusCodes.NOT_FOUND })
   }
-  const { hash, limit, order: orderBy } = req.query
+  const { hash, limit, order, timestamp } = req.query
   if (!hash) {
     next({ message: ReasonPhrases.BAD_REQUEST, statusCode: StatusCodes.BAD_REQUEST })
   }
   const limitNumber = tryParseInt(limit) ?? 10
+  const timestampNumber = tryParseInt(timestamp)
   assertEx(limitNumber > 0 && limitNumber <= maxLimit, `limit must be between 1 and ${maxLimit}`)
-  const parsedOrderBy = orderBy?.toLowerCase?.() === 'asc' ? 'asc' : 'desc'
-  const boundWitnesses = await getBoundWitnesses(archive.archive, hash, limitNumber, parsedOrderBy)
+  const parsedOrder = order?.toLowerCase?.() === 'asc' ? 'asc' : 'desc'
+  const boundWitnesses = await getBoundWitnesses(archive.archive, hash, timestampNumber, limitNumber, parsedOrder)
   if (boundWitnesses) {
     res.json(scrubBoundWitnesses(boundWitnesses))
     next()
--- src/archive/archive/payload/get/get.ts
+++ src/archive/archive/payload/get/get.ts
@@ -13,22 +13,48 @@ const defaultLimit = 10
 const maxLimit = 100
 
 export interface GetArchivePayloadsQueryParams extends NoReqQuery {
-  hash: string
+  hash?: string
   limit?: string
   order?: SortDirection
+  timestamp?: string
 }
 
 // TODO: Move to SDK lib
-const getPayloads = async (archive: string, hash: string, limit = defaultLimit, sortOrder: SortDirection = 'asc') => {
+const getPayloads = async (
+  archive: string,
+  hash?: string,
+  timestamp?: number,
+  limit = defaultLimit,
+  sortOrder: SortDirection = 'asc'
+) => {
   const sdk = await getArchivistPayloadMongoSdk(archive)
-  const payload = await sdk.findOne({ _hash: hash })
-  if (!payload || !payload._timestamp) {
+  // If no hash/timestamp was supplied, just return from the start/end of the archive
+  if (!hash && !timestamp) {
+    throw new Error('Not implemented')
+    // TODO: Implement these methods in the sdk
+    // return sortOrder === 'asc' ? sdk.findAfter(0, limit) : sdk.findBefore(Date.now(), limit)
+  }
+  // If a filter was supplied, find that block
+  const findOneFilter: { _hash?: string; _timestamp?: number } = {}
+  if (hash) findOneFilter._hash = hash
+  if (timestamp) findOneFilter._timestamp = timestamp
+  const payloads = await sdk.findOne(findOneFilter)
+  if (!payloads) {
     return null
   }
-  const _timestampPredicate = sortOrder === 'asc' ? { $gte: payload._timestamp } : { $lte: payload._timestamp }
+
+  // Find all blocks starting from that block
+  const _idPredicate = sortOrder === 'asc' ? { $gte: payloads._id } : { $lte: payloads._id }
+  const _timestampPredicate = sortOrder === 'asc' ? { $gte: payloads._timestamp } : { $lte: payloads._timestamp }
   return await (
     await sdk.useCollection((col) =>
-      col.find({ _archive: archive, _timestamp: _timestampPredicate }).sort({ _timestamp: sortOrder }).limit(limit)
+      col
+        // Order of keys really matters here since we're relying on the archive/timestamp
+        // index so we disable eslint from sorting them alphabetically
+        // eslint-disable-next-line sort-keys-fix/sort-keys-fix
+        .find({ _archive: archive, _timestamp: _timestampPredicate, _id: _idPredicate })
+        .sort({ _timestamp: sortOrder })
+        .limit(limit)
     )
   ).toArray()
 }
@@ -44,14 +70,15 @@ const handler: RequestHandler<
   if (!archive) {
     next({ message: ReasonPhrases.NOT_FOUND, statusCode: StatusCodes.NOT_FOUND })
   }
-  const { hash, limit, order: orderBy } = req.query
+  const { hash, limit, order, timestamp } = req.query
   if (!hash) {
     next({ message: ReasonPhrases.BAD_REQUEST, statusCode: StatusCodes.BAD_REQUEST })
   }
   const limitNumber = tryParseInt(limit) ?? 10
+  const timestampNumber = tryParseInt(timestamp)
   assertEx(limitNumber > 0 && limitNumber <= maxLimit, `limit must be between 1 and ${maxLimit}`)
-  const parsedOrderBy = orderBy?.toLowerCase?.() === 'asc' ? 'asc' : 'desc'
-  const payloads = await getPayloads(archive.archive, hash, limitNumber, parsedOrderBy)
+  const parsedOrder = order?.toLowerCase?.() === 'asc' ? 'asc' : 'desc'
+  const payloads = await getPayloads(archive.archive, hash, timestampNumber, limitNumber, parsedOrder)
   if (payloads) {
     res.json(payloads)
     next()
