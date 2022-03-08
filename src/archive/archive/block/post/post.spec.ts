ea7d5c6c0689fd9081c9cacaa46c11330cd829d2
JoelBCarter <joelbcarter@gmail.com>
2022-03-08 08:59:47 -0600

    update test wording

--- src/archive/archive/block/post/post.spec.ts
+++ src/archive/archive/block/post/post.spec.ts
@@ -11,50 +11,56 @@ import {
 } from '../../../../test'
 
 describe('/archive/:archive/block', () => {
-  it('Allows posting blocks to existing archives', async () => {
-    const token = await getTokenForNewUser()
-    const archive = (await claimArchive(token)).archive
-    await postBlock(getNewBlock(), archive)
-  })
-  it('Allows posting blocks to non-existing archives', async () => {
-    await postBlock(getNewBlock(), getArchiveName())
-  })
-  it('Allows posting block with payload', async () => {
-    const response = await postBlock(getNewBlockWithPayloads(1), getArchiveName())
-    expect(response.boundWitnesses).toEqual(1)
-    expect(response.payloads).toEqual(1)
-  })
-  it('Allows posting block with multiple payload', async () => {
-    const response = await postBlock(getNewBlockWithPayloads(2), getArchiveName())
-    expect(response.boundWitnesses).toEqual(1)
-    expect(response.payloads).toEqual(2)
-  })
-  it('Allows posting block without payloads', async () => {
-    const response = await postBlock(getNewBlock(), getArchiveName())
-    expect(response.boundWitnesses).toEqual(1)
-    expect(response.payloads).toEqual(0)
-  })
-  it('Allows posting block with multiple bound witnesses', async () => {
-    const response = await postBlock(getNewBlockWithBoundWitnesses(2), getArchiveName())
-    expect(response.boundWitnesses).toEqual(2)
-    expect(response.payloads).toEqual(0)
-  })
-  it('Allows posting block with multiple bound witnesses with payloads', async () => {
-    const response = await postBlock(getNewBlockWithBoundWitnessesWithPayloads(2), getArchiveName())
-    expect(response.boundWitnesses).toEqual(2)
-    expect(response.payloads).toEqual(2)
-  })
-  it('Allows posting block with multiple bound witnesses with multiple payloads', async () => {
-    const response = await postBlock(getNewBlockWithBoundWitnessesWithPayloads(2, 2), getArchiveName())
-    expect(response.boundWitnesses).toEqual(2)
-    expect(response.payloads).toEqual(4)
-  })
-  it('Allows posting multiple bound witnesses some with payloads and some without', async () => {
-    const boundWitness1 = getNewBlock(...getPayloads(2))
-    const boundWitness2 = getNewBlock()
-    const block = [boundWitness1, boundWitness2]
-    const response = await postBlock(block, getArchiveName())
-    expect(response.boundWitnesses).toEqual(2)
-    expect(response.payloads).toEqual(2)
+  describe('allows posting block', () => {
+    let token = ''
+    let archive = ''
+    beforeAll(async () => {
+      token = await getTokenForNewUser()
+      archive = (await claimArchive(token)).archive
+    })
+    it('to existing archive', async () => {
+      await postBlock(getNewBlock(), archive)
+    })
+    it('to non-existing archives', async () => {
+      await postBlock(getNewBlock(), getArchiveName())
+    })
+    it('with single payload', async () => {
+      const response = await postBlock(getNewBlockWithPayloads(1), archive)
+      expect(response.boundWitnesses).toEqual(1)
+      expect(response.payloads).toEqual(1)
+    })
+    it('with multiple payloads', async () => {
+      const response = await postBlock(getNewBlockWithPayloads(2), archive)
+      expect(response.boundWitnesses).toEqual(1)
+      expect(response.payloads).toEqual(2)
+    })
+    it('without payloads', async () => {
+      const response = await postBlock(getNewBlock(), archive)
+      expect(response.boundWitnesses).toEqual(1)
+      expect(response.payloads).toEqual(0)
+    })
+    it('with multiple bound witnesses', async () => {
+      const response = await postBlock(getNewBlockWithBoundWitnesses(2), archive)
+      expect(response.boundWitnesses).toEqual(2)
+      expect(response.payloads).toEqual(0)
+    })
+    it('with multiple bound witnesses with payloads', async () => {
+      const response = await postBlock(getNewBlockWithBoundWitnessesWithPayloads(2), archive)
+      expect(response.boundWitnesses).toEqual(2)
+      expect(response.payloads).toEqual(2)
+    })
+    it('with multiple bound witnesses with multiple payloads', async () => {
+      const response = await postBlock(getNewBlockWithBoundWitnessesWithPayloads(2, 2), archive)
+      expect(response.boundWitnesses).toEqual(2)
+      expect(response.payloads).toEqual(4)
+    })
+    it('with multiple bound witnesses some with payloads and some without', async () => {
+      const boundWitness1 = getNewBlock(...getPayloads(2))
+      const boundWitness2 = getNewBlock()
+      const block = [boundWitness1, boundWitness2]
+      const response = await postBlock(block, archive)
+      expect(response.boundWitnesses).toEqual(2)
+      expect(response.payloads).toEqual(2)
+    })
   })
 })
