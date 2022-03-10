14d6d2dcbd2b45c465dec022c7b7fa78bd1f807f
JoelBCarter <joelbcarter@gmail.com>
2022-03-09 22:52:48 -0600

    Ensure only distinct archives are returned

--- src/archive/get/get.ts
+++ src/archive/get/get.ts
@@ -5,13 +5,19 @@ import { getArchivesByOwner } from '../../lib'
 import { ArchiveResponse } from '../archiveResponse'
 import { defaultPublicArchives } from './DefaultPublicArchives'
 
+const getArchivesDistinctByName = (archives: ArchiveResponse[]): ArchiveResponse[] => {
+  return archives.reduce((uniques, item) => {
+    return uniques.find((existing) => existing.archive === item.archive) ? uniques : [...uniques, item]
+  }, [] as ArchiveResponse[])
+}
+
 const handler: RequestHandler<NoReqParams, ArchiveResponse[]> = async (req, res, next) => {
   const id = req?.user?.id
   if (!id) {
     res.json(defaultPublicArchives)
   } else {
     const userArchives = await getArchivesByOwner(id)
-    res.json([...defaultPublicArchives, ...userArchives])
+    res.json(getArchivesDistinctByName([...defaultPublicArchives, ...userArchives]))
   }
   next()
 }
