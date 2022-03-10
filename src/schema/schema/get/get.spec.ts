566aacb92ccce1af4111acab8505dbc712f94cd0
Joel <JoelBCarter@users.noreply.github.com>
2022-03-10 07:11:48 -0600

    Update Schema Routes (#87)
    * Use async response wrapper for schema route
    
    * Add test helper methods for schema

--- src/schema/schema/get/get.spec.ts
+++ src/schema/schema/get/get.spec.ts
@@ -1,31 +1,26 @@
-import { XyoBoundWitnessBuilder, XyoPayload } from '@xyo-network/sdk-xyo-client-js'
+import { XyoBoundWitnessBuilder } from '@xyo-network/sdk-xyo-client-js'
 
-import { claimArchive, getArchivist, getTokenForNewUser } from '../../../test'
+import { claimArchive, getHash, getSchema, getTokenForNewUser, postBlock } from '../../../test'
 
 describe('/schema', () => {
   const schema = 'network.xyo.schema'
-  const bwBuilder = new XyoBoundWitnessBuilder({ inlinePayloads: true })
-  const bw = bwBuilder
-    .payload({
-      definition: { $schema: 'http://json-schema.org/draft-07/schema#' },
-      schema,
-    })
-    .build()
-  beforeEach(async () => {
+  const definition = { $schema: 'http://json-schema.org/draft-07/schema#' }
+  const bw = new XyoBoundWitnessBuilder({ inlinePayloads: true }).payload({ definition, schema }).build()
+  beforeAll(async () => {
     const token = await getTokenForNewUser()
     const archive = (await claimArchive(token)).archive
-    await getArchivist()
-      .post(`/archive/${archive}/block`)
-      .send({ boundWitnesses: [bw] })
+    await postBlock(bw, archive)
+    const payloadHash = bw.payload_hashes[0]
+    expect(payloadHash).toBeTruthy()
+    const response = await getHash(payloadHash)
+    expect(response).toBeTruthy()
+    expect(response.schema).toEqual(schema)
   })
-  it('Check if payload wrote', async () => {
-    const response = await getArchivist().get('/18f97b3e85f5bede65e7c0a85d74aee896de58ead8bc4b1b3d7300646c653057')
-    const payload = (response.body as { data: XyoPayload }).data
-    expect(payload.schema).toEqual(schema)
-  })
+
   it('Gets information about the schema', async () => {
-    const response = await getArchivist().get(`/schema/${schema}`)
-    const payload = (response.body as { data: XyoPayload }).data
-    expect(payload.schema).toEqual(schema)
+    const response = await getSchema(schema)
+    expect(response).toBeTruthy()
+    expect(response.schema).toEqual(schema)
+    expect(response.definition).toEqual(definition)
   })
 })
