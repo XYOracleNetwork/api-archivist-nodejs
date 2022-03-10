ad9a182427ca8662c00c4af1b8555216bf1cdd22
JoelBCarter <joelbcarter@gmail.com>
2022-03-09 18:57:46 -0600

    Use payload specific method

--- src/schema/schema/SchemaCache.ts
+++ src/schema/schema/SchemaCache.ts
@@ -1,7 +1,7 @@
 import { Huri, XyoDomainConfigWrapper, XyoPayload } from '@xyo-network/sdk-xyo-client-js'
 import LRU from 'lru-cache'
 
-import { findOneByHash } from '../../lib'
+import { findOnePayloadByHash } from '../../lib'
 
 export class SchemaCache {
   private cache = new LRU<string, XyoPayload | null>({ max: 500, ttl: 1000 * 60 * 5 })
@@ -17,7 +17,7 @@ export class SchemaCache {
       const schemaHuri = (await config.discover(schema))?.schema?.[schema]
       if (schemaHuri) {
         const huri = new Huri(schemaHuri)
-        const payload = (await findOneByHash(huri.hash)) as XyoPayload
+        const payload = await findOnePayloadByHash(huri.hash)
         this.cache.set(schema, payload ?? null)
       }
     }
