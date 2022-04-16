8980cb38c76a3ee71f4417d95e100ec198938fa3
Arie Trouw <arie.trouw@xylabs.com>
2022-04-15 17:16:34 -0700

    deps

--- src/schema/schema/get/get.spec.ts
+++ src/schema/schema/get/get.spec.ts
@@ -1,11 +1,11 @@
-import { XyoAddress, XyoBoundWitnessBuilder } from '@xyo-network/sdk-xyo-client-js'
+import { XyoBoundWitnessBuilder, XyoWallet } from '@xyo-network/sdk-xyo-client-js'
 
 import { claimArchive, getHash, getSchema, getTokenForNewUser, postBlock } from '../../../test'
 
 describe('/schema', () => {
   const schema = 'network.xyo.schema'
   const definition = { $schema: 'http://json-schema.org/draft-07/schema#' }
-  const bw = new XyoBoundWitnessBuilder({ inlinePayloads: true }).payload({ definition, schema }).witness(XyoAddress.random()).build()
+  const bw = new XyoBoundWitnessBuilder({ inlinePayloads: true }).payload({ definition, schema }).witness(XyoWallet.random()).build()
   beforeAll(async () => {
     const token = await getTokenForNewUser()
     const archive = (await claimArchive(token)).archive
