027bb8f72c42554a36e8ac1fab0df771b25625e6
Arie Trouw <arie.trouw@xylabs.com>
2022-04-20 19:22:13 -0700

    Factoring out validation (#100)
    Co-authored-by: Arie Trouw <arie.trouw@xylabs.com>
    
    Co-authored-by: Joel Carter <JoelBCarter@users.noreply.github.com>

--- src/lib/getArchiveByName.ts
+++ src/lib/getArchiveByName.ts
@@ -3,7 +3,7 @@ import { XyoArchive } from '@xyo-network/sdk-xyo-client-js'
 import { getArchivistArchiveMongoSdk } from './dbSdk'
 import { determineArchiveAccessControl } from './determineArchiveAccessControl'
 
-export const getArchiveByName = async (name: string): Promise<XyoArchive | null> => {
+export const getArchiveByName = async (name: string | undefined): Promise<XyoArchive | null> => {
   if (!name) {
     return null
   }
