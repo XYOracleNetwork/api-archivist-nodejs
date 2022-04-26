08aa2fc4a3ed37fd61fbd5cc948345c1d661f4ee
Arie Trouw <arie.trouw@xylabs.com>
2022-04-26 11:43:00 -0700

    domain config fixes

--- src/domain/get/get.ts
+++ src/domain/get/get.ts
@@ -1,6 +1,7 @@
 import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
 import { XyoDomainConfig, XyoDomainConfigWrapper } from '@xyo-network/sdk-xyo-client-js'
 import { RequestHandler } from 'express'
+import { StatusCodes } from 'http-status-codes'
 
 export type DomainPathParams = {
   domain: string
@@ -11,8 +12,10 @@ const handler: RequestHandler<DomainPathParams, XyoDomainConfig> = async (req, r
   const config = await XyoDomainConfigWrapper.discover(domain)
   if (config) {
     res.json(config.payload)
+    next()
+  } else {
+    next({ message: 'Config not found', statusCode: StatusCodes.NOT_FOUND })
   }
-  next()
 }
 
 export const getDomain = asyncHandler(handler)
