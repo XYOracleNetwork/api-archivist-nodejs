9208ffe81d8e153ee8d1fc03f81a4ca842177a23
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-04-26 18:49:58 -0500

    Default environment to 'development'

--- src/middleware/rollbar/rollbar.ts
+++ src/middleware/rollbar/rollbar.ts
@@ -1,11 +1,14 @@
 import { NextFunction } from 'express'
 import Rollbar, { ExpressErrorHandler } from 'rollbar'
 
+const defaultEnvironment = 'development'
+
 const noOpErrorHandler: ExpressErrorHandler = (err, _req, _res, next: NextFunction) => {
   next(err)
 }
 
 export const rollbarErrorHandler = (): ExpressErrorHandler => {
   const accessToken = process.env.ROLLBAR_ACCESS_TOKEN
-  return accessToken ? new Rollbar({ accessToken }).errorHandler : noOpErrorHandler
+  const environment = process.env.ROLLBAR_ENVIRONMENT || defaultEnvironment
+  return accessToken ? new Rollbar({ accessToken, environment }).errorHandler : noOpErrorHandler
 }
