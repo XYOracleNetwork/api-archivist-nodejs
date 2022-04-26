bc22caf6b293f0f75d4240d65278b2796a2d8c53
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-04-26 12:38:01 -0500

    Add test assertions

--- src/archive/archive/schema/recent/get.spec.ts
+++ src/archive/archive/schema/recent/get.spec.ts
@@ -5,17 +5,27 @@ import { claimArchive, getArchiveSchemaRecent, getTokenForNewUser, postBlock } f
 describe('/archive/:archive/schema/recent', () => {
   const schema = 'network.xyo.schema'
   const definition = { $schema: 'http://json-schema.org/draft-07/schema#' }
+  const schemaToAdd = 5
   let token: string
   let archive: string
   beforeAll(async () => {
     token = await getTokenForNewUser()
     archive = (await claimArchive(token)).archive
-    const bw = new XyoBoundWitnessBuilder({ inlinePayloads: true }).payload({ definition, schema }).witness(XyoAccount.random()).build()
-    const response = await postBlock(bw, archive)
-    expect(response).toBeTruthy()
+    for (let i = 0; i < schemaToAdd; i++) {
+      const bw = new XyoBoundWitnessBuilder({ inlinePayloads: true }).payload({ definition, schema }).witness(XyoAccount.random()).build()
+      const response = await postBlock(bw, archive)
+      expect(response).toBeTruthy()
+    }
   })
   it('Gets recently uploaded schema for the archive', async () => {
     const response = await getArchiveSchemaRecent(archive)
     expect(response).toBeTruthy()
+    expect(Array.isArray(response)).toBeTruthy()
+    expect(response.length).toBe(schemaToAdd)
+    response.forEach((payload) => {
+      expect(payload._archive).toBe(archive)
+      expect(payload.schema).toBe(schema)
+      expect(payload.definition).toStrictEqual(definition)
+    })
   })
 })
