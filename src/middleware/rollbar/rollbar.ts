ccd9cbd99fad1636d41a594569716709f11c90fd
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-04-26 18:57:42 -0500

    Add rollbar error handler

--- src/middleware/rollbar/rollbar.ts
+++ src/middleware/rollbar/rollbar.ts
@@ -9,6 +9,11 @@ const noOpErrorHandler: ExpressErrorHandler = (err, _req, _res, next: NextFuncti
 
 export const rollbarErrorHandler = (): ExpressErrorHandler => {
   const accessToken = process.env.ROLLBAR_ACCESS_TOKEN
-  const environment = process.env.ROLLBAR_ENVIRONMENT || defaultEnvironment
-  return accessToken ? new Rollbar({ accessToken, environment }).errorHandler : noOpErrorHandler
+  if (accessToken) {
+    const environment = process.env.ROLLBAR_ENVIRONMENT || defaultEnvironment
+    console.log(`Configuring Rollbar for Environment: ${environment}`)
+    return new Rollbar({ accessToken, environment }).errorHandler
+  } else {
+    return noOpErrorHandler
+  }
 }
--- src/server/addErrorHandlers.ts
+++ src/server/addErrorHandlers.ts
@@ -1,6 +1,9 @@
 import { standardErrors } from '@xylabs/sdk-api-express-ecs'
 import { Express } from 'express'
 
+import { rollbarErrorHandler } from '../middleware'
+
 export const addErrorHandlers = (app: Express) => {
+  app.use(rollbarErrorHandler())
   app.use(standardErrors)
 }
