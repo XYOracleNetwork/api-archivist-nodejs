1037108f299338d2e06a690128cd3b43663a5c3e
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-04-28 09:06:51 -0500

    Validate body with AJV

--- src/debug/post/post.ts
+++ src/debug/post/post.ts
@@ -1,5 +1,22 @@
 import { RequestHandler } from 'express'
+import { AllowedSchema, Validator } from 'express-json-validator-middleware'
 
-export const postDebug: RequestHandler = (req, res, _next) => {
-  res.json({})
+const requestSchema: AllowedSchema = {
+  properties: {
+    street: {
+      type: 'string',
+    },
+  },
+  required: ['street'],
+  type: 'object',
 }
+
+const { validate } = new Validator({})
+
+const requestValidator: RequestHandler = validate({ body: requestSchema })
+
+const handler: RequestHandler = (req, res, _next) => {
+  res.json({ valid: true })
+}
+
+export const postDebug: RequestHandler[] = [requestValidator, handler]
