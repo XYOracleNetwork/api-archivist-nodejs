73c0fac079786b794d4ac56ba2fe50af11b254a5
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-05-05 09:39:57 -0500

    Use the schema cache payload hash as the AJV cache key

--- src/archive/archive/block/post/getPayloadValidatorFromSchemaCache.ts
+++ src/archive/archive/block/post/getPayloadValidatorFromSchemaCache.ts
@@ -1,4 +1,4 @@
-import { XyoPayload, XyoSchemaCache } from '@xyo-network/sdk-xyo-client-js'
+import { XyoPayload, XyoPayloadWrapper, XyoSchemaCache } from '@xyo-network/sdk-xyo-client-js'
 // eslint-disable-next-line import/no-named-as-default
 import Ajv from 'ajv'
 
@@ -7,14 +7,20 @@ import { GetValidator } from './GetValidator'
 const ajv = new Ajv()
 
 export const getPayloadValidatorFromSchemaCache: GetValidator<XyoPayload> = async (payload) => {
-  // TODO: Ensure the ajv cached version reflects the schema cached version?
-  const validate = ajv.getSchema(payload.schema)
-  if (!validate) {
-    const schema = await XyoSchemaCache.instance.get(payload.schema)
-    if (!schema) {
-      return undefined
-    }
-    ajv.addSchema(schema, payload.schema)
-    return ajv.getSchema(payload.schema)
-  }
+  // Get the schema from the schema cache
+  const schema = await XyoSchemaCache.instance.get(payload.schema)
+  // If it doesn't exist return undefined
+  if (!schema) return undefined
+  const { definition, _hash } = schema.payload
+  // Use the schema cache payload hash as the AJV cache key to memoize
+  // the AJV validator
+  const key = _hash || new XyoPayloadWrapper(schema.payload).sortedHash()
+  // Check if we already cached the validator
+  const validate = ajv.getSchema(key)
+  // Return the cached validator for this schema
+  if (validate) return validate
+  // Otherwise, compile it now
+  ajv.addSchema(definition, key)
+  // and return it
+  return ajv.getSchema(key)
 }
