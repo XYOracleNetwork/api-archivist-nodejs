ad4b7614148700ebe2497f840d580f0006e9af48
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-05-06 15:51:44 -0500

    Remove AJV error-handler middleware

--- src/debug/post/post.ts
+++ src/debug/post/post.ts
@@ -1,22 +1,7 @@
 import { RequestHandler } from 'express'
-import { AllowedSchema, Validator } from 'express-json-validator-middleware'
-
-const requestBodySchema: AllowedSchema = {
-  properties: {
-    test: {
-      type: 'string',
-    },
-  },
-  required: ['test'],
-  type: 'object',
-}
-
-const { validate } = new Validator({})
-
-const requestValidator: RequestHandler = validate({ body: requestBodySchema })
 
 const handler: RequestHandler = (_req, res, _next) => {
   res.json({ valid: true })
 }
 
-export const postDebug: RequestHandler[] = [requestValidator, handler]
+export const postDebug: RequestHandler[] = [handler]
--- src/middleware/jsonSchema/isErrorObject.ts
+++
@@ -1,5 +0,0 @@
-import { ErrorObject } from 'ajv'
-
-export const isErrorObject = (item: ErrorObject | undefined): item is ErrorObject => {
-  return !!item
-}
