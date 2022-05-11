ffcd9e5a09804ab25ef5736ecf83c36f49b5d984
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-05-11 17:55:38 -0500

    Witness insertion

--- src/lib/repository/mongodbPayloadRepository.ts
+++ src/lib/repository/mongodbPayloadRepository.ts
@@ -1,27 +1,29 @@
-import { XyoPayload, XyoPayloadBody } from '@xyo-network/sdk-xyo-client-js'
+import { XyoAccount, XyoBoundWitness, XyoBoundWitnessBuilder, XyoBoundWitnessBuilderConfig, XyoPayload, XyoPayloadBody } from '@xyo-network/sdk-xyo-client-js'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { Filter } from 'mongodb'
 
 import { PayloadRepository } from '../../model'
-import { getArchivistAllPayloadMongoSdk } from '../dbSdk'
+import { getArchivistAllBoundWitnessesMongoSdk, getArchivistAllPayloadMongoSdk } from '../dbSdk'
 
 export class MongoDBPayloadRepository implements PayloadRepository<XyoPayload, Filter<XyoPayload>> {
   constructor(
-    private readonly sdk: BaseMongoSdk<XyoPayload> = getArchivistAllPayloadMongoSdk() /*
+    private readonly payloadsSdk: BaseMongoSdk<XyoPayload> = getArchivistAllPayloadMongoSdk(),
+    private readonly boundWitnessSdk: BaseMongoSdk<XyoBoundWitness> = getArchivistAllBoundWitnessesMongoSdk(),
     private readonly account: XyoAccount = XyoAccount.random(),
     private readonly config: XyoBoundWitnessBuilderConfig = { inlinePayloads: false }
-    */
   ) {}
   async find(filter: Filter<XyoPayload>): Promise<XyoPayload[]> {
-    return (await this.sdk.find(filter)).toArray()
+    return (await this.payloadsSdk.find(filter)).toArray()
   }
   async get(huri: string): Promise<XyoPayload[]> {
-    return (await this.sdk.find({ _hash: huri })).toArray()
+    return (await this.payloadsSdk.find({ _hash: huri })).toArray()
   }
   async insert(payloads: XyoPayloadBody[]): Promise<XyoPayload[]> {
-    // TODO: Witness before insert?
+    const bw = new XyoBoundWitnessBuilder(this.config).witness(this.account).payloads(payloads).build()
+    const bwResult = await this.boundWitnessSdk.insertOne(bw)
+    if (bwResult.acknowledged && bwResult.insertedId) throw new Error('Error inserting payloads')
     // TODO: Validate payloads before insert?
-    const result = await this.sdk.insertMany(payloads)
+    const result = await this.payloadsSdk.insertMany(payloads)
     if (result.insertedCount != payloads.length) throw new Error('Error inserting payloads')
     return payloads
   }
