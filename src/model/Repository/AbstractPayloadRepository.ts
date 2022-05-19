e128433fcc421d7677ef49658492c6f9ac6bc785
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-05-19 15:35:33 -0500

    Add query types to interface

--- src/model/Repository/AbstractPayloadRepository.ts
+++ src/model/Repository/AbstractPayloadRepository.ts
@@ -1,8 +1,18 @@
-import { Repository } from './Repository'
-import { XyoStoredPayload } from './XyoStoredPayload'
+import { XyoPayload, XyoPayloadBody } from '@xyo-network/sdk-xyo-client-js'
 
-export abstract class AbstractPayloadRepository<TInsert, TResponse extends XyoStoredPayload<TInsert>, TId, TQuery> implements Repository<TInsert[], TResponse[], TId, TQuery> {
-  abstract find(filter: TQuery): Promise<TResponse[]>
-  abstract get(id: TId): Promise<TResponse[]>
-  abstract insert(items: TInsert[]): Promise<TResponse[]>
+import { PayloadRepository } from './PayloadRepository'
+
+// export abstract class AbstractPayloadRepository<TInsert, TResponse extends XyoStoredPayload<TInsert>, TId, TQuery> implements PayloadRepository<> {
+export abstract class AbstractPayloadRepository<
+  TWriteResponse extends XyoPayload,
+  TWrite extends XyoPayloadBody,
+  TReadResponse = TWriteResponse,
+  TId = string,
+  TQueryResponse = unknown,
+  TQuery = unknown
+> implements PayloadRepository<TWriteResponse, TWrite, TReadResponse, TId, TQueryResponse, TQuery>
+{
+  abstract find(filter: TQuery): Promise<TQueryResponse>
+  abstract get(id: TId): Promise<TReadResponse[]>
+  abstract insert(items: TWrite[]): Promise<TWriteResponse[]>
 }
--- src/model/Repository/PayloadRepository.ts
+++ src/model/Repository/PayloadRepository.ts
@@ -2,4 +2,11 @@ import { XyoPayload, XyoPayloadBody } from '@xyo-network/sdk-xyo-client-js'
 
 import { Repository } from './Repository'
 
-export type PayloadRepository<TInsert extends XyoPayloadBody, TResponse extends XyoPayload, TId = string, TQuery = unknown> = Repository<TInsert[], TResponse[], TId, TQuery>
+export type PayloadRepository<
+  TWriteResponse extends XyoPayload,
+  TWrite extends XyoPayloadBody,
+  TReadResponse = TWriteResponse,
+  TId = string,
+  TQueryResponse = unknown,
+  TQuery = unknown
+> = Repository<TWriteResponse[], TWrite[], TReadResponse[], TId, TQueryResponse, TQuery>
