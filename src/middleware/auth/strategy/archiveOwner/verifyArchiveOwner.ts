18a760ef65679583017b2e2547282d54ec942555
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-05-19 15:03:17 -0500

    Use repository for verifying archive access

--- src/middleware/auth/strategy/archiveOwner/verifyArchiveOwner.ts
+++ src/middleware/auth/strategy/archiveOwner/verifyArchiveOwner.ts
@@ -1,11 +1,9 @@
 import { Request } from 'express'
 
-import { getArchivesByOwner } from '../../../../lib'
-
 export const verifyArchiveOwner = async (req: Request): Promise<boolean> => {
   // Validate user from request
   const { user } = req
-  if (!user || !user?.id) {
+  if (!user || !user.id) {
     return false
   }
   // Validate archive from request
@@ -13,8 +11,7 @@ export const verifyArchiveOwner = async (req: Request): Promise<boolean> => {
   if (!archive) {
     return false
   }
-  // Get archives owned by the user
-  const userArchives = await getArchivesByOwner(user.id)
-  // Verify user owns archive from path
-  return userArchives.some((userArchive) => userArchive.archive === archive)
+  const existingArchive = await req.app.archiveRepository.get(archive)
+  // Validate user is archive owner
+  return !!existingArchive && existingArchive.user === user.id
 }
