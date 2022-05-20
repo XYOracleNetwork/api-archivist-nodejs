88036cbb373e501e338ea94cac31202cc887bad2
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-05-19 19:45:06 -0500

    Add find to archive repository

--- src/middleware/Diviner/Archive/ArchiveRepository.ts
+++ src/middleware/Diviner/Archive/ArchiveRepository.ts
@@ -1,6 +1,8 @@
 import { XyoArchive } from '@xyo-network/sdk-xyo-client-js'
 
 import { UpsertResult } from '../../../lib'
-import { ReadWriteRepository } from '../../../model'
+import { Repository } from '../../../model'
 
-export type ArchiveRepository = ReadWriteRepository<Required<XyoArchive> & UpsertResult, Required<XyoArchive>, Required<XyoArchive> | null, string>
+export type EntityArchive = Required<XyoArchive>
+
+export type ArchiveRepository = Repository<EntityArchive & UpsertResult, EntityArchive, EntityArchive | null, string>
