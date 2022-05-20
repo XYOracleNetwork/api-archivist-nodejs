31dd338c7af8e618b659330c36c9727116ac4fc6
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-05-20 13:23:10 -0500

    Simplify ctor

--- src/lib/Repository/AbstractMongoDBPayloadRepository.ts
+++ src/lib/Repository/AbstractMongoDBPayloadRepository.ts
@@ -5,13 +5,33 @@ import { Filter } from 'mongodb'
 import { AbstractPayloadRepository } from '../../model'
 import { getArchivistAllBoundWitnessesMongoSdk, getArchivistAllPayloadMongoSdk } from '../dbSdk'
 
+export interface AbstractMongoDBPayloadRepositoryOpts {
+  account: XyoAccount
+  boundWitnessSdk: BaseMongoSdk<XyoBoundWitness>
+  config: XyoBoundWitnessBuilderConfig
+  payloadsSdk: BaseMongoSdk<XyoPayload>
+}
+
+export const getDefaultAbstractMongoDBPayloadRepositoryOpts = (): AbstractMongoDBPayloadRepositoryOpts => {
+  return {
+    account: XyoAccount.random(),
+    boundWitnessSdk: getArchivistAllBoundWitnessesMongoSdk(),
+    config: { inlinePayloads: false },
+    payloadsSdk: getArchivistAllPayloadMongoSdk(),
+  }
+}
+
 export abstract class AbstractMongoDBPayloadRepository<T, TId = string, TQuery = Filter<T>> extends AbstractPayloadRepository<T, TId, TQuery> {
-  protected constructor(
-    protected readonly payloadsSdk: BaseMongoSdk<XyoPayload> = getArchivistAllPayloadMongoSdk(),
-    protected readonly boundWitnessSdk: BaseMongoSdk<XyoBoundWitness> = getArchivistAllBoundWitnessesMongoSdk(),
-    protected readonly account: XyoAccount = XyoAccount.random(),
-    protected readonly config: XyoBoundWitnessBuilderConfig = { inlinePayloads: false }
-  ) {
+  protected readonly account: XyoAccount
+  protected readonly boundWitnessSdk: BaseMongoSdk<XyoBoundWitness>
+  protected readonly config: XyoBoundWitnessBuilderConfig
+  protected readonly payloadsSdk: BaseMongoSdk<XyoPayload>
+
+  protected constructor(opts: AbstractMongoDBPayloadRepositoryOpts = getDefaultAbstractMongoDBPayloadRepositoryOpts()) {
     super()
+    this.account = opts.account
+    this.boundWitnessSdk = opts.boundWitnessSdk
+    this.config = opts.config
+    this.payloadsSdk = opts.payloadsSdk
   }
 }
