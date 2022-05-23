828e4de26b64844373d5532b92c80afd20874888
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-05-23 17:53:59 -0500

    Auth strategy for archive access by wallet address

---
+++ src/middleware/auth/strategy/ArchiveAccount/addressCanAccessArchive.ts
@@ -0,0 +1,5 @@
+import { Request } from 'express'
+export const addressCanAccessArchive = (req: Request, address: string, archive: string) => {
+  // TODO: Get repository from request
+  return false
+}
