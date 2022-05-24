a120e60513c70ab43c067fd843f3ce21cb689fa9
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-05-24 17:29:53 -0500

    Capture auth permutations

--- src/middleware/auth/strategy/ArchiveAccount/archiveAccountStrategy.spec.ts
+++ src/middleware/auth/strategy/ArchiveAccount/archiveAccountStrategy.spec.ts
@@ -1,11 +1,25 @@
 import { StatusCodes } from 'http-status-codes'
 
-import { claimArchive, getArchivist, getExistingWeb3User, signInWeb3User } from '../../../../test'
+import { ArchivePermissions, ArchivePermissionsPayload } from '../../../../model'
+import { claimArchive, getArchivist, getExistingWeb3User, signInWeb3User, TestWeb3User } from '../../../../test'
 
-const postCommandToArchive = async (archive: string, token?: string, expectedStatus: StatusCodes = StatusCodes.OK) => {
+const allowedSchema = 'network.xyo.debug'
+const otherSchema = 'network.xyo.test'
+
+const setArchivePermissions = async (archive: string, token: string, permissions: ArchivePermissions) => {
+  const data: ArchivePermissionsPayload = {
+    ...permissions,
+    _archive: archive,
+    schema: 'network.xyo.security.archive.permissions',
+  }
+  const response = await getArchivist().post('/').send(data).auth(token, { type: 'bearer' }).expect(StatusCodes.OK)
+  return response.body.data
+}
+
+const postCommandToArchive = async (archive: string, token?: string, schema = allowedSchema, expectedStatus: StatusCodes = StatusCodes.OK) => {
   const data = {
     _archive: archive,
-    schema: 'network.xyo.debug',
+    schema,
   }
   const response = token
     ? await getArchivist().post('/').send(data).auth(token, { type: 'bearer' }).expect(expectedStatus)
@@ -14,13 +28,15 @@ const postCommandToArchive = async (archive: string, token?: string, expectedSta
 }
 
 describe('ArchiveAccountStrategy', () => {
+  let user: TestWeb3User
   let token: string
   let archive: string
-  beforeAll(async () => {
-    token = await signInWeb3User(await getExistingWeb3User())
-    archive = (await claimArchive(token)).archive
-  })
   describe('with no archive permissions', () => {
+    beforeAll(async () => {
+      user = await getExistingWeb3User()
+      token = await signInWeb3User(user)
+      archive = (await claimArchive(token)).archive
+    })
     describe('with allowed address', () => {
       it('allows operation by address', async () => {
         await postCommandToArchive(archive, token)
@@ -28,13 +44,24 @@ describe('ArchiveAccountStrategy', () => {
     })
     describe('with anonymous', () => {
       it('allows operation', async () => {
-        await postCommandToArchive(archive, token)
+        await postCommandToArchive(archive)
       })
     })
   })
   describe('with archive permissions', () => {
     describe('for allowing', () => {
       describe('address', () => {
+        beforeAll(async () => {
+          user = await getExistingWeb3User()
+          token = await signInWeb3User(user)
+          archive = (await claimArchive(token)).archive
+          await setArchivePermissions(archive, token, {
+            allow: {
+              addresses: [user.address],
+            },
+            schema: 'network.xyo.security.archive.permissions',
+          })
+        })
         describe('with allowed address', () => {
           it('allows operation by address', async () => {
             await postCommandToArchive(archive, token)
@@ -42,55 +69,96 @@ describe('ArchiveAccountStrategy', () => {
         })
         describe('with address not in allowed list', () => {
           it('disallows operation by address', async () => {
-            // TODO:
+            const otherUser = await getExistingWeb3User()
+            const otherToken = await signInWeb3User(otherUser)
+            await postCommandToArchive(archive, otherToken, allowedSchema, StatusCodes.UNAUTHORIZED)
           })
         })
         describe('with anonymous', () => {
           it('disallows operation', async () => {
-            // TODO:
+            await postCommandToArchive(archive, undefined, allowedSchema, StatusCodes.UNAUTHORIZED)
           })
         })
       })
       describe('schema', () => {
+        beforeAll(async () => {
+          user = await getExistingWeb3User()
+          token = await signInWeb3User(user)
+          archive = (await claimArchive(token)).archive
+          await setArchivePermissions(archive, token, {
+            allow: {
+              schemas: [allowedSchema],
+            },
+            schema: 'network.xyo.security.archive.permissions',
+          })
+        })
         describe('with allowed schema', () => {
           it('allows operation by address', async () => {
-            // TODO:
+            await postCommandToArchive(archive, token)
           })
         })
         describe('with schema not in allowed list', () => {
           it('disallows operation by address', async () => {
-            // TODO:
+            await postCommandToArchive(archive, token, otherSchema, StatusCodes.UNAUTHORIZED)
           })
         })
       })
     })
     describe('for rejecting', () => {
       describe('address', () => {
+        let otherUser: TestWeb3User
+        let otherToken: string
+        beforeAll(async () => {
+          otherUser = await getExistingWeb3User()
+          otherToken = await signInWeb3User(otherUser)
+          user = await getExistingWeb3User()
+          token = await signInWeb3User(user)
+          archive = (await claimArchive(token)).archive
+          await setArchivePermissions(archive, token, {
+            reject: {
+              addresses: [otherUser.address],
+            },
+            schema: 'network.xyo.security.archive.permissions',
+          })
+        })
         describe('with disallowed address', () => {
           it('disallows operation by address', async () => {
-            // TODO:
+            await postCommandToArchive(archive, otherToken)
           })
         })
         describe('with address not in disallowed list', () => {
-          it('TODO: allows operation by address', async () => {
-            // TODO:
+          it('allows operation by address', async () => {
+            const otherUser = await getExistingWeb3User()
+            const otherToken = await signInWeb3User(otherUser)
+            await postCommandToArchive(archive, otherToken)
           })
         })
         describe('with anonymous address', () => {
-          it('TODO: allows operation by address', async () => {
-            // TODO:
+          it('allows operation by address', async () => {
+            await postCommandToArchive(archive)
           })
         })
       })
       describe('schema', () => {
+        beforeAll(async () => {
+          user = await getExistingWeb3User()
+          token = await signInWeb3User(user)
+          archive = (await claimArchive(token)).archive
+          await setArchivePermissions(archive, token, {
+            reject: {
+              schemas: [otherSchema],
+            },
+            schema: 'network.xyo.security.archive.permissions',
+          })
+        })
         describe('with disallowed schema', () => {
           it('disallows operation by address', async () => {
-            // TODO:
+            await postCommandToArchive(archive, token, otherSchema)
           })
         })
         describe('with schema not in disallowed list', () => {
           it('allows operation by address', async () => {
-            // TODO:
+            await postCommandToArchive(archive, token)
           })
         })
       })
