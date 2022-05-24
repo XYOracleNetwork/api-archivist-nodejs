e4b37a919eb4ec6e2e85625696092f0bbfa103c5
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-05-24 06:09:04 -0500

    Strongly type ArchivePermissions payload

--- src/middleware/Diviner/ArchivePermissions/MongoDB/MongoDBArchivePermissionsPayloadRepository.ts
+++ src/middleware/Diviner/ArchivePermissions/MongoDB/MongoDBArchivePermissionsPayloadRepository.ts
@@ -1,32 +1,29 @@
-import { XyoBoundWitnessBuilder, XyoPayload, XyoPayloadBuilder } from '@xyo-network/sdk-xyo-client-js'
+import { XyoBoundWitnessBuilder, XyoPayloadBuilder } from '@xyo-network/sdk-xyo-client-js'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { Filter } from 'mongodb'
 
 import { AbstractMongoDBPayloadRepository, AbstractMongoDBPayloadRepositoryOpts, getDefaultAbstractMongoDBPayloadRepositoryOpts } from '../../../../lib'
-import { ArchivePermissions } from '../ArchivePermissions'
+import { ArchivePermissionsPayload, ArchivePermissionsPayloadSchema } from '../ArchivePermissions'
 
-const schema = 'network.xyo.archive'
+const schema: ArchivePermissionsPayloadSchema = 'network.xyo.security.archive.permissions'
 
-export class MongoDBArchivePermissionsPayloadRepository extends AbstractMongoDBPayloadRepository<ArchivePermissions> {
-  constructor(
-    protected readonly itemsSdk: BaseMongoSdk<XyoPayload<ArchivePermissions>>,
-    opts: AbstractMongoDBPayloadRepositoryOpts = getDefaultAbstractMongoDBPayloadRepositoryOpts()
-  ) {
+export class MongoDBArchivePermissionsPayloadPayloadRepository extends AbstractMongoDBPayloadRepository<ArchivePermissionsPayload> {
+  constructor(protected readonly items: BaseMongoSdk<ArchivePermissionsPayload>, opts: AbstractMongoDBPayloadRepositoryOpts = getDefaultAbstractMongoDBPayloadRepositoryOpts()) {
     super(opts)
   }
-  async find(filter: Filter<XyoPayload<ArchivePermissions>>): Promise<XyoPayload<ArchivePermissions>[]> {
-    return (await this.itemsSdk.find(filter)).toArray()
+  async find(filter: Filter<ArchivePermissionsPayload>): Promise<ArchivePermissionsPayload[]> {
+    return (await this.items.find(filter)).toArray()
   }
-  async get(name: string): Promise<XyoPayload<ArchivePermissions>[]> {
-    return (await this.itemsSdk.find({ name })).toArray()
+  async get(archive: string): Promise<ArchivePermissionsPayload[]> {
+    return (await this.items.find({ _archive: archive, schema })).sort({ _timestamp: -1 }).toArray()
   }
-  async insert(items: ArchivePermissions[]): Promise<XyoPayload<ArchivePermissions>[]> {
+  async insert(items: ArchivePermissionsPayload[]): Promise<ArchivePermissionsPayload[]> {
     const payloads = items.map((i) => new XyoPayloadBuilder({ schema }).fields({ i }).build())
     const bw = new XyoBoundWitnessBuilder(this.config).witness(this.account).payloads(payloads).build()
     const bwResult = await this.boundWitnessSdk.insertOne(bw)
-    if (bwResult.acknowledged && bwResult.insertedId) throw new Error('MongoDBPayloadRepository: Error inserting BoundWitness')
+    if (bwResult.acknowledged && bwResult.insertedId) throw new Error('MongoDBArchivePermissionsPayloadPayloadRepository: Error inserting BoundWitness')
     const result = await this.payloadsSdk.insertMany(payloads)
-    if (result.insertedCount != payloads.length) throw new Error('MongoDBPayloadRepository: Error inserting Payloads')
-    return payloads as XyoPayload<ArchivePermissions>[]
+    if (result.insertedCount != payloads.length) throw new Error('MongoDBArchivePermissionsPayloadPayloadRepository: Error inserting Payloads')
+    return payloads as ArchivePermissionsPayload[]
   }
 }
