c1bbc3f89b6dead53111acc1ca86672f80df9496
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-05-26 08:12:02 -0500

    Post as array of BoundWitnesses

--- src/post/post.ts
+++ src/post/post.ts
@@ -1,19 +1,37 @@
 import { asyncHandler, NoReqParams } from '@xylabs/sdk-api-express-ecs'
-import { XyoPayload } from '@xyo-network/sdk-xyo-client-js'
+import { XyoBoundWitness, XyoBoundWitnessBuilder, XyoPayload, XyoPayloadBuilder } from '@xyo-network/sdk-xyo-client-js'
 import { RequestHandler } from 'express'
 
+// TODO: Move to SDK
+export const exists = <T>(x: T | undefined): x is T => {
+  return !!x
+}
+
+const unsupportedSchemaType = 'network.xyo.unsupported'
+const responseSchemaType = 'network.xyo.command.response'
+
 // TODO: CQRS pattern of returning empty for commands, redirect for queries
-const handler: RequestHandler<NoReqParams, unknown[], XyoPayload[]> = async (req, res, next) => {
+const handler: RequestHandler<NoReqParams, XyoBoundWitness[], XyoBoundWitness[]> = async (req, res, next) => {
   const { processors } = res.app.payloadProcessorRegistry
-  const payloads: XyoPayload[] = Array.isArray(req.body) ? req.body : [req.body]
-  const result: unknown[] =
-    (await Promise.all(
-      payloads.map((p) => {
+  const boundWitnesses: XyoBoundWitness[] = Array.isArray(req.body) ? req.body : [req.body]
+  const result = boundWitnesses.map((boundWitness) => {
+    const payloads: XyoPayload[] = boundWitness._payloads?.filter(exists) || []
+    return (
+      payloads.map(async (p) => {
         const processor = processors[p.schema]
-        return processor ? processor(p) : { schema: 'network.xyo.unsupported' }
-      })
-    )) || []
-  res.json(result)
+        // TODO: Communicate response schema back from command/query result
+        // TODO: Link response to requested payload
+        return processor ? new XyoPayloadBuilder({ schema: 'todo' }).fields((await processor(p)) as Partial<XyoPayload>).build() : { schema: unsupportedSchemaType }
+      }) || []
+    )
+  })
+  const response: XyoBoundWitness[] = await Promise.all(
+    result.map(async (x) => {
+      const payloads: XyoPayload[] = await Promise.all(x)
+      return new XyoBoundWitnessBuilder({ inlinePayloads: true }).payloads(payloads).build()
+    })
+  )
+  res.json(response)
   next()
 }
 
