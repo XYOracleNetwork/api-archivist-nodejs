32c1450db02c0851808e0882e42160febd351058
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-01 10:18:10 -0500

    Commands/Quries extend XyoPayload to allow for known props

--- src/model/Query/Query.ts
+++ src/model/Query/Query.ts
@@ -1,3 +1,6 @@
-export type Query = {
+import { XyoPayload } from '@xyo-network/sdk-xyo-client-js'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Query extends XyoPayload {
   // Marker interface for all Queries
 }
