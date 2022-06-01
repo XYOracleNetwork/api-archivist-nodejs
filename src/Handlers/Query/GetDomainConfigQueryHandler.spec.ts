a2896c4879e76c9a97cb609fb6ea09ffd0e69ee6
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-01 09:39:49 -0500

    Tests for domain config handler

---
+++ src/Handlers/Query/GetDomainConfigQueryHandler.spec.ts
@@ -0,0 +1,19 @@
+import { getDomainConfigSchema } from '../../model'
+import { GetDomainConfigQueryHandler } from './GetDomainConfigQueryHandler'
+
+const schema = getDomainConfigSchema
+const domain = 'network.xyo'
+
+describe('GetDomainConfigQueryHandler', () => {
+  describe('With valid domain', () => {
+    it('Returns the domain config', async () => {
+      const sut = new GetDomainConfigQueryHandler({})
+      const actual = await sut.handle({ domain, schema })
+      expect(actual).toBeTruthy()
+      // TODO: Replace with const from SDK
+      expect(actual?.schema).toBe('network.xyo.domain')
+      expect(actual?.aliases).toBeDefined()
+      expect(actual?.aliases?.['network.xyo.schema']).toBeDefined()
+    })
+  })
+})
