9b8ac56e5487f496e1b71b93a3a988ef361ee7db
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-01 10:55:28 -0500

    Modularize schema processor registration

--- src/server/addSchemaHandlers.ts
+++ src/server/addSchemaHandlers.ts
@@ -13,9 +13,21 @@ import {
 
 export const addSchemaHandlers = (app: Application) => {
   const registry: XyoPayloadProcessorRegistry = app.payloadProcessorRegistry
+  addDebugHandlers(registry)
+  addCommandHandlers(app, registry)
+  addQueryHandlers(app, registry)
+}
+
+export const addDebugHandlers = (registry: XyoPayloadProcessorRegistry) => {
   registry.registerProcessorForSchema('network.xyo.debug', () => Promise.resolve())
   registry.registerProcessorForSchema('network.xyo.test', () => Promise.resolve())
+}
+
+export const addCommandHandlers = (app: Application, registry: XyoPayloadProcessorRegistry) => {
   registry.registerProcessorForSchema(setArchivePermissionsSchema, (x: SetArchivePermissionsPayload) => new SetArchivePermissionsCommandHandler({ ...app }).handle(x))
+}
+
+export const addQueryHandlers = (app: Application, registry: XyoPayloadProcessorRegistry) => {
   registry.registerProcessorForSchema(getArchivePermissionsSchema, (x: GetArchivePermissionsPayload) => new GetArchivePermissionsQueryHandler({ ...app }).handle(x))
   registry.registerProcessorForSchema(getDomainConfigSchema, (x: GetDomainConfigPayload) => new GetDomainConfigQueryHandler({ ...app }).handle(x))
 }
