322eef85e47a26bb6b36241a2f8bb37271a70b87
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-05-31 21:10:19 -0500

    Break apart handler into smaller methods

--- src/hash/get/get.ts
+++ src/hash/get/get.ts
@@ -1,6 +1,6 @@
 import { asyncHandler, NoReqBody, NoReqQuery } from '@xylabs/sdk-api-express-ecs'
 import { deepOmitUnderscoreFields, XyoPayload } from '@xyo-network/sdk-xyo-client-js'
-import { RequestHandler } from 'express'
+import { Request, RequestHandler } from 'express'
 import { StatusCodes } from 'http-status-codes'
 
 import { findByHash, isPublicArchive, isRequestUserOwnerOfArchive } from '../../lib'
@@ -10,6 +10,21 @@ const reservedHashes = ['archive', 'schema', 'doc', 'domain']
 
 export type HashPathParams = {
   hash: string
+}
+
+const getBlockForRequest = async (req: Request, hash: string): Promise<XyoPayload | undefined> => {
+  for (const block of await findByHash(hash)) {
+    const name = block?._archive
+    if (!name) {
+      continue
+    }
+    const archive = await req.app.archiveRepository.get(name)
+    // If the archive is public or if the archive is private but this is
+    // an auth'd request from the archive owner
+    if (isPublicArchive(archive) || isRequestUserOwnerOfArchive(req, archive)) {
+      return block
+    }
+  }
 }
 
 const handler: RequestHandler<HashPathParams, XyoPayload, NoReqBody, NoReqQuery> = async (req, res, next) => {
@@ -25,19 +40,11 @@ const handler: RequestHandler<HashPathParams, XyoPayload, NoReqBody, NoReqQuery>
     next()
     return
   }
-  for (const block of await findByHash(hash)) {
-    const name = block?._archive
-    if (!name) {
-      continue
-    }
-    const archive = await req.app.archiveRepository.get(name)
-    // If the archive is public or if the archive is private but this is
-    // an auth'd request from the archive owner
-    if (isPublicArchive(archive) || isRequestUserOwnerOfArchive(req, archive)) {
-      setRawResponseFormat(res)
-      res.json({ ...deepOmitUnderscoreFields(block) })
-      return
-    }
+  const block = await getBlockForRequest(req, hash)
+  if (block) {
+    setRawResponseFormat(res)
+    res.json({ ...deepOmitUnderscoreFields(block) })
+    return
   }
   next({ message: 'Hash not found', statusCode: StatusCodes.NOT_FOUND })
 }
