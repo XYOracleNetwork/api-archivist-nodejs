a202ec34bbd68f36c6bc9a85f2c2a8c745323f0e
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-06 11:49:43 -0500

    Archivist-witnessed payload repository

--- src/middleware/Diviner/ArchivistWitnessedPayloadRepository/MongoDB/MongoDBArchivistWitnessedPayloadRepository.ts
+++ src/middleware/Diviner/ArchivistWitnessedPayloadRepository/MongoDB/MongoDBArchivistWitnessedPayloadRepository.ts
@@ -1,9 +1,14 @@
-import { XyoAccount, XyoBoundWitness, XyoPayload } from '@xyo-network/sdk-xyo-client-js'
+import { exists } from '@xylabs/sdk-js'
+import { XyoAccount, XyoBoundWitness, XyoBoundWitnessBuilder, XyoPayload } from '@xyo-network/sdk-xyo-client-js'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { Filter } from 'mongodb'
 
 import { getBaseMongoSdk } from '../../../../lib'
 import { AbstractPayloadRepository } from '../../../../model'
+
+const unique = <T>(value: T, index: number, self: T[]) => {
+  return self.indexOf(value) === index
+}
 
 export class MongoDBArchivistWitnessedPayloadRepository extends AbstractPayloadRepository<XyoPayload, string, Filter<XyoPayload>> {
   constructor(
@@ -18,16 +23,26 @@ export class MongoDBArchivistWitnessedPayloadRepository extends AbstractPayloadR
     // TODO: How to support filtering but add our own filter in aggregation
   }
   async get(hash: string): Promise<XyoPayload[]> {
-    // TODO: Find bw signed by us that has this hash, then find payload
-    return (await this.payloads.find({ _hash: hash })).toArray()
+    // Find bw signed by us that has this hash
+    const bound_witnesses = await (await this.boundWitnesses.find({ payload_hashes: hash })).toArray()
+    const archives = bound_witnesses
+      .map((bw) => bw._archive)
+      .filter(exists)
+      .filter(unique)
+    return (await this.payloads.find({ _archive: { $in: archives }, _hash: hash })).toArray()
   }
-  async insert(items: XyoPayload[]): Promise<XyoPayload[]> {
-    // TODO: Witness from archivist
-    // TODO: sanitize before storage, grab methods: prepareBoundWitnesses, validatePayloadSchema, storeBoundWitnesses, storePayloads from post.ts for reference
-    const result = await this.payloads.insertMany(items)
-    if (result.insertedCount != items.length) {
+  async insert(payloads: XyoPayload[]): Promise<XyoPayload[]> {
+    // Witness from archivist
+    const bw = new XyoBoundWitnessBuilder({ inlinePayloads: false }).payloads(payloads).build()
+    const witnessResult = await this.boundWitnesses.insertOne(bw)
+    if (!witnessResult.acknowledged || !witnessResult.insertedId) {
+      throw new Error('Error inserting BoundWitness')
+    }
+    // Store payloads
+    const result = await this.payloads.insertMany(payloads)
+    if (result.insertedCount != payloads.length) {
       throw new Error('Error inserting Payloads')
     }
-    return items
+    return payloads
   }
 }
