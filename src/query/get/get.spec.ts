82dbccfda06bc7e57c8091958282fdf21a96bbbe
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-06 19:44:05 -0500

    Test scaffolding query status endpoint

--- src/query/get/get.spec.ts
+++ src/query/get/get.spec.ts
@@ -1,10 +1,46 @@
+import { XyoPayload, XyoPayloadBuilder } from '@xyo-network/sdk-xyo-client-js'
 import { StatusCodes } from 'http-status-codes'
+import { v4 } from 'uuid'
 
 import { getArchivist } from '../../test'
 
+const getTestRequest = (delay = 1): XyoPayload => {
+  const schema = 'network.xyo.debug'
+  const fields = { delay, nonce: v4() }
+  return new XyoPayloadBuilder({ schema }).fields(fields).build()
+}
+
+const postRequest = (payload = getTestRequest(1)): Promise<XyoPayload> => {
+  return Promise.resolve(payload)
+}
+
 describe('/query/:hash', () => {
-  it('is not implemented', async () => {
-    const sut = getArchivist()
-    await sut.get('/query/foo').expect(StatusCodes.ACCEPTED)
+  let payload: XyoPayload
+  describe('for processing query', () => {
+    beforeEach(async () => {
+      payload = await postRequest(getTestRequest(10000))
+    })
+    it('returns accepted', async () => {
+      const sut = getArchivist()
+      await sut.get(`/query/${payload._hash}`).expect(StatusCodes.ACCEPTED)
+    })
+  })
+  describe('for non-existent query', () => {
+    beforeEach(() => {
+      payload = getTestRequest(1)
+    })
+    it('returns not found', async () => {
+      const sut = getArchivist()
+      await sut.get(`/query/${payload._hash}`).expect(StatusCodes.NOT_FOUND)
+    })
+  })
+  describe('for completed query', () => {
+    beforeEach(async () => {
+      payload = await postRequest()
+    })
+    it('redirects to HURI', async () => {
+      const sut = getArchivist()
+      await sut.get(`/query/${payload._hash}`).expect(StatusCodes.MOVED_TEMPORARILY)
+    })
   })
 })
