ed4201a8fc44ae3acdc4400ab7bd0af76d7acf3d
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-08 06:12:51 -0500

    Types for query processor responses

--- src/middleware/QueryProcessor/QueryProcessorRegistry.ts
+++ src/middleware/QueryProcessor/QueryProcessorRegistry.ts
@@ -1,7 +1,9 @@
-import { Query } from '../../model'
+import { XyoPayload } from '@xyo-network/sdk-xyo-client-js'
+
+import { Optional, Query } from '../../model'
 import { QueryProcessor } from './QueryProcessor'
 
-export interface QueryProcessorRegistry {
-  processors: Readonly<Record<string, QueryProcessor>>
-  registerProcessorForSchema: <T extends Query = Query, R = unknown>(schema: string, processor: QueryProcessor<T, R>) => void
+export interface QueryProcessorRegistry<T extends Query = Query, R extends Optional<XyoPayload> = Optional<XyoPayload>> {
+  processors: Readonly<Record<string, QueryProcessor<T, R>>>
+  registerProcessorForSchema: (schema: string, processor: QueryProcessor<T, R>) => void
 }
