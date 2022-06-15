de615d6b6f4c766c64f28d17aa12db2e4a7e62f1
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-15 09:54:30 -0700

    Ensure legacy access control is only boolean

--- src/lib/Archive/determineArchiveAccessControl.ts
+++ src/lib/Archive/determineArchiveAccessControl.ts
@@ -1,6 +1,19 @@
 import { XyoArchive } from '@xyo-network/sdk-xyo-client-js'
 
-export const determineArchiveAccessControl = (record: XyoArchive) => {
+/**
+ * Determines if there's access controls on an archive
+ * @param record The archive record
+ * @returns True if there's access controls, false otherwise
+ */
+export const determineArchiveAccessControl = (record: XyoArchive): boolean => {
   const { accessControl } = record
-  return accessControl || false
+  let controls = false
+  if (accessControl === true) {
+    controls = true
+  } else if (accessControl === false) {
+    controls = false
+  } else {
+    controls = !!accessControl?.access
+  }
+  return controls
 }
