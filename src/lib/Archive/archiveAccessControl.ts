090d0080348fc19bcf44f8419e7995a36f20a30c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-15 10:56:43 -0700

    Add option to update newer permissions on archive create/update

---
+++ src/lib/Archive/archiveAccessControl.ts
@@ -0,0 +1,32 @@
+import { ArchivePermissionsRepository } from '../../middleware'
+import { SetArchivePermissionsSchema, setArchivePermissionsSchema } from '../../model'
+
+const getPrivatePermissions = (archive: string) => {
+  return {
+    _archive: archive,
+    _timestamp: Date.now(),
+    allow: {
+      addresses: [],
+    },
+    schema: setArchivePermissionsSchema as SetArchivePermissionsSchema,
+  }
+}
+const getPublicPermissions = (archive: string) => {
+  return {
+    _archive: archive,
+    _timestamp: Date.now(),
+    schema: setArchivePermissionsSchema as SetArchivePermissionsSchema,
+  }
+}
+
+export function setArchiveAccessPublic(archivePermissionsRepository: ArchivePermissionsRepository, archive: string) {
+  return setArchiveAccess(archivePermissionsRepository, archive, true)
+}
+export function setArchiveAccessPrivate(archivePermissionsRepository: ArchivePermissionsRepository, archive: string) {
+  return setArchiveAccess(archivePermissionsRepository, archive, true)
+}
+
+function setArchiveAccess(archivePermissionsRepository: ArchivePermissionsRepository, archive: string, accessControl: boolean) {
+  const permissions = accessControl ? getPrivatePermissions(archive) : getPublicPermissions(archive)
+  return archivePermissionsRepository.insert([permissions])
+}
