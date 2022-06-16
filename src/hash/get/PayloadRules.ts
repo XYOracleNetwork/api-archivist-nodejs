a556a2e311f81976cef1a0e1aab4b6cad219b76e
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-16 10:29:33 -0700

    Filtering for PayloadRule edge cases

--- src/hash/get/PayloadRules.ts
+++ src/hash/get/PayloadRules.ts
@@ -5,14 +5,23 @@ import { SortDirection } from '../../model'
 export interface PayloadArchiveRule {
   archive: string
 }
+const isPayloadArchiveRule = (rule: PayloadRule): rule is PayloadArchiveRule => {
+  return !!(rule as PayloadArchiveRule)?.archive
+}
 
 export interface PayloadTimestampDirectionRule {
   direction: SortDirection
   timestamp: number
 }
+const isPayloadTimestampDirectionRule = (rule: PayloadRule): rule is PayloadTimestampDirectionRule => {
+  return !!(rule as PayloadTimestampDirectionRule)?.timestamp
+}
 
 export interface PayloadSchemaRule {
   schema: string
+}
+const isPayloadSchemaRule = (rule: PayloadRule): rule is PayloadSchemaRule => {
+  return !!(rule as PayloadSchemaRule)?.schema
 }
 
 export type PayloadRule = PayloadArchiveRule | PayloadTimestampDirectionRule | PayloadSchemaRule
@@ -27,24 +36,27 @@ export interface PayloadSearchCriteria {
 export const combineRules = (rules: PayloadRule[][]): PayloadSearchCriteria => {
   const archive = rules
     .flatMap((r) => r)
-    .map((r) => (r as PayloadArchiveRule)?.archive)
+    .filter(isPayloadArchiveRule)
+    .map((r) => r.archive)
     .filter(exists)
+  assertEx(archive.length, 'At least one archive must be supplied')
+
   const schema = rules
     .flatMap((r) => r)
-    .map((r) => (r as PayloadSchemaRule)?.schema)
+    .filter(isPayloadSchemaRule)
+    .map((r) => r.schema)
     .filter(exists)
-  const foundDirection = rules
-    .flatMap((r) => r)
-    .map((r) => (r as PayloadTimestampDirectionRule)?.direction)
-    .filter(exists)
-  const direction: SortDirection = foundDirection.length ? foundDirection[0] : 'desc'
-  const foundTimestamp = rules
-    .flatMap((r) => r)
-    .map((r) => (r as PayloadTimestampDirectionRule)?.timestamp)
-    .filter(exists)
-  const timestamp: number = foundTimestamp.length ? foundTimestamp[0] : Date.now()
-  assertEx(archive.length, 'At least one archive must be supplied')
   assertEx(schema.length, 'At least one schema must be supplied')
+
+  const directionTimestamp = rules
+    .flatMap((r) => r)
+    .filter(isPayloadTimestampDirectionRule)
+    .filter(exists)
+  assertEx(directionTimestamp.length < 2, 'Must not supply more than 1 direction/timestamp rule')
+
+  const direction: SortDirection = directionTimestamp.length ? directionTimestamp[0]?.direction : 'desc'
+  const timestamp: number = directionTimestamp.length ? directionTimestamp[0]?.timestamp : Date.now()
+
   return {
     archive,
     direction,
