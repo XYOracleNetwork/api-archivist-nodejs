65049a6475c310c36045da2226acb65778ad2e73
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-17 11:17:10 -0700

    Use existing type for path params

--- src/management/migrate/permissions/archive/post.ts
+++ src/management/migrate/permissions/archive/post.ts
@@ -1,21 +1,18 @@
-import { asyncHandler, NoReqParams } from '@xylabs/sdk-api-express-ecs'
+import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
 import { XyoArchive } from '@xyo-network/sdk-xyo-client-js'
 import { RequestHandler } from 'express'
 import { StatusCodes } from 'http-status-codes'
 
 import { getBaseMongoSdk } from '../../../../lib'
+import { ArchivePathParams } from '../../../../model'
 import { migrateLegacyArchives } from '../migrateLegacyArchives'
-
-export interface MigratePathParams extends NoReqParams {
-  archive: string
-}
 
 const getArchive = (archive: string): Promise<XyoArchive | null> => {
   const sdk = getBaseMongoSdk<XyoArchive>('archives')
   return sdk.findOne({ archive })
 }
 
-const handler: RequestHandler<MigratePathParams> = async (req, res, next) => {
+const handler: RequestHandler<ArchivePathParams> = async (req, res, next) => {
   const { archive } = req.params
   const { archivePermissionsRepository } = req.app
   const entity = await getArchive(archive)
