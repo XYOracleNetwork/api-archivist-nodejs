e9a65c590a33197af3bdc65066f223df815bae94
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-17 15:13:01 -0700

    Permissions migration tests

--- src/management/migrate/permissions/archive/post.spec.ts
+++ src/management/migrate/permissions/archive/post.spec.ts
@@ -1,12 +1,47 @@
-import { delay } from '@xylabs/sdk-js'
+import { ForgetPromise } from '@xylabs/sdk-js'
+import { XyoArchive } from '@xyo-network/sdk-xyo-client-js'
+import { StatusCodes } from 'http-status-codes'
 
-import {} from '../../../../test'
+import { SetArchivePermissionsPayload } from '../../../../model'
+import { claimArchive, getArchivist, getTokenForNewUser, setArchiveAccessControl } from '../../../../test'
+
+interface MigrationResponse {
+  archive: XyoArchive
+  migrated: SetArchivePermissionsPayload
+}
+
+const migrateArchive = async (archive: string): Promise<MigrationResponse> => {
+  const path = `/management/migrate/permissions/archives/${archive}`
+  const header = { 'x-api-key': process.env.API_KEY }
+  const response = await getArchivist().post(path).set(header).expect(StatusCodes.OK)
+  const result = response.body.data
+  expect(result).toBeDefined()
+  expect(result.archive).toBeDefined()
+  expect(result.archive.archive).toBe(archive)
+  expect(result).toBeDefined()
+  expect(result.migrated).toBeDefined()
+  expect(result.migrated?.reject).toBeUndefined()
+  return result
+}
 
 describe('/management/migrate/permissions/archives/:archive', () => {
-  it.skip('Migrates a public archive', async () => {
-    await delay(1)
+  let token: string
+  let archive: XyoArchive
+  beforeEach(async () => {
+    token = await getTokenForNewUser()
+    archive = await claimArchive(token)
   })
-  it.skip('Migrates a private archive', async () => {
-    await delay(1)
+  it('Migrates a public archive', async () => {
+    const result = await migrateArchive(archive.archive)
+    expect(result.migrated?.allow).toBeUndefined()
+  })
+  it('Migrates a private archive', async () => {
+    archive.accessControl = true
+    await setArchiveAccessControl(token, archive.archive, archive)
+    const result = await migrateArchive(archive.archive)
+    expect(result.migrated?.allow?.addresses).toEqual([])
+  })
+  afterAll(async () => {
+    await ForgetPromise.awaitInactive()
   })
 })
