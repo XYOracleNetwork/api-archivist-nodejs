11533d1a3900a1e8aeaccc3c4bcbf507c3f3bdad
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-17 11:10:10 -0700

    Add endpoint for migrating single archive

--- src/management/migrateLegacyPermissions/migrateArchives/post.ts
+++ src/management/migrateLegacyPermissions/migrateArchives/post.ts
@@ -4,43 +4,20 @@ import { XyoArchive } from '@xyo-network/sdk-xyo-client-js'
 import { RequestHandler } from 'express'
 import { StatusCodes } from 'http-status-codes'
 
-import { getBaseMongoSdk, isLegacyPrivateArchive } from '../../lib'
-import { ArchivePermissionsRepository, SetArchivePermissions, setArchivePermissionsSchema } from '../../model'
+import { getBaseMongoSdk } from '../../../lib'
+import { migrateLegacyArchives } from '../migrateLegacyArchives'
 
 export interface MigrateQueryParams extends NoReqQuery {
   limit: string
   offset: string
 }
 
-const schema = setArchivePermissionsSchema
 const defaultLimit = 200
 const defaultOffset = 0
-
-const publicArchivePermissions: SetArchivePermissions = {
-  schema,
-}
-
-const privateArchivePermissions: SetArchivePermissions = {
-  allow: {
-    addresses: [],
-  },
-  schema,
-}
 
 const getArchives = async (limit: number, offset: number): Promise<XyoArchive[]> => {
   const sdk = getBaseMongoSdk<XyoArchive>('archives')
   return (await sdk.find({})).sort({ _id: -1 }).skip(offset).limit(limit).toArray()
-}
-
-// TODO: Witness each record by Archivist?
-const migrateArchives = async (repository: ArchivePermissionsRepository, archives: XyoArchive[]) => {
-  const migrations = archives.map((archive) => {
-    // create a new public/private archive record for the legacy archive
-    const permissions: SetArchivePermissions = isLegacyPrivateArchive(archive) ? privateArchivePermissions : publicArchivePermissions
-    return repository.insert([permissions])
-  })
-  const results = await Promise.all(migrations)
-  return results.map((result) => result?.[0])
 }
 
 const handler: RequestHandler<NoReqParams, NoResBody, NoReqBody, MigrateQueryParams> = async (req, res, next) => {
@@ -50,10 +27,10 @@ const handler: RequestHandler<NoReqParams, NoResBody, NoReqBody, MigrateQueryPar
   const parsedOffset = tryParseInt(offset) || defaultOffset
   const archives = await getArchives(parsedLimit, parsedOffset)
   const archiveCount = archives.length
-  const migrated = await migrateArchives(archivePermissionsRepository, archives)
+  const migrated = await migrateLegacyArchives(archivePermissionsRepository, archives)
   const migratedCount = migrated.filter(exists).length
   res.status(StatusCodes.OK).json({ archiveCount, migratedCount })
   next()
 }
 
-export const postMigrateLegacyArchivePermissions = asyncHandler(handler)
+export const postMigrateLegacyArchivesPermissions = asyncHandler(handler)
