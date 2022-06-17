9b6234c0109ee61aaef6d5b3970990589fa40831
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-17 13:01:20 -0700

    Test scaffolding

---
+++ src/management/migrate/permissions/archives/post.spec.ts
@@ -0,0 +1,9 @@
+import { delay } from '@xylabs/sdk-js'
+
+import {} from '../../../../test'
+
+describe('/management/migrate/permissions/archives', () => {
+  it.skip('TODO: Has tests', async () => {
+    await delay(1)
+  })
+})
