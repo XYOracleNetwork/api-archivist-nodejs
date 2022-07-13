2adc5b231b50d26b2c7ff4ee26f33a0d3c592a3b
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-13 14:31:58 -0500

    DI for ArchiveAccessControlStrategy

--- src/server/addAuth.ts
+++ src/server/addAuth.ts
@@ -5,12 +5,22 @@ import { decorate, injectable } from 'inversify'
 import passport, { Strategy } from 'passport'
 
 import dependencies from '../inversify.config'
-import { AdminApiKeyStrategy, adminApiKeyStrategyName, AllowUnauthenticatedStrategy, allowUnauthenticatedStrategyName, Web3AuthStrategy, web3StrategyName } from '../middleware'
+import {
+  AdminApiKeyStrategy,
+  adminApiKeyStrategyName,
+  AllowUnauthenticatedStrategy,
+  allowUnauthenticatedStrategyName,
+  ArchiveAccessControlStrategy,
+  archiveAccessControlStrategyName,
+  Web3AuthStrategy,
+  web3StrategyName,
+} from '../middleware'
 
 decorate(injectable(), Strategy)
 
 export const addAuth = (_app: Application) => {
   passport.use(adminApiKeyStrategyName, dependencies.get(AdminApiKeyStrategy))
   passport.use(allowUnauthenticatedStrategyName, dependencies.get(AllowUnauthenticatedStrategy))
+  passport.use(archiveAccessControlStrategyName, dependencies.get(ArchiveAccessControlStrategy))
   passport.use(web3StrategyName, dependencies.get(Web3AuthStrategy))
 }
