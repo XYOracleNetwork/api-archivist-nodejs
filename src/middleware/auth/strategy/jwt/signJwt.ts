70955dd6c470911a880cb9f40b626cd989fc3e55
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-13 14:39:13 -0500

    Remove dependency cycles

--- src/middleware/auth/strategy/jwt/signJwt.ts
+++ src/middleware/auth/strategy/jwt/signJwt.ts
@@ -1,7 +1,5 @@
-import { Logger } from '@xylabs/sdk-api-express-ecs'
 import jwt, { SignOptions } from 'jsonwebtoken'
 
-import dependencies from '../../../../inversify.config'
 import { UserDto } from '../../dto'
 
 export const signJwt = async (user: UserDto, secretOrKey: string, options: SignOptions): Promise<string | undefined> => {
@@ -9,9 +7,6 @@ export const signJwt = async (user: UserDto, secretOrKey: string, options: SignO
     // eslint-disable-next-line import/no-named-as-default-member
     jwt.sign({ user }, secretOrKey, options, (err: Error | null, encoded?: string) => {
       if (err) {
-        const logger = dependencies.get<Logger>('Logger')
-        logger.log('Error signing JWT')
-        logger.log(err.message)
         // Don't reject with anything so we don't leak sensitive information
         reject()
       } else {
--- src/middleware/rollbar/rollbar.ts
+++ src/middleware/rollbar/rollbar.ts
@@ -1,8 +1,5 @@
-import { Logger } from '@xylabs/sdk-api-express-ecs'
 import { NextFunction, Request, Response } from 'express'
 import Rollbar, { ExpressErrorHandler } from 'rollbar'
-
-import dependencies from '../../inversify.config'
 
 const defaultEnvironment = 'development'
 
@@ -17,8 +14,6 @@ export const rollbarErrorHandler = (): ExpressErrorHandler => {
   const accessToken = process.env.ROLLBAR_ACCESS_TOKEN
   if (accessToken) {
     const environment = process.env.ROLLBAR_ENVIRONMENT || defaultEnvironment
-    const logger = dependencies.get<Logger>('Logger')
-    logger.log(`Configuring Rollbar for Environment: ${environment}`)
     return new Rollbar({ accessToken, environment }).errorHandler
   } else {
     return noOpErrorHandler
