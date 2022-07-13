8199b9d0a0271489d5874839200b8a5f269266eb
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-13 14:37:13 -0500

    DI for ArchiveAccountStrategy

--- src/middleware/auth/configureStrategies.ts
+++ src/middleware/auth/configureStrategies.ts
@@ -1,12 +1,11 @@
 import { assertEx } from '@xylabs/sdk-js'
 
 import { AuthConfig } from './AuthConfig'
-import { configureArchiveAccountStrategy, configureArchiveApiKeyStrategy, configureArchiveOwnerStrategy, configureJwtStrategy, configureLocalStrategy } from './strategy'
+import { configureArchiveApiKeyStrategy, configureArchiveOwnerStrategy, configureJwtStrategy, configureLocalStrategy } from './strategy'
 
 export const configureStrategies = (config: AuthConfig) => {
   assertEx(config?.secretOrKey, 'Missing JWT secretOrKey')
   const secretOrKey = config?.secretOrKey as string
-  configureArchiveAccountStrategy()
   configureArchiveApiKeyStrategy()
   configureArchiveOwnerStrategy()
   const jwtRequestHandler = configureJwtStrategy(secretOrKey)
