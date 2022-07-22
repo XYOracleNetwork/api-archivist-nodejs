d2ca45b544d18bc50d49968899b7d2b58f1505da
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-22 14:30:53 -0400

    Strongly typed BoundWitness building for schema payloads

--- src/schema/schema/get/get.spec.ts
+++ src/schema/schema/get/get.spec.ts
@@ -1,11 +1,11 @@
-import { XyoAccount, XyoBoundWitnessBuilder } from '@xyo-network/sdk-xyo-client-js'
+import { XyoAccount, XyoBoundWitnessBase, XyoBoundWitnessBuilder, XyoSchemaPayload } from '@xyo-network/sdk-xyo-client-js'
 
 import { claimArchive, getHash, getSchema, getTokenForNewUser, postBlock } from '../../../test'
 
 describe('/schema/:schema', () => {
   const schema = 'network.xyo.schema'
   const definition = { $schema: 'http://json-schema.org/draft-07/schema#' }
-  const bw = new XyoBoundWitnessBuilder({ inlinePayloads: true }).payload({ definition, schema }).witness(XyoAccount.random()).build()
+  const bw = new XyoBoundWitnessBuilder<XyoBoundWitnessBase, XyoSchemaPayload>({ inlinePayloads: true }).payload({ definition, schema }).witness(XyoAccount.random()).build()
   beforeAll(async () => {
     const token = await getTokenForNewUser()
     const archive = (await claimArchive(token)).archive
