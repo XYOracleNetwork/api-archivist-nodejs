ad5a4fce0dd004d9644b6623ca08637be9eafbea
Arie Trouw <arie.trouw@xylabs.com>
2022-07-30 13:25:01 -0700

    Split packages into two folders (core and express).  The express folder contains all the packages that depend directly or indirectly on express.
    Had to split the lib package into two packages, one that had the function that has express dependency for the express packages folder.
    
    Also removed the scripts section from all packages except root.
    
    Ran deps to update some deps

---
+++ packages/express/routes/src/routes/post/formatRequest.ts
@@ -0,0 +1,17 @@
+import { getRequestMeta } from '@xyo-network/archivist-express-lib'
+import { augmentWithMetadata } from '@xyo-network/archivist-lib'
+import { XyoBoundWitnessWithMeta, XyoPayloadMeta } from '@xyo-network/sdk-xyo-client-js'
+
+import { PostNodeRequest } from './PostNodeRequest'
+
+export const formatRequest = (req: PostNodeRequest): XyoBoundWitnessWithMeta[] => {
+  const [boundWitnessMeta, payloadMeta] = getRequestMeta(req)
+  const boundWitnesses: XyoBoundWitnessWithMeta[] = (Array.isArray(req.body) ? req.body : [req.body]) as XyoBoundWitnessWithMeta[]
+  return augmentWithMetadata(
+    boundWitnesses.map<XyoBoundWitnessWithMeta>((bw) => {
+      bw._payloads = bw._payloads?.length ? augmentWithMetadata(bw._payloads, payloadMeta as XyoPayloadMeta) : []
+      return bw
+    }),
+    boundWitnessMeta
+  )
+}
