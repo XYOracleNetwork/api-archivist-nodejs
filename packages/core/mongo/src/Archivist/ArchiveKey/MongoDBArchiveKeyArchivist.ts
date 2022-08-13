a5a4c7ca742a968cc8e8cd424ea925b19cb18264
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-13 16:47:11 -0500

    Add dependency

--- packages/core/mongo/src/Archivist/ArchiveKey/MongoDBArchiveKeyArchivist.ts
+++ packages/core/mongo/src/Archivist/ArchiveKey/MongoDBArchiveKeyArchivist.ts
@@ -1,17 +1,39 @@
 import { ArchiveKeyArchivist } from '@xyo-network/archivist-model'
 import { TYPES } from '@xyo-network/archivist-types'
+import { XyoArchiveKey } from '@xyo-network/sdk-xyo-client-js'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { inject, injectable } from 'inversify'
+import { Collection, Filter, WithId } from 'mongodb'
+
+const fromDb = (k: WithId<XyoArchiveKey>) => {
+  return {
+    archive: k.archive,
+    created: k._id.getTimestamp().getTime(),
+    key: k.key,
+  }
+}
+
 @injectable()
 export class MongoDBArchiveKeyArchivist implements ArchiveKeyArchivist {
-  constructor(@inject(TYPES.ArchiveKeySdkMongo) protected readonly keys: BaseMongoSdk<string>) {}
-  find(query: unknown): Promise<string[]> {
-    throw new Error('Method not implemented.')
+  constructor(@inject(TYPES.ArchiveKeySdkMongo) protected readonly keys: BaseMongoSdk<XyoArchiveKey>) {}
+  async find(filter: Filter<XyoArchiveKey>): Promise<XyoArchiveKey[]> {
+    return (await (await this.keys.find(filter)).toArray()).map(fromDb)
   }
-  get(id: string): Promise<string[] | null> {
-    throw new Error('Method not implemented.')
+  async get(archive: string): Promise<XyoArchiveKey[]> {
+    return (await (await this.keys.find({ archive })).toArray()).map(fromDb)
   }
-  insert(item: string[]): Promise<string[]> {
-    throw new Error('Method not implemented.')
+  async insert(item: XyoArchiveKey): Promise<XyoArchiveKey> {
+    return await this.keys.useCollection(async (collection: Collection<XyoArchiveKey>) => {
+      const result = await collection.insertOne({ ...item })
+      if (result.acknowledged) {
+        return {
+          archive: item.archive,
+          created: result.insertedId.getTimestamp().getTime(),
+          key: item.key,
+        }
+      } else {
+        throw new Error('Insert Failed')
+      }
+    })
   }
 }
--- packages/express/server/src/addDependencies.ts
+++ packages/express/server/src/addDependencies.ts
@@ -1,6 +1,13 @@
 import { dependencies } from '@xyo-network/archivist-dependencies'
 import { SchemaToQueryProcessorRegistry, XyoPayloadToQueryConverterRegistry } from '@xyo-network/archivist-middleware'
-import { ArchiveArchivist, ArchivePermissionsArchivist, Query, UserManager, WitnessedPayloadArchivist } from '@xyo-network/archivist-model'
+import {
+  ArchiveArchivist,
+  ArchiveKeyArchivist,
+  ArchivePermissionsArchivist,
+  Query,
+  UserManager,
+  WitnessedPayloadArchivist,
+} from '@xyo-network/archivist-model'
 import { IdentifiableHuri, Queue } from '@xyo-network/archivist-queue'
 import { TYPES } from '@xyo-network/archivist-types'
 import { Application } from 'express'
@@ -8,6 +15,7 @@ import { Application } from 'express'
 export const addDependencies = (app: Application) => {
   app.archivistWitnessedPayloadArchivist = dependencies.get<WitnessedPayloadArchivist>(TYPES.WitnessedPayloadArchivist)
   app.archiveArchivist = dependencies.get<ArchiveArchivist>(TYPES.ArchiveArchivist)
+  app.archiveKeyArchivist = dependencies.get<ArchiveKeyArchivist>(TYPES.ArchiveKeyArchivist)
   app.archivePermissionsArchivist = dependencies.get<ArchivePermissionsArchivist>(TYPES.ArchivePermissionsArchivist)
   app.queryConverters = dependencies.get<XyoPayloadToQueryConverterRegistry>(TYPES.PayloadToQueryConverterRegistry)
   app.queryProcessors = dependencies.get<SchemaToQueryProcessorRegistry>(TYPES.SchemaToQueryProcessorRegistry)
