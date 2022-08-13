a8c89c83e9c757880207c20aa7af43b0426965c6
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-12 22:17:15 -0500

    Cleanup deps

--- packages/core/lib/src/index.ts
+++ packages/core/lib/src/index.ts
@@ -1,28 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// Since Passport augments each successfully auth'd request
-// with our User, we need to redefine the default Express
-// User (just an empty Object) to be our User so we don't
-// have to cast every request
-import { UserWithoutId } from '@xyo-network/archivist-model'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import passport from 'passport'
-
-export interface UserCreationAuthInfo {
-  updated?: boolean
-}
-
-// https://github.com/DefinitelyTyped/DefinitelyTyped/commit/91c229dbdb653dbf0da91992f525905893cbeb91#r34812715
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace Express {
-    interface User extends UserWithoutId {
-      id?: string
-    }
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface AuthInfo extends UserCreationAuthInfo {}
-  }
-}
-
 export * from './Archive'
 export * from './dbSdk'
 export * from './Payload'
