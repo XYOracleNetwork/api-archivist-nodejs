a8a102376f0c0eb6a9d0644f0fc086001fe2df5c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-14 16:42:36 -0500

    Strongly type diviner query response

--- packages/core/model/src/Diviner/SchemaCountDiviner.ts
+++ packages/core/model/src/Diviner/SchemaCountDiviner.ts
@@ -1,3 +1,3 @@
 import { QueryableArchivist } from '../Archivist'
 
-export type SchemaCountDiviner = QueryableArchivist<unknown, string>
+export type SchemaCountDiviner = QueryableArchivist<Record<string, number>, string>
