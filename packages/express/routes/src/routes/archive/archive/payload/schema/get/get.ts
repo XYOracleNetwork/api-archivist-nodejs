7c11e43f7a9c19daf474756fb9e29644a0a62f54
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-16 07:09:16 -0500

    Use DI for archiveSchemaListDiviner

--- packages/express/routes/src/routes/archive/archive/payload/schema/get/get.ts
+++ packages/express/routes/src/routes/archive/archive/payload/schema/get/get.ts
@@ -1,11 +1,11 @@
 import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
-import { getPayloadSchemasInArchive } from '@xyo-network/archivist-lib'
 import { ArchivePathParams } from '@xyo-network/archivist-model'
 import { RequestHandler } from 'express'
 
 const handler: RequestHandler<ArchivePathParams, string[]> = async (req, res) => {
   const { archive } = req.params
-  const schemas = await getPayloadSchemasInArchive(archive)
+  const { archiveSchemaListDiviner } = req.app
+  const schemas = await archiveSchemaListDiviner.find(archive)
   res.json(schemas)
 }
 
