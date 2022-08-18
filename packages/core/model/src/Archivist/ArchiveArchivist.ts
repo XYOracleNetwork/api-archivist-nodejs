2b11969d5fd92be89209a5ef691237745310fd0d
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-18 08:57:10 -0500

    Use predicate for filter

--- packages/core/model/src/Archivist/ArchiveArchivist.ts
+++ packages/core/model/src/Archivist/ArchiveArchivist.ts
@@ -2,7 +2,15 @@ import { XyoArchive } from '@xyo-network/sdk-xyo-client-js'
 
 import { UpsertResult } from '../UpsertResult'
 import { Archivist } from './Archivist'
+import { XyoPayloadFilterPredicate } from './XyoPayloadFilterPredicate'
 
 export type EntityArchive = Required<XyoArchive>
 
-export type ArchiveArchivist = Archivist<EntityArchive & UpsertResult, EntityArchive, EntityArchive | null, string, EntityArchive[]>
+export type ArchiveArchivist = Archivist<
+  EntityArchive & UpsertResult,
+  EntityArchive,
+  EntityArchive | null,
+  string,
+  EntityArchive[],
+  XyoPayloadFilterPredicate<EntityArchive>
+>
