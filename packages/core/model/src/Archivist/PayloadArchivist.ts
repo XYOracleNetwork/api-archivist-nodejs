bdeb1439b2a2027a7ca76bbaf8a2618a97fd1aae
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-18 09:05:31 -0500

    Use XyoPayloadFilterPredicate in base archivist types

--- packages/core/model/src/Archivist/PayloadArchivist.ts
+++ packages/core/model/src/Archivist/PayloadArchivist.ts
@@ -1,12 +1,13 @@
-import { XyoPayloadFindFilter, XyoPayloadWithMeta } from '@xyo-network/sdk-xyo-client-js'
+import { XyoPayloadWithMeta } from '@xyo-network/sdk-xyo-client-js'
 
 import { Archivist } from './Archivist'
+import { XyoPayloadFilterPredicate } from './XyoPayloadFilterPredicate'
 
-export type PayloadArchivist<T extends object, TId = string> = Archivist<
+export type PayloadArchivist<T extends object, TId = string, TQuery = XyoPayloadFilterPredicate<Partial<T>>> = Archivist<
   XyoPayloadWithMeta<T>[],
   T[],
   XyoPayloadWithMeta<T>[],
   TId,
   XyoPayloadWithMeta<T>[],
-  XyoPayloadFindFilter
+  TQuery
 >
