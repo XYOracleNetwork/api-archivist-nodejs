ffa38d752308a682bcd0c0a1c3c4d279c4ad8e43
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-18 15:36:47 -0500

    Remove schema witnessing from Archivist

--- packages/express/routes/src/routes/archive/archive/schema/current/witness/getAllCachedSchema.ts
+++
@@ -1,8 +0,0 @@
-import { XyoSchemaCache, XyoSchemaCacheEntry } from '@xyo-network/sdk-xyo-client-js'
-
-export const getAllCachedSchema = async (): Promise<XyoSchemaCacheEntry[]> => {
-  // TODO: Enumerate all keys/entries within the schema cache
-  // and potentially filter out by just schema in this archive
-  await XyoSchemaCache.instance.get('')
-  return [] as XyoSchemaCacheEntry[]
-}
