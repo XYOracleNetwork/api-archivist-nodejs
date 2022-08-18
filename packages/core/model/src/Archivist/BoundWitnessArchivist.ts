ff340fd0664becc0f322143d7d62c8700a932f2c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-18 09:05:31 -0500

    Use XyoPayloadFilterPredicate in base archivist types

--- packages/core/model/src/Archivist/BoundWitnessArchivist.ts
+++ packages/core/model/src/Archivist/BoundWitnessArchivist.ts
@@ -1,5 +1,10 @@
 import { XyoBoundWitness } from '@xyo-network/sdk-xyo-client-js'
 
 import { PayloadArchivist } from './PayloadArchivist'
+import { XyoPayloadFilterPredicate } from './XyoPayloadFilterPredicate'
 
-export type BoundWitnessArchivist<TId = string> = PayloadArchivist<XyoBoundWitness, TId>
+export type BoundWitnessArchivist<TId = string, TQuery = XyoPayloadFilterPredicate<Partial<XyoBoundWitness>>> = PayloadArchivist<
+  XyoBoundWitness,
+  TId,
+  TQuery
+>
