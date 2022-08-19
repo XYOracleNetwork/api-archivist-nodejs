8e674fa18438e72611f5ebfb496df74056698d1c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-19 10:13:39 -0500

    Remove exported MongoDB helper method

--- packages/core/lib/src/dbSdk/getArchivistBoundWitnessesMongoSdk.ts
+++ packages/core/lib/src/dbSdk/getArchivistBoundWitnessesMongoSdk.ts
@@ -1,6 +1,5 @@
 import { assertEx } from '@xylabs/sdk-js'
-import { XyoArchivistBoundWitnessMongoSdk, XyoBoundWitness } from '@xyo-network/sdk-xyo-client-js'
-import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
+import { XyoArchivistBoundWitnessMongoSdk } from '@xyo-network/sdk-xyo-client-js'
 
 import { getMongoDBConfig } from './getMongoDBConfig'
 
@@ -17,16 +16,4 @@ export const getArchivistBoundWitnessesMongoSdk = (archive: string) => {
     },
     archive,
   )
-}
-
-export const getArchivistAllBoundWitnessesMongoSdk = () => {
-  const env = getMongoDBConfig()
-  return new BaseMongoSdk<XyoBoundWitness>({
-    collection: 'bound_witnesses',
-    dbConnectionString: env.MONGO_CONNECTION_STRING,
-    dbDomain: assertEx(env.MONGO_DOMAIN, 'Missing Mongo Domain'),
-    dbName: assertEx(env.MONGO_DATABASE, 'Missing Mongo Database'),
-    dbPassword: assertEx(env.MONGO_PASSWORD, 'Missing Mongo Password'),
-    dbUserName: assertEx(env.MONGO_USERNAME, 'Missing Mongo Username'),
-  })
 }
