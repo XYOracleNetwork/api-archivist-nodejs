2952582f103bc8ed48e15075485ad435aab5c766
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-20 14:11:59 -0500

    Refine model

--- packages/core/model/src/Diviner/BoundWitnessStatsDiviner.ts
+++ packages/core/model/src/Diviner/BoundWitnessStatsDiviner.ts
@@ -1,4 +1,4 @@
-import { XyoDiviner, XyoPayload, XyoQueryPayload } from '@xyo-network/sdk-xyo-client-js'
+import { ArchiveStatsQueryPayload, StatsDiviner, StatsPayload } from './StatsDiviner'
 
 export type BoundWitnessStatsSchema = 'network.xyo.archivist.boundwitness.stats'
 export const BoundWitnessStatsSchema: BoundWitnessStatsSchema = 'network.xyo.archivist.boundwitness.stats'
@@ -6,7 +6,7 @@ export const BoundWitnessStatsSchema: BoundWitnessStatsSchema = 'network.xyo.arc
 export type BoundWitnessStatsQuerySchema = 'network.xyo.archivist.boundwitness.stats.query'
 export const BoundWitnessStatsQuerySchema: BoundWitnessStatsQuerySchema = 'network.xyo.archivist.boundwitness.stats.query'
 
-export type BoundWitnessCountsPayload = XyoPayload<{ count: number; schema: BoundWitnessStatsSchema }>
-export type BoundWitnessCountsQueryPayload = XyoQueryPayload<{ archive?: string; schema: BoundWitnessStatsQuerySchema }>
+export type BoundWitnessStatsPayload = StatsPayload<{ schema: BoundWitnessStatsSchema }>
+export type BoundWitnessStatsQueryPayload = ArchiveStatsQueryPayload<{ schema: BoundWitnessStatsQuerySchema }>
 
-export type BoundWitnessCountsDiviner = XyoDiviner<BoundWitnessCountsQueryPayload>
+export type BoundWitnessStatsDiviner = StatsDiviner<BoundWitnessStatsQueryPayload>
--- packages/core/model/src/Diviner/CountsDiviner.ts
+++
@@ -1,6 +0,0 @@
-import { XyoDiviner, XyoPayload, XyoQueryPayload } from '@xyo-network/sdk-xyo-client-js'
-
-export type CountsPayload = XyoPayload<XyoPayload & { count: number }>
-export type CountsQueryPayload = XyoQueryPayload<XyoPayload & { archive?: string }>
-
-export type CountsDiviner = XyoDiviner<CountsQueryPayload>
