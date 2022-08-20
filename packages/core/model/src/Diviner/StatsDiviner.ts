955c0aeef5a74cee8e31ee0430681ceb9af2e7e5
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-20 14:11:59 -0500

    Refine model

---
+++ packages/core/model/src/Diviner/StatsDiviner.ts
@@ -0,0 +1,7 @@
+import { XyoDiviner, XyoPayload, XyoQueryPayload } from '@xyo-network/sdk-xyo-client-js'
+
+export type StatsPayload<T extends XyoPayload = XyoPayload> = XyoPayload<T & { count: number }>
+
+export type ArchiveStatsQueryPayload<T extends XyoQueryPayload = XyoQueryPayload> = XyoQueryPayload<T & { archive?: string }>
+
+export type StatsDiviner<T extends XyoQueryPayload = ArchiveStatsQueryPayload> = XyoDiviner<T>
