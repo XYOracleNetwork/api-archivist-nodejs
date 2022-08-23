516635daa9398927eca67e6f6e84b48026ad2e57
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-23 18:34:51 -0500

    Simplify multi-insertion

--- packages/core/mongo/src/Archivist/ArchivePermissions/MongoDBArchivePermissionsPayloadArchivist.ts
+++ packages/core/mongo/src/Archivist/ArchivePermissions/MongoDBArchivePermissionsPayloadArchivist.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
 
+import { assertEx } from '@xylabs/sdk-js'
 import {
   SetArchivePermissionsPayload,
   SetArchivePermissionsPayloadWithMeta,
@@ -7,12 +8,12 @@ import {
   setArchivePermissionsSchema,
 } from '@xyo-network/archivist-model'
 import { TYPES } from '@xyo-network/archivist-types'
-import { XyoAccount, XyoBoundWitness, XyoBoundWitnessBuilder, XyoPayloadBuilder, XyoPayloadFindFilter } from '@xyo-network/sdk-xyo-client-js'
+import { XyoAccount, XyoBoundWitness, XyoBoundWitnessBuilder, XyoPayloadFindFilter } from '@xyo-network/sdk-xyo-client-js'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { inject, injectable } from 'inversify'
 
 import { AbstractMongoDBPayloadArchivist } from '../../AbstractArchivist'
-import { getArchivistBoundWitnessesMongoSdk, getArchivistPayloadMongoSdk, removeId } from '../../Mongo'
+import { removeId } from '../../Mongo'
 import { MONGO_TYPES } from '../../types'
 
 const schema: SetArchivePermissionsSchema = setArchivePermissionsSchema
@@ -33,23 +34,23 @@ export class MongoDBArchivePermissionsPayloadPayloadArchivist extends AbstractMo
     return (await this.payloads.find({ _archive: archive, schema })).sort({ _timestamp: -1 }).limit(1).toArray()
   }
   async insert(items: SetArchivePermissionsPayloadWithMeta[]): Promise<SetArchivePermissionsPayloadWithMeta[]> {
-    const sanitized: SetArchivePermissionsPayloadWithMeta[] = items.map<SetArchivePermissionsPayloadWithMeta>(removeId)
-    for (let i = 0; i < sanitized.length; i++) {
-      const item = sanitized[i]
-      const _timestamp = Date.now()
-      const archive = item._archive
-      if (archive) {
-        const payload = new XyoPayloadBuilder<SetArchivePermissionsPayloadWithMeta>({ schema }).fields({ ...item, _timestamp }).build()
-        const bw = new XyoBoundWitnessBuilder(this.config).witness(this.account).payload(payload).build()
-        bw._archive = archive
-        const payloadResult = await this.payloads.insertOne(payload)
-        if (!payloadResult.acknowledged || !payloadResult.insertedId)
-          throw new Error('MongoDBArchivePermissionsPayloadPayloadArchivist: Error inserting Payload')
-        const bwResult = await this.boundWitnesses.insertOne(bw)
-        if (!bwResult.acknowledged || !bwResult.insertedId)
-          throw new Error('MongoDBArchivePermissionsPayloadPayloadArchivist: Error inserting BoundWitness')
-      }
-    }
+    const _timestamp = Date.now()
+    const payloads: SetArchivePermissionsPayloadWithMeta[] = items.map((p) => {
+      return { ...removeId(p), _archive: assertEx(p._archive, 'No archive supplied for SetArchivePermissionsPayloadWithMeta'), _timestamp }
+    })
+    const boundWitnesses: XyoBoundWitness[] = payloads.map((p) => {
+      const bw = new XyoBoundWitnessBuilder(this.config).witness(this.account).payload(p).build()
+      const _archive = p._archive
+      return { ...bw, _archive, _timestamp }
+    })
+    const payloadsResult = await this.payloads.insertMany(payloads)
+    if (!payloadsResult.acknowledged || payloadsResult.insertedCount !== payloads.length)
+      throw new Error('MongoDBArchivePermissionsPayloadPayloadArchivist: Error inserting Payloads')
+
+    const boundWitnessesResult = await this.boundWitnesses.insertMany(boundWitnesses)
+    if (!boundWitnessesResult.acknowledged || boundWitnessesResult.insertedCount !== boundWitnesses.length)
+      throw new Error('MongoDBArchivePermissionsPayloadPayloadArchivist: Error inserting BoundWitnesses')
+
     return items
   }
 }
