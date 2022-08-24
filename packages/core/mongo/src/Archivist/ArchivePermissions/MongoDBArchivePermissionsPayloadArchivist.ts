db2dbcf30022d0c7675880f5b11a3462cce4c9a0
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-23 20:03:40 -0500

    Better error messages

--- packages/core/mongo/src/Archivist/ArchivePermissions/MongoDBArchivePermissionsPayloadArchivist.ts
+++ packages/core/mongo/src/Archivist/ArchivePermissions/MongoDBArchivePermissionsPayloadArchivist.ts
@@ -8,7 +8,7 @@ import {
   setArchivePermissionsSchema,
 } from '@xyo-network/archivist-model'
 import { TYPES } from '@xyo-network/archivist-types'
-import { XyoAccount, XyoBoundWitness, XyoBoundWitnessBuilder, XyoPayloadFindFilter, XyoPayloadWrapper } from '@xyo-network/sdk-xyo-client-js'
+import { XyoAccount, XyoBoundWitness, XyoBoundWitnessBuilder, XyoBoundWitnessWithMeta, XyoPayloadFindFilter } from '@xyo-network/sdk-xyo-client-js'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { inject, injectable } from 'inversify'
 import { Filter } from 'mongodb'
@@ -24,7 +24,7 @@ export class MongoDBArchivePermissionsPayloadPayloadArchivist extends AbstractMo
   constructor(
     @inject(TYPES.Account) protected readonly account: XyoAccount,
     @inject(MONGO_TYPES.PayloadSdkMongo) protected readonly payloads: BaseMongoSdk<SetArchivePermissionsPayloadWithMeta>,
-    @inject(MONGO_TYPES.BoundWitnessSdkMongo) protected readonly boundWitnesses: BaseMongoSdk<XyoBoundWitness>,
+    @inject(MONGO_TYPES.BoundWitnessSdkMongo) protected readonly boundWitnesses: BaseMongoSdk<XyoBoundWitnessWithMeta>,
   ) {
     super()
   }
@@ -32,19 +32,27 @@ export class MongoDBArchivePermissionsPayloadPayloadArchivist extends AbstractMo
     throw new Error('Not Implemented')
   }
   async get(_archive: string): Promise<SetArchivePermissionsPayloadWithMeta[]> {
-    const addresses: string = this.account.addressValue.bn.toString('hex')
-    const filter: Filter<XyoBoundWitness> = {
-      _archive,
-      addresses,
-      payload_schemas: schema,
-    }
+    const addresses: string = assertEx(
+      this.account.addressValue.bn.toString('hex'),
+      'MongoDBArchivePermissionsPayloadPayloadArchivist: Invalid signing account address',
+    )
+    const filter: Filter<XyoBoundWitness> = { _archive, addresses, payload_schemas: schema }
     const boundWitnesses = await (await this.boundWitnesses.find(filter)).sort({ _timestamp: -1 }).limit(1).toArray()
     const lastWitnessedPermissions = boundWitnesses.pop()
     if (!lastWitnessedPermissions) return []
     const permissionsPayloadIndex = lastWitnessedPermissions.payload_schemas.findIndex((s) => s === schema)
-    assertEx(permissionsPayloadIndex > -1, 'Invalid permissions index')
-    const _hash = assertEx(lastWitnessedPermissions.payload_hashes[permissionsPayloadIndex], 'Missing hash')
-    const permissions = assertEx(await this.payloads.findOne({ _archive, _hash, schema }), 'Permissions hash missing')
+    assertEx(
+      permissionsPayloadIndex > -1,
+      `MongoDBArchivePermissionsPayloadPayloadArchivist: Invalid permissions index in BoundWitness (${lastWitnessedPermissions._hash})`,
+    )
+    const _hash = assertEx(
+      lastWitnessedPermissions.payload_hashes[permissionsPayloadIndex],
+      `MongoDBArchivePermissionsPayloadPayloadArchivist: Missing permissions payload hash in BoundWitness (${lastWitnessedPermissions._hash})`,
+    )
+    const permissions = assertEx(
+      await this.payloads.findOne({ _archive, _hash, schema }),
+      `MongoDBArchivePermissionsPayloadPayloadArchivist: Missing Payload (${_hash}) from BoundWitness (${lastWitnessedPermissions._hash})`,
+    )
     return [permissions]
   }
   async insert(items: SetArchivePermissionsPayloadWithMeta[]): Promise<SetArchivePermissionsPayloadWithMeta[]> {
@@ -52,7 +60,7 @@ export class MongoDBArchivePermissionsPayloadPayloadArchivist extends AbstractMo
     const payloads: SetArchivePermissionsPayloadWithMeta[] = items.map((p) => {
       return { ...removeId(p), _archive: assertEx(p._archive, 'No archive supplied for SetArchivePermissionsPayloadWithMeta'), _timestamp }
     })
-    const boundWitnesses: XyoBoundWitness[] = payloads.map((p) => {
+    const boundWitnesses: XyoBoundWitnessWithMeta[] = payloads.map((p) => {
       const bw = new XyoBoundWitnessBuilder(this.config).witness(this.account).payload(p).build()
       const _archive = p._archive
       return { ...bw, _archive, _timestamp }
