20af8f2c7766eef9cd62f5a06e6d4646ce2e159f
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-30 14:58:29 -0500

    Use health check as out of band init

---
+++ scripts/mongo/healthcheck/healthcheck.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+const config = {
+  _id: 'dbrs',
+  version: 1,
+  members: [
+      {
+          _id: 1,
+          host: '127.0.0.1:27017',
+          priority: 1
+      },
+  ]
+}
+rs.initiate(config, { force: false })
+rs.status()
