245d10e89421d044de20276f3b9dd0e08c0a4150
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-30 14:06:00 -0500

    Use config on boot

---
+++ scripts/mongo/docker-entrypoint-initdb.d/00-init-db.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+// const config = {
+//   _id: 'dbrs',
+//   version: 1,
+//   members: [
+//       {
+//           _id: 1,
+//           host: 'mongo:27017',
+//           priority: 1
+//       },
+//   ]
+// };
+// rs.initiate(config, { force: true });
+// rs.status();
+// // db.createUser({user: 'admin', pwd: 'admin', roles: [ { role: 'root', db: 'admin' } ]});
