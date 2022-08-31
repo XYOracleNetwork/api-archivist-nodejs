0cf5b0afb9078e0d0ef1a8981474c2b59b5db021
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-31 09:40:42 -0500

    Use constant for stats collection

--- packages/core/mongo/src/collections.ts
+++ packages/core/mongo/src/collections.ts
@@ -1,7 +1,9 @@
+// TODO: By DB
 export const COLLECTIONS = {
   ArchiveKeys: 'archive_keys',
   Archives: 'archives',
   BoundWitnesses: 'bound_witnesses',
   Payloads: 'payloads',
+  Stats: 'stats',
   Users: 'users',
 }
