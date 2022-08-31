a2a21320737f57155b73773e116c3983d6bbc3bd
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-31 07:51:03 -0500

    Register with change stream & error handling

--- packages/core/mongo/src/Diviner/PayloadStats/MongoDBArchivePayloadStatsDiviner.ts
+++ packages/core/mongo/src/Diviner/PayloadStats/MongoDBArchivePayloadStatsDiviner.ts
@@ -4,9 +4,17 @@ import { XyoAccount } from '@xyo-network/account'
 import { PayloadStatsPayload, PayloadStatsSchema } from '@xyo-network/archivist-model'
 import { TYPES } from '@xyo-network/archivist-types'
 import { XyoDiviner, XyoDivinerDivineQuerySchema } from '@xyo-network/diviner'
-import { XyoArchivistPayloadDivinerConfigSchema, XyoPayload, XyoPayloadBuilder, XyoPayloads, XyoPayloadSchema } from '@xyo-network/sdk-xyo-client-js'
+import {
+  XyoArchivistPayloadDivinerConfigSchema,
+  XyoPayload,
+  XyoPayloadBuilder,
+  XyoPayloads,
+  XyoPayloadSchema,
+  XyoPayloadWithMeta,
+} from '@xyo-network/sdk-xyo-client-js'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { inject, injectable } from 'inversify'
+import { ChangeStreamInsertDocument, ChangeStreamOptions, ResumeToken } from 'mongodb'
 
 import { MONGO_TYPES } from '../../types'
 import { MongoArchivePayload, MongoArchiveSchema } from '../MongoArchivePayload'
@@ -14,8 +22,11 @@ import { ArchiveConfigPayload } from '../Payloads'
 
 @injectable()
 export class MongoDBArchivePayloadStatsDiviner extends XyoDiviner<XyoPayload, ArchiveConfigPayload> {
+  protected resumeAfter: ResumeToken | undefined = undefined
+
   constructor(@inject(TYPES.Account) account: XyoAccount, @inject(MONGO_TYPES.PayloadSdkMongo) protected sdk: BaseMongoSdk<XyoPayload>) {
     super({ account, schema: XyoArchivistPayloadDivinerConfigSchema, targetSchema: XyoPayloadSchema })
+    this.registerWithChangeStream()
   }
 
   get queries() {
@@ -31,5 +42,29 @@ export class MongoDBArchivePayloadStatsDiviner extends XyoDiviner<XyoPayload, Ar
       : await this.sdk.useCollection((collection) => collection.estimatedDocumentCount())
     const result = new XyoPayloadBuilder<PayloadStatsPayload>({ schema: PayloadStatsSchema }).fields({ count }).build()
     return result
+  }
+
+  private processChange = (change: ChangeStreamInsertDocument<XyoPayloadWithMeta>) => {
+    console.log('processing change from change stream')
+    this.resumeAfter = change._id
+    const payload: XyoPayloadWithMeta = change.fullDocument
+    console.log(payload.schema)
+    // TODO: Increment counts in DB
+    console.log('processed change from change stream')
+  }
+
+  private registerWithChangeStream = () => {
+    console.log('registering with change stream')
+    void this.sdk?.useCollection((collection) => {
+      const opts: ChangeStreamOptions = this.resumeAfter ? { resumeAfter: this.resumeAfter } : {}
+      const changeStream = collection.watch([], opts)
+      changeStream.on('change', this.processChange)
+      changeStream.on('error', () => {
+        console.log('re-registering with change stream')
+        this.registerWithChangeStream
+        console.log('re-registered with change stream')
+      })
+      console.log('registered with change stream')
+    })
   }
 }
