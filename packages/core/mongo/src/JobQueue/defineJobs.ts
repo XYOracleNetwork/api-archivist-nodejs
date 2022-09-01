006f261bc0466c7639cbea9d3dc8582fb9edea0f
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-31 20:17:40 -0500

    Add optional job events

--- packages/core/mongo/src/JobQueue/defineJobs.ts
+++ packages/core/mongo/src/JobQueue/defineJobs.ts
@@ -6,5 +6,12 @@ import { DefineOptions } from 'agenda'
 const options: DefineOptions = { lockLifetime: 10000 }
 
 export const defineJobs = (jobQueue: JobQueue, jobs: Job[]) => {
-  jobs.map((job) => jobQueue.define(job.name, options, job.task))
+  jobs.map((job) => {
+    const { name, task } = job
+    jobQueue.define(name, options, task)
+    if (job.onComplete) jobQueue.on(`complete:${name}`, job.onComplete)
+    if (job.onFail) jobQueue.on(`fail:${name}`, job.onFail)
+    if (job.onStart) jobQueue.on(`start:${name}`, job.onStart)
+    if (job.onSuccess) jobQueue.on(`success:${name}`, job.onSuccess)
+  })
 }
