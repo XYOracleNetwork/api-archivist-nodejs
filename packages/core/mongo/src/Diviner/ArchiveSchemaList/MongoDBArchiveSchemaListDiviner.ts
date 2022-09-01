b13ca01daeafdab559b5f12dbf8cc03a2185fc15
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-01 09:52:02 -0500

    Map schemas into changes

--- packages/core/mongo/src/Diviner/ArchiveSchemaList/MongoDBArchiveSchemaListDiviner.ts
+++ packages/core/mongo/src/Diviner/ArchiveSchemaList/MongoDBArchiveSchemaListDiviner.ts
@@ -1,18 +1,26 @@
 import 'reflect-metadata'
 
+import { assertEx } from '@xylabs/sdk-js'
 import { ArchiveArchivist, ArchiveSchemaListDiviner, Job, JobProvider } from '@xyo-network/archivist-model'
 import { TYPES } from '@xyo-network/archivist-types'
-import { XyoPayload } from '@xyo-network/payload'
-import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
+import { XyoPayload, XyoPayloadWithMeta } from '@xyo-network/payload'
+import { BaseMongoSdk, MongoClientWrapper } from '@xyo-network/sdk-xyo-mongo-js'
 import { inject, injectable } from 'inversify'
+import { ChangeStreamInsertDocument, ChangeStreamOptions, ResumeToken, UpdateOptions } from 'mongodb'
 
+import { COLLECTIONS } from '../../collections'
+import { DATABASES } from '../../databases'
 import { MONGO_TYPES } from '../../types'
+
+const updateOptions: UpdateOptions = { upsert: true }
 
 // TODO: Make into Diviner via plugin interface
 @injectable()
 export class MongoDBArchiveSchemaListDiviner implements ArchiveSchemaListDiviner, JobProvider {
   protected readonly batchLimit = 100
   protected nextOffset = 0
+  protected pendingCounts: Record<string, Record<string, number>> = {}
+  protected resumeAfter: ResumeToken | undefined = undefined
 
   constructor(
     @inject(TYPES.ArchiveArchivist) protected archiveArchivist: ArchiveArchivist,
@@ -23,9 +31,11 @@ export class MongoDBArchiveSchemaListDiviner implements ArchiveSchemaListDiviner
     return [
       {
         name: 'MongoDBArchiveSchemaListDiviner.UpdateChanges',
-        onSuccess: () => async () => await Promise.resolve(),
+        onSuccess: () => {
+          this.pendingCounts = {}
+        },
         schedule: '1 minute',
-        task: async () => await Promise.resolve(),
+        task: async () => await this.updateChanges(),
       },
       {
         name: 'MongoDBArchiveSchemaListDiviner.DivineArchivesBatch',
@@ -39,5 +49,39 @@ export class MongoDBArchiveSchemaListDiviner implements ArchiveSchemaListDiviner
     return await this.sdk.useCollection((collection) => {
       return collection.distinct('schema', { _archive: archive })
     })
+  }
+
+  private processChange = (change: ChangeStreamInsertDocument<XyoPayloadWithMeta>) => {
+    this.resumeAfter = change._id
+    const archive = change.fullDocument._archive
+    const schema = change.fullDocument.schema
+    if (archive && schema) {
+      if (!this.pendingCounts[archive]) this.pendingCounts[archive] = {}
+      this.pendingCounts[archive][schema] = (this.pendingCounts[archive][schema] || 0) + 1
+    }
+  }
+
+  private registerWithChangeStream = async () => {
+    const wrapper = MongoClientWrapper.get(this.sdk.uri, this.sdk.config.maxPoolSize)
+    const connection = await wrapper.connect()
+    assertEx(connection, 'Connection failed')
+    const collection = connection.db(DATABASES.Archivist).collection(COLLECTIONS.Payloads)
+    const opts: ChangeStreamOptions = this.resumeAfter ? { resumeAfter: this.resumeAfter } : {}
+    const changeStream = collection.watch([], opts)
+    changeStream.on('change', this.processChange)
+    changeStream.on('error', this.registerWithChangeStream)
+  }
+
+  private updateChanges = async () => {
+    const updates = Object.keys(this.pendingCounts).map((archive) => {
+      const $inc = Object.keys(this.pendingCounts[archive]).map((schema) => {
+        return { [`schema.count.${schema}`]: this.pendingCounts[archive][schema] }
+      })
+      this.pendingCounts[archive] = {}
+      return this.sdk.useMongo(async (mongo) => {
+        await mongo.db(DATABASES.Archivist).collection(COLLECTIONS.ArchivistStats).updateOne({ archive }, { $inc }, updateOptions)
+      })
+    })
+    await Promise.allSettled(updates)
   }
 }
