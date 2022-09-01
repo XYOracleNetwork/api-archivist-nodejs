2de8d5e6641c209a5df92c10c92715a020898905
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-31 20:50:37 -0500

    Move interface to model package

--- packages/core/mongo/src/Job/getJobs.ts
+++ packages/core/mongo/src/Job/getJobs.ts
@@ -1,13 +1,10 @@
-import { Job } from '@xyo-network/archivist-model'
+import { Job, JobProvider } from '@xyo-network/archivist-model'
 import { TYPES } from '@xyo-network/archivist-types'
 import { Container } from 'inversify'
-
-interface JobProvider {
-  get jobs(): Job[]
-}
 
 export const getJobs = (container: Container): Job[] => {
   const boundWitnessStatsDiviner = container.get<JobProvider>(TYPES.BoundWitnessStatsDiviner)
   const payloadStatsDiviner = container.get<JobProvider>(TYPES.PayloadStatsDiviner)
-  return [...boundWitnessStatsDiviner.jobs, ...payloadStatsDiviner.jobs]
+  const jobProviders = [boundWitnessStatsDiviner, payloadStatsDiviner]
+  return jobProviders.flatMap((provider) => provider.jobs)
 }
