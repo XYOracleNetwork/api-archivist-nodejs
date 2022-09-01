f185c4d765438bdefd581c573c837b27901eceb3
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-31 19:06:50 -0500

    batch jobs

--- packages/core/mongo/src/Job/getJobs.ts
+++ packages/core/mongo/src/Job/getJobs.ts
@@ -1,5 +1,18 @@
-import { Job } from '@xyo-network/archivist-model'
+import { Job, Task } from '@xyo-network/archivist-model'
+import { TYPES } from '@xyo-network/archivist-types'
+import { Container } from 'inversify'
 
-export const getJobs = (): Job[] => {
-  return []
+interface TaskProvider {
+  get task(): Task
+}
+
+const schedule = '1 minute'
+
+export const getJobs = (container: Container): Job[] => {
+  const boundWitnessStatsDiviner = container.get<TaskProvider>(TYPES.BoundWitnessStatsDiviner)
+  const payloadStatsDiviner = container.get<TaskProvider>(TYPES.PayloadStatsDiviner)
+  return [
+    { name: 'boundWitnessStatsDiviner', schedule, task: boundWitnessStatsDiviner.task },
+    { name: 'payloadStatsDiviner', schedule, task: payloadStatsDiviner.task },
+  ]
 }
