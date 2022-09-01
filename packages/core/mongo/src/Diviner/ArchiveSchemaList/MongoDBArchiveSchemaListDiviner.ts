f280eeaf10863486be05b4bd54c2a9137809ec97
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-01 08:50:54 -0500

    Placeholder for archive schema lists jobs

--- packages/core/mongo/src/Diviner/ArchiveSchemaList/MongoDBArchiveSchemaListDiviner.ts
+++ packages/core/mongo/src/Diviner/ArchiveSchemaList/MongoDBArchiveSchemaListDiviner.ts
@@ -1,15 +1,40 @@
 import 'reflect-metadata'
 
-import { ArchiveSchemaListDiviner } from '@xyo-network/archivist-model'
+import { ArchiveArchivist, ArchiveSchemaListDiviner, Job, JobProvider } from '@xyo-network/archivist-model'
+import { TYPES } from '@xyo-network/archivist-types'
 import { XyoPayload } from '@xyo-network/payload'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { inject, injectable } from 'inversify'
 
 import { MONGO_TYPES } from '../../types'
 
+// TODO: Make into Diviner via plugin interface
 @injectable()
-export class MongoDBArchiveSchemaListDiviner implements ArchiveSchemaListDiviner {
-  constructor(@inject(MONGO_TYPES.PayloadSdkMongo) protected sdk: BaseMongoSdk<XyoPayload>) {}
+export class MongoDBArchiveSchemaListDiviner implements ArchiveSchemaListDiviner, JobProvider {
+  protected readonly batchLimit = 100
+  protected nextOffset = 0
+
+  constructor(
+    @inject(TYPES.ArchiveArchivist) protected archiveArchivist: ArchiveArchivist,
+    @inject(MONGO_TYPES.PayloadSdkMongo) protected sdk: BaseMongoSdk<XyoPayload>,
+  ) {}
+
+  get jobs(): Job[] {
+    return [
+      {
+        name: 'MongoDBArchiveSchemaListDiviner.UpdateChanges',
+        onSuccess: () => async () => await Promise.resolve(),
+        schedule: '1 minute',
+        task: async () => await Promise.resolve(),
+      },
+      {
+        name: 'MongoDBArchiveSchemaListDiviner.DivineArchivesBatch',
+        schedule: '10 minute',
+        task: async () => await Promise.resolve(),
+      },
+    ]
+  }
+
   async find(archive: string): Promise<string[]> {
     return await this.sdk.useCollection((collection) => {
       return collection.distinct('schema', { _archive: archive })
