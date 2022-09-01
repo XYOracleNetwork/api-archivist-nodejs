15e3f74e73ce199dd794e07896774467583d8acd
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-01 09:55:18 -0500

    Register job with job queue

--- packages/core/mongo/src/Job/getJobs.ts
+++ packages/core/mongo/src/Job/getJobs.ts
@@ -5,6 +5,7 @@ import { Container } from 'inversify'
 export const getJobs = (container: Container): Job[] => {
   const boundWitnessStatsDiviner = container.get<JobProvider>(TYPES.BoundWitnessStatsDiviner)
   const payloadStatsDiviner = container.get<JobProvider>(TYPES.PayloadStatsDiviner)
-  const jobProviders = [boundWitnessStatsDiviner, payloadStatsDiviner]
+  const archiveSchemaListDiviner = container.get<JobProvider>(TYPES.ArchiveSchemaListDiviner)
+  const jobProviders = [archiveSchemaListDiviner, boundWitnessStatsDiviner, payloadStatsDiviner]
   return jobProviders.flatMap((provider) => provider.jobs)
 }
