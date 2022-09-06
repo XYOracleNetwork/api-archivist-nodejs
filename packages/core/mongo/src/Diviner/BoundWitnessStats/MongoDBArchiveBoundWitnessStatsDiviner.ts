2a8a6d954955c83d26c121a46b8fb4385da92e8f
Arie Trouw <arie.trouw@xylabs.com>
2022-09-05 20:53:17 -0700

    new scripts and targetSchemas removed

--- packages/core/mongo/src/Diviner/BoundWitnessStats/MongoDBArchiveBoundWitnessStatsDiviner.ts
+++ packages/core/mongo/src/Diviner/BoundWitnessStats/MongoDBArchiveBoundWitnessStatsDiviner.ts
@@ -14,7 +14,7 @@ import {
 import { TYPES } from '@xyo-network/archivist-types'
 import { XyoBoundWitnessWithMeta } from '@xyo-network/boundwitness'
 import { XyoArchivistPayloadDivinerConfigSchema, XyoDiviner, XyoDivinerDivineQuerySchema } from '@xyo-network/diviner'
-import { XyoPayload, XyoPayloadBuilder, XyoPayloads, XyoPayloadSchema } from '@xyo-network/payload'
+import { XyoPayload, XyoPayloadBuilder, XyoPayloads } from '@xyo-network/payload'
 import { BaseMongoSdk, MongoClientWrapper } from '@xyo-network/sdk-xyo-mongo-js'
 import { inject, injectable } from 'inversify'
 import { ChangeStreamInsertDocument, ChangeStreamOptions, ResumeToken, UpdateOptions } from 'mongodb'
@@ -50,7 +50,7 @@ export class MongoDBArchiveBoundWitnessStatsDiviner
     @inject(TYPES.ArchiveArchivist) protected archiveArchivist: ArchiveArchivist,
     @inject(MONGO_TYPES.BoundWitnessSdkMongo) protected readonly sdk: BaseMongoSdk<XyoBoundWitnessWithMeta>,
   ) {
-    super({ account, schema: XyoArchivistPayloadDivinerConfigSchema, targetSchema: XyoPayloadSchema })
+    super({ account, schema: XyoArchivistPayloadDivinerConfigSchema })
     void this.registerWithChangeStream()
   }
 
