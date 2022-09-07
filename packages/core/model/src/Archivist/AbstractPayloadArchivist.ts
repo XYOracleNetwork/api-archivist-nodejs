f6e165ade52612e792f7710120d805f866dba169
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-06 20:44:28 -0500

    Depend on primitives not SDK types

--- packages/core/model/src/Archivist/AbstractPayloadArchivist.ts
+++ packages/core/model/src/Archivist/AbstractPayloadArchivist.ts
@@ -1,6 +1,5 @@
 import 'reflect-metadata'
 
-import { NullablePromisableArray, PromisableArray } from '@xyo-network/promisable'
 import { EmptyObject, XyoPayloadWithMeta } from '@xyo-network/sdk-xyo-client-js'
 import { injectable } from 'inversify'
 
@@ -9,7 +8,7 @@ import { XyoPayloadFilterPredicate } from './XyoPayloadFilterPredicate'
 
 @injectable()
 export abstract class AbstractPayloadArchivist<T extends EmptyObject = EmptyObject, TId = string> implements PayloadArchivist<T, TId> {
-  abstract find(filter: XyoPayloadFilterPredicate<T>): PromisableArray<XyoPayloadWithMeta<T>>
-  abstract get(id: TId[]): NullablePromisableArray<XyoPayloadWithMeta<T>>
-  abstract insert(items: T[]): PromisableArray<XyoPayloadWithMeta<T>>
+  abstract find(filter: XyoPayloadFilterPredicate<T>): Promise<XyoPayloadWithMeta<T>[]>
+  abstract get(id: TId[]): Promise<Array<XyoPayloadWithMeta<T> | null>>
+  abstract insert(items: T[]): Promise<XyoPayloadWithMeta<T>[]>
 }
