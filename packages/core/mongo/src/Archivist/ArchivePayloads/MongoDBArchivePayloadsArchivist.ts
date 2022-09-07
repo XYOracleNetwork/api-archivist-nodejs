a70229dddb770ce2d6c0c02b6a8e65d248a4a54b
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-06 19:48:24 -0500

    Move MongoDB archivists over to SDK interface

--- packages/core/mongo/src/Archivist/ArchivePayloads/MongoDBArchivePayloadsArchivist.ts
+++ packages/core/mongo/src/Archivist/ArchivePayloads/MongoDBArchivePayloadsArchivist.ts
@@ -2,6 +2,7 @@ import { assertEx } from '@xylabs/sdk-js'
 import { ArchivePayloadsArchivist, ArchivePayloadsArchivistId, XyoArchivePayloadFilterPredicate } from '@xyo-network/archivist-model'
 import { EmptyObject } from '@xyo-network/core'
 import { XyoPayloadWithMeta } from '@xyo-network/payload'
+import { PromisableArray } from '@xyo-network/promisable'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { inject, injectable } from 'inversify'
 import { Filter, SortDirection } from 'mongodb'
@@ -29,7 +30,9 @@ export class MongoDBArchivePayloadsArchivist implements ArchivePayloadsArchivist
     if (schemas?.length) filter.schema = { $in: schemas }
     return (await this.sdk.find(filter)).sort(sort).limit(parsedLimit).maxTimeMS(2000).toArray()
   }
-  async get(id: ArchivePayloadsArchivistId): Promise<XyoPayloadWithMeta[]> {
+  async get(ids: ArchivePayloadsArchivistId[]): Promise<XyoPayloadWithMeta[]> {
+    assertEx(ids.length === 1, 'Retrieval of multiple Payloads not supported')
+    const id = assertEx(ids.pop(), 'Missing id')
     const predicate = { _archive: assertEx(id.archive), _hash: assertEx(id.hash) }
     return (await this.sdk.find(predicate)).limit(1).toArray()
   }
