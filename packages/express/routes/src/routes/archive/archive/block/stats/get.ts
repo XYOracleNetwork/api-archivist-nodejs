8a0e67b90a36b9f90d99966054aec51850692de1
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-09 07:08:01 -0500

    Identity functions for query types

--- packages/express/routes/src/routes/archive/archive/block/stats/get.ts
+++ packages/express/routes/src/routes/archive/archive/block/stats/get.ts
@@ -1,9 +1,14 @@
 import 'source-map-support/register'
 
 import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
-import { ArchivePathParams, BoundWitnessStatsPayload, BoundWitnessStatsSchema } from '@xyo-network/archivist-model'
+import {
+  ArchivePathParams,
+  BoundWitnessStatsPayload,
+  BoundWitnessStatsQueryPayload,
+  BoundWitnessStatsQuerySchema,
+  BoundWitnessStatsSchema,
+} from '@xyo-network/archivist-model'
 import { XyoDivinerDivineQuerySchema } from '@xyo-network/diviner'
-import { XyoPayload } from '@xyo-network/payload'
 import { RequestHandler } from 'express'
 
 const unknownCount: BoundWitnessStatsPayload = { count: -1, schema: BoundWitnessStatsSchema }
@@ -15,14 +20,8 @@ export interface GetArchiveBlockStats {
 const handler: RequestHandler<ArchivePathParams, GetArchiveBlockStats> = async (req, res) => {
   const { archive } = req.params
   const { boundWitnessStatsDiviner: diviner } = req.app
-  const payload: XyoPayload<{ archive: string }> = {
-    archive,
-    schema: 'xyo.network.mongo.archive',
-  }
-  const result = await diviner.query({
-    payloads: [payload],
-    schema: XyoDivinerDivineQuerySchema,
-  })
+  const payloads: BoundWitnessStatsQueryPayload[] = [{ archive, schema: BoundWitnessStatsQuerySchema }]
+  const result = await diviner.query({ payloads, schema: XyoDivinerDivineQuerySchema })
   const answer: BoundWitnessStatsPayload = (result[1].pop() as BoundWitnessStatsPayload) || unknownCount
   res.json(answer)
 }
