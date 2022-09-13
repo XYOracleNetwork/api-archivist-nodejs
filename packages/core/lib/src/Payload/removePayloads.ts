1bbcfa3f043a1418f9f9bda90d4775e7d45fa8e9
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-12 21:03:41 -0500

    Be specific about type

--- packages/core/lib/src/Payload/removePayloads.ts
+++ packages/core/lib/src/Payload/removePayloads.ts
@@ -1,6 +1,6 @@
 import { XyoBoundWitnessWithMeta } from '@xyo-network/boundwitness'
 
-export const removePayloads = (boundWitnesses: XyoBoundWitnessWithMeta[]): XyoBoundWitnessWithMeta[] => {
+export const removePayloads = (boundWitnesses: XyoBoundWitnessWithMeta[]): Omit<XyoBoundWitnessWithMeta, '_payloads'>[] => {
   return boundWitnesses.map((boundWitness) => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { _payloads, ...sanitized } = boundWitness
