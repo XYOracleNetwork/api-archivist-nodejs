105a0908b53238a7d5b436e855494703fb157603
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-12 21:07:51 -0500

    Remove unused exports

--- packages/core/lib/src/Payload/augmentPayloadsWithMetadata.ts
+++
@@ -1,8 +0,0 @@
-import { XyoPayloadMeta, XyoPayloadWithMeta, XyoPayloadWithPartialMeta, XyoPayloadWrapper } from '@xyo-network/payload'
-
-export const augmentPayloadsWithMetadata = <T extends XyoPayloadWithPartialMeta>(payloads: T[], meta: XyoPayloadMeta): XyoPayloadWithMeta<T>[] => {
-  return payloads.map((payload) => {
-    const wrapper = new XyoPayloadWrapper(payload)
-    return { ...payload, ...meta, _hash: wrapper.hash }
-  })
-}
