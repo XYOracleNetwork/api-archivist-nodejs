fe66d50c90e63e4426a027be68ae4fc4da6596f7
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-12 20:39:16 -0500

    Handle BW or Payload

--- packages/express/routes/src/routes/post/formatRequest.ts
+++ packages/express/routes/src/routes/post/formatRequest.ts
@@ -1,9 +1,16 @@
 import { getRequestMeta } from '@xyo-network/archivist-express-lib'
-import { augmentBoundWitnessWithMetadata, augmentPayloadsWithMetadata } from '@xyo-network/archivist-lib'
-import { XyoBoundWitnessBuilder, XyoBoundWitnessWithMeta, XyoBoundWitnessWithPartialMeta } from '@xyo-network/boundwitness'
-import { XyoPayloadMeta, XyoPayloadWithPartialMeta } from '@xyo-network/payload'
+import { augmentWithMetadata } from '@xyo-network/archivist-lib'
+import {
+  XyoBoundWitnessBuilder,
+  XyoBoundWitnessBuilderConfig,
+  XyoBoundWitnessWithMeta,
+  XyoBoundWitnessWithPartialMeta,
+} from '@xyo-network/boundwitness'
+import { XyoPayloadWithPartialMeta } from '@xyo-network/payload'
 
 import { PostNodeRequest } from './PostNodeRequest'
+
+const config: XyoBoundWitnessBuilderConfig = { inlinePayloads: true }
 
 export const formatRequest = (req: PostNodeRequest): XyoBoundWitnessWithMeta[] => {
   const [boundWitnessMeta, payloadMeta] = getRequestMeta(req)
@@ -17,11 +24,11 @@ export const formatRequest = (req: PostNodeRequest): XyoBoundWitnessWithMeta[] =
         // to process payloads. We're witnessing them here as the pipeline
         // expects BWs but if we can modify the pipeline to accept BWs or
         // Payloads we can remove this overhead.
-        new XyoBoundWitnessBuilder({ inlinePayloads: true }).payload(x).build()
+        new XyoBoundWitnessBuilder(config).payload(x).build()
   })
-  return augmentBoundWitnessWithMetadata(
+  return augmentWithMetadata(
     boundWitnesses.map((bw) => {
-      bw._payloads = bw._payloads?.length ? augmentPayloadsWithMetadata(bw._payloads, payloadMeta as XyoPayloadMeta) : []
+      if (bw?._payloads && bw?._payloads?.length) bw._payloads = augmentWithMetadata(bw._payloads, payloadMeta)
       return bw
     }),
     boundWitnessMeta,
