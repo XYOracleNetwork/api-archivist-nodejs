7b8d8eccdaa7218a41aed84d401366521bc097ec
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-13 13:52:19 -0500

    Update MongoDBArchiveArchivist to conform to SDK

--- packages/core/mongo/src/Archivist/Archive/MongoDBArchiveArchivist.ts
+++ packages/core/mongo/src/Archivist/Archive/MongoDBArchiveArchivist.ts
@@ -2,7 +2,10 @@ import 'reflect-metadata'
 
 import { assertEx } from '@xylabs/assert'
 import { XyoArchive } from '@xyo-network/api'
+import { XyoArchivistQuery, XyoArchivistQuerySchema } from '@xyo-network/archivist'
 import { ArchiveArchivist, UpsertResult, XyoPayloadFilterPredicate } from '@xyo-network/archivist-model'
+import { XyoModuleQueryResult } from '@xyo-network/module'
+import { Promisable, PromisableArray } from '@xyo-network/promisable'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { inject, injectable } from 'inversify'
 import { Filter, SortDirection, WithId } from 'mongodb'
@@ -24,7 +27,12 @@ interface UpsertFilter {
 export class MongoDBArchiveArchivist implements ArchiveArchivist {
   constructor(@inject(MONGO_TYPES.ArchiveSdkMongo) protected archives: BaseMongoSdk<Required<XyoArchive>>) {}
 
-  async find(predicate: XyoPayloadFilterPredicate<XyoArchive>): Promise<Required<XyoArchive>[]> {
+  public get address(): string {
+    return 'TODO'
+  }
+
+  async find(predicate?: XyoPayloadFilterPredicate<XyoArchive>): Promise<Required<XyoArchive>[]> {
+    if (!predicate) return []
     const { archives, limit, offset, order, user } = predicate
     const parsedLimit = limit || 100
     const parsedOrder = order || 'desc'
@@ -43,7 +51,7 @@ export class MongoDBArchiveArchivist implements ArchiveArchivist {
     return [result]
   }
 
-  async insert(items: Array<Required<XyoArchive>>): Promise<Array<WithId<Required<XyoArchive> & UpsertResult>>> {
+  async insert(items: Required<XyoArchive>[]): Promise<WithId<Required<XyoArchive>> & UpsertResult> {
     return await this.archives.useCollection(async (collection) => {
       assertEx(items.length === 1, 'Insertion of multiple archives not supported')
       const item = assertEx(items.pop(), 'Missing archive')
@@ -55,9 +63,19 @@ export class MongoDBArchiveArchivist implements ArchiveArchivist {
       const result = await collection.findOneAndUpdate(filter, { $set: item }, { returnDocument: 'after', upsert: true })
       if (result.ok && result.value) {
         const updated = !!result?.lastErrorObject?.updatedExisting || false
-        return [{ ...result.value, updated }]
+        return { ...result.value, updated }
       }
       throw new Error('Insert Failed')
     })
+  }
+
+  public queries(): XyoArchivistQuerySchema[] {
+    return []
+  }
+  public query(_query: XyoArchivistQuery): Promisable<XyoModuleQueryResult> {
+    throw new Error('')
+  }
+  public queryable(_schema: string): boolean {
+    return false
   }
 }
