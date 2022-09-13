b0ee29bc4abaf42903b12b85d30b5c67de4b224a
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-13 12:30:23 -0500

    Update Payload Archivist to use module system

--- packages/core/model/src/Archivist/AbstractPayloadArchivist.ts
+++ packages/core/model/src/Archivist/AbstractPayloadArchivist.ts
@@ -1,15 +1,64 @@
 import 'reflect-metadata'
 
+import {
+  XyoArchivistConfig,
+  XyoArchivistFindQuerySchema,
+  XyoArchivistGetQuerySchema,
+  XyoArchivistInsertQuerySchema,
+  XyoArchivistQuery,
+  XyoArchivistQuerySchema,
+} from '@xyo-network/archivist'
+import { XyoBoundWitness } from '@xyo-network/boundwitness'
 import { EmptyObject } from '@xyo-network/core'
-import { XyoPayloadWithMeta } from '@xyo-network/payload'
+import { XyoModule, XyoModuleQueryResult } from '@xyo-network/module'
+import { XyoPayload, XyoPayloadWithMeta } from '@xyo-network/payload'
+import { Promisable } from '@xyo-network/promisable'
 import { injectable } from 'inversify'
 
 import { PayloadArchivist } from './PayloadArchivist'
 import { XyoPayloadFilterPredicate } from './XyoPayloadFilterPredicate'
 
 @injectable()
-export abstract class AbstractPayloadArchivist<T extends EmptyObject = EmptyObject, TId = string> implements PayloadArchivist<T, TId> {
+export abstract class AbstractPayloadArchivist<T extends EmptyObject = EmptyObject>
+  extends XyoModule<XyoArchivistConfig, XyoArchivistQuery>
+  implements PayloadArchivist<T>
+{
+  abstract get address(): string
+
+  public override queries(): XyoArchivistQuerySchema[] {
+    return [
+      XyoArchivistFindQuerySchema,
+      XyoArchivistGetQuerySchema,
+      XyoArchivistInsertQuerySchema,
+      // TODO: Support initialize, etc.
+      // XyoModuleInitializeQuerySchema,
+      // XyoModuleShutdownQuerySchema,
+    ]
+  }
+
+  async query(query: XyoArchivistQuery): Promise<XyoModuleQueryResult> {
+    if (!this.queries().find((schema) => schema === query.schema)) {
+      console.error(`Undeclared Module Query: ${query.schema}`)
+    }
+
+    const payloads: (XyoPayload | null)[] = []
+    switch (query.schema) {
+      case XyoArchivistFindQuerySchema:
+        if (query.filter) payloads.push(...(await this.find(query.filter as XyoPayloadFilterPredicate<T>)))
+        break
+      case XyoArchivistGetQuerySchema:
+        payloads.push(...(await this.get(query.hashes)))
+        break
+      case XyoArchivistInsertQuerySchema:
+        payloads.push(await this.insert(query.payloads as XyoPayload<T>[]), ...query.payloads)
+        break
+      default:
+        throw new Error(`${query.schema} Not Implemented`)
+    }
+    return [this.bindPayloads(payloads), payloads]
+  }
+
   abstract find(filter: XyoPayloadFilterPredicate<T>): Promise<XyoPayloadWithMeta<T>[]>
-  abstract get(id: TId[]): Promise<Array<XyoPayloadWithMeta<T> | null>>
-  abstract insert(items: T[]): Promise<XyoPayloadWithMeta<T>[]>
+  abstract get(id: string[]): Promise<Array<XyoPayloadWithMeta<T> | null>>
+  abstract insert(items: XyoPayload<T>[]): Promisable<XyoBoundWitness | null>
 }
