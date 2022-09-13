78959d7e0a891fea26dfe8edcd1b01407edcbe55
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-13 12:50:51 -0500

    Make Query ID generic

--- packages/core/model/src/Archivist/BoundWitnessArchivist.ts
+++ packages/core/model/src/Archivist/BoundWitnessArchivist.ts
@@ -3,12 +3,12 @@ import { Archivist, XyoArchivistQuery } from '@xyo-network/sdk-xyo-client-js'
 
 import { XyoBoundWitnessFilterPredicate } from './XyoBoundWitnessFilterPredicate'
 
-export type BoundWitnessArchivist = Archivist<
+export type BoundWitnessArchivist<TId = string> = Archivist<
   XyoBoundWitnessWithMeta | null,
   XyoBoundWitness | null,
   XyoBoundWitness,
   XyoBoundWitnessWithMeta | null,
   XyoBoundWitnessFilterPredicate,
-  string,
+  TId,
   XyoArchivistQuery
 >
