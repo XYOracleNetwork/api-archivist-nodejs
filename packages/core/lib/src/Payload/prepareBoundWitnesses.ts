9d5eceef2e86fd3d6bace7cfe60c0667bcd94aec
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-12 20:11:30 -0500

    Accept BoundWitnesses or Payloads for query

--- packages/core/lib/src/Payload/prepareBoundWitnesses.ts
+++ packages/core/lib/src/Payload/prepareBoundWitnesses.ts
@@ -1,7 +1,7 @@
 import 'source-map-support/register'
 
-import { XyoBoundWitnessMeta, XyoBoundWitnessWithMeta } from '@xyo-network/boundwitness'
-import { XyoPayload, XyoPayloadMeta, XyoPayloadWithMeta, XyoPayloadWrapper } from '@xyo-network/payload'
+import { XyoBoundWitnessMeta, XyoBoundWitnessWithMeta, XyoBoundWitnessWithPartialMeta } from '@xyo-network/boundwitness'
+import { XyoPayloadMeta, XyoPayloadWithMeta, XyoPayloadWithPartialMeta, XyoPayloadWrapper } from '@xyo-network/payload'
 
 export interface PrepareBoundWitnessesResult {
   payloads: XyoPayloadWithMeta[]
@@ -9,12 +9,12 @@ export interface PrepareBoundWitnessesResult {
 }
 
 export const prepareBoundWitnesses = (
-  boundWitnesses: XyoBoundWitnessWithMeta[],
+  boundWitnesses: XyoBoundWitnessWithPartialMeta[],
   boundWitnessMetaData: XyoBoundWitnessMeta,
   payloadMetaData: XyoPayloadMeta,
 ): PrepareBoundWitnessesResult => {
-  const sanitized = removePayloads(augmentWithMetadata(boundWitnesses, boundWitnessMetaData))
-  const payloads = augmentWithMetadata(
+  const sanitized = removePayloads(augmentPayloadsWithMetadata(boundWitnesses, boundWitnessMetaData))
+  const payloads = augmentPayloadsWithMetadata(
     boundWitnesses.map((bw) => bw._payloads || []).flatMap((p) => p),
     payloadMetaData,
   )
@@ -29,9 +29,19 @@ export const removePayloads = (boundWitnesses: XyoBoundWitnessWithMeta[]): XyoBo
   })
 }
 
-export const augmentWithMetadata = <T extends XyoPayload>(payloads: T[], payloadMetaData: XyoPayloadMeta): XyoPayloadWithMeta<T>[] => {
+export const augmentBoundWitnessWithMetadata = <T extends XyoBoundWitnessWithPartialMeta>(
+  boundWitnesses: T[],
+  meta: XyoBoundWitnessMeta,
+): XyoPayloadWithMeta<T>[] => {
+  return boundWitnesses.map((payload) => {
+    const wrapper = new XyoPayloadWrapper(payload)
+    return { ...payload, ...meta, _hash: wrapper.hash }
+  })
+}
+
+export const augmentPayloadsWithMetadata = <T extends XyoPayloadWithPartialMeta>(payloads: T[], meta: XyoPayloadMeta): XyoPayloadWithMeta<T>[] => {
   return payloads.map((payload) => {
     const wrapper = new XyoPayloadWrapper(payload)
-    return { ...payload, ...payloadMetaData, _hash: wrapper.hash }
+    return { ...payload, ...meta, _hash: wrapper.hash }
   })
 }
