52d95af250f3c00ae9b62724e922d164d0ef6e93
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-12 20:15:04 -0500

    Simplify types

--- packages/express/routes/src/routes/post/PostNodeRequestHandler.ts
+++ packages/express/routes/src/routes/post/PostNodeRequestHandler.ts
@@ -1,6 +1,6 @@
 import { ArchivePathParams } from '@xyo-network/archivist-model'
-import { XyoBoundWitness } from '@xyo-network/boundwitness'
-import { XyoPayload } from '@xyo-network/payload'
 import { RequestHandler } from 'express'
 
-export type PostNodeRequestHandler = RequestHandler<ArchivePathParams, string[][], XyoPayload | XyoBoundWitness | XyoPayload[] | XyoBoundWitness[]>
+import { PostNodeRequestBody } from './PostNodeRequestBody'
+
+export type PostNodeRequestHandler = RequestHandler<ArchivePathParams, string[][], PostNodeRequestBody>
