2d1da073177a2035ecd6426a7b7be26f62ac163c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-13 20:42:19 -0500

    Add Initializable Modules to DI

--- packages/core/types/src/index.ts
+++ packages/core/types/src/index.ts
@@ -1,11 +1,13 @@
 import { ARCHIVIST_TYPES } from './archivist'
 import { DIVINER_TYPES } from './diviner'
+import { MODULE_TYPES } from './module'
 
 export const TYPES = {
-  Account: Symbol('Account'),
-  ApiKey: Symbol('ApiKey'),
   ...ARCHIVIST_TYPES,
   ...DIVINER_TYPES,
+  ...MODULE_TYPES,
+  Account: Symbol('Account'),
+  ApiKey: Symbol('ApiKey'),
   JobQueue: Symbol('JobQueue'),
   JwtSecret: Symbol('JwtSecret'),
   Logger: Symbol('Logger'),
