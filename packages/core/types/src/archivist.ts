81b384ad0db8007c04ffaca580a4e4f1ea7ad0af
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-13 20:16:42 -0500

    Modularize type constants

---
+++ packages/core/types/src/archivist.ts
@@ -0,0 +1,11 @@
+export const ARCHIVIST_TYPES = {
+  ArchiveArchivist: Symbol('ArchiveArchivist'),
+  ArchiveBoundWitnessesArchivist: Symbol('ArchiveBoundWitnessesArchivist'),
+  ArchiveKeyArchivist: Symbol('ArchiveKeyArchivist'),
+  ArchivePayloadsArchivist: Symbol('ArchivePayloadsArchivist'),
+  ArchivePermissionsArchivist: Symbol('ArchivePermissionsArchivist'),
+  BoundWitnessesArchivist: Symbol('BoundWitnessesArchivist'),
+  PayloadsArchivist: Symbol('PayloadsArchivist'),
+  UserArchivist: Symbol('UserArchivist'),
+  WitnessedPayloadArchivist: Symbol('WitnessedPayloadArchivist'),
+}
