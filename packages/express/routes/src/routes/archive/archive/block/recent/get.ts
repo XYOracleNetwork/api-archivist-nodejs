c28a3b4795763be1459f6a5183fe3738b098367c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-19 09:47:05 -0500

    Find archive payloads via module query

--- packages/express/routes/src/routes/archive/archive/block/recent/get.ts
+++ packages/express/routes/src/routes/archive/archive/block/recent/get.ts
@@ -4,6 +4,7 @@ import { assertEx } from '@xylabs/assert'
 import { asyncHandler, tryParseInt } from '@xylabs/sdk-api-express-ecs'
 import { XyoArchivistFindQuery, XyoArchivistFindQuerySchema } from '@xyo-network/archivist'
 import { ArchiveBoundWitnessesArchivist, XyoArchiveBoundWitnessFilterPredicate } from '@xyo-network/archivist-model'
+import { XyoBoundWitness } from '@xyo-network/boundwitness'
 import { RequestHandler } from 'express'
 
 import { BlockRecentPathParams } from './BlockRecentPathParams'
@@ -17,12 +18,12 @@ const getBoundWitnesses = (archivist: ArchiveBoundWitnessesArchivist, archive: s
   return archivist.query(query)
 }
 
-const handler: RequestHandler<BlockRecentPathParams> = async (req, res) => {
+const handler: RequestHandler<BlockRecentPathParams, (XyoBoundWitness | null)[]> = async (req, res) => {
   const { archive, limit } = req.params
   const { archiveBoundWitnessesArchivist: archivist } = req.app
   const limitNumber = tryParseInt(limit) ?? 20
   assertEx(limitNumber > 0 && limitNumber <= 100, 'limit must be between 1 and 100')
-  const boundWitnesses = (await getBoundWitnesses(archivist, archive, limitNumber))?.[1]
+  const boundWitnesses = (await getBoundWitnesses(archivist, archive, limitNumber))?.[1] as (XyoBoundWitness | null)[]
   res.json(boundWitnesses)
 }
 
