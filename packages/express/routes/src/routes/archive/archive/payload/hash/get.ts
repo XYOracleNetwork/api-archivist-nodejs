ba6bf20910bf7fa40f064bda9bbfbe1ca64fb136
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-19 09:22:12 -0500

    Get archive payload by module query

--- packages/express/routes/src/routes/archive/archive/payload/hash/get.ts
+++ packages/express/routes/src/routes/archive/archive/payload/hash/get.ts
@@ -2,6 +2,7 @@ import 'source-map-support/register'
 
 import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
 import { exists } from '@xylabs/sdk-js'
+import { XyoArchivistGetQuery, XyoArchivistGetQuerySchema } from '@xyo-network/archivist'
 import { XyoPayload, XyoPayloadWrapper } from '@xyo-network/payload'
 import { RequestHandler } from 'express'
 
@@ -9,9 +10,14 @@ import { PayloadHashPathParams } from '../payloadHashPathParams'
 
 const handler: RequestHandler<PayloadHashPathParams, XyoPayload[]> = async (req, res) => {
   const { archive, hash } = req.params
-  const { archivePayloadsArchivist } = req.app
-  const payloads = (await archivePayloadsArchivist.get([{ archive, hash }])) ?? []
-  res.json(payloads.filter(exists).map((payload) => new XyoPayloadWrapper(payload).body))
+  const { archivePayloadsArchivist: archivist } = req.app
+  const query: XyoArchivistGetQuery = {
+    hashes: [{ archive, hash }] as unknown as string[],
+    schema: XyoArchivistGetQuerySchema,
+  }
+  const result = await archivist.query(query)
+  const payload = result?.[1].filter(exists).map((payload) => new XyoPayloadWrapper(payload).body)?.[0]
+  res.json(payload ? [payload] : [])
 }
 
 export const getArchivePayloadHash = asyncHandler(handler)
