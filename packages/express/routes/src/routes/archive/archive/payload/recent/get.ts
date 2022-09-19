6e8c8e6192389148ec0d312a967f945c9d629a08
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-19 09:50:32 -0500

    Find recent payloads via module query

--- packages/express/routes/src/routes/archive/archive/payload/recent/get.ts
+++ packages/express/routes/src/routes/archive/archive/payload/recent/get.ts
@@ -2,25 +2,30 @@ import 'source-map-support/register'
 
 import { assertEx } from '@xylabs/assert'
 import { asyncHandler, tryParseInt } from '@xylabs/sdk-api-express-ecs'
-import { exists } from '@xylabs/sdk-js'
+import { XyoArchivistFindQuery, XyoArchivistFindQuerySchema } from '@xyo-network/archivist'
 import { XyoArchivePayloadFilterPredicate } from '@xyo-network/archivist-model'
 import { XyoPayload } from '@xyo-network/payload'
 import { RequestHandler } from 'express'
 
 import { PayloadRecentPathParams } from './payloadRecentPathParams'
 
-const handler: RequestHandler<PayloadRecentPathParams, XyoPayload[]> = async (req, res) => {
+const handler: RequestHandler<PayloadRecentPathParams, (XyoPayload | null)[]> = async (req, res) => {
   const { archive, limit } = req.params
-  const { archivePayloadsArchivist } = req.app
+  const { archivePayloadsArchivist: archivist } = req.app
   const limitNumber = tryParseInt(limit) ?? 20
   assertEx(limitNumber > 0 && limitNumber <= 100, 'limit must be between 1 and 100')
-  const predicate: XyoArchivePayloadFilterPredicate<XyoPayload> = {
+  const filter: XyoArchivePayloadFilterPredicate<XyoPayload> = {
     archive,
     limit: limitNumber,
     order: 'desc',
   }
-  const payloads = await archivePayloadsArchivist.find(predicate)
-  res.json(payloads.filter(exists))
+  const query: XyoArchivistFindQuery = {
+    filter,
+    schema: XyoArchivistFindQuerySchema,
+  }
+  const result = await archivist.query(query)
+  const payloads = result?.[1]
+  res.json(payloads)
 }
 
 export const getArchivePayloadRecent = asyncHandler(handler)
