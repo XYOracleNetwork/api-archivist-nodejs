5f8796a1632a3dd97d27a6fbd1f0acef967c753e
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-19 09:02:21 -0500

    Get archive BW from module query

--- packages/express/routes/src/routes/archive/archive/block/hash/get.ts
+++ packages/express/routes/src/routes/archive/archive/block/hash/get.ts
@@ -1,7 +1,7 @@
 import 'source-map-support/register'
 
 import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
-import { exists } from '@xylabs/sdk-js'
+import { XyoArchivistGetQuery, XyoArchivistGetQuerySchema } from '@xyo-network/archivist'
 import { scrubBoundWitnesses } from '@xyo-network/archivist-lib'
 import { XyoBoundWitness } from '@xyo-network/boundwitness'
 import { RequestHandler } from 'express'
@@ -10,9 +10,14 @@ import { BlockHashPathParams } from './blockHashPathParams'
 
 const handler: RequestHandler<BlockHashPathParams, XyoBoundWitness[]> = async (req, res) => {
   const { archive, hash } = req.params
-  const { archiveBoundWitnessesArchivist } = req.app
-  const result = await archiveBoundWitnessesArchivist.get([{ archive, hash }])
-  res.json(scrubBoundWitnesses(result.filter(exists)) ?? [])
+  const { archiveBoundWitnessesArchivist: archivist } = req.app
+  const query: XyoArchivistGetQuery = {
+    hashes: [{ archive, hash }] as unknown as string[],
+    schema: XyoArchivistGetQuerySchema,
+  }
+  const result = await archivist.query(query)
+  const block = result?.[1]?.[0] as unknown as XyoBoundWitness
+  res.json(scrubBoundWitnesses(block ? [block] : []))
 }
 
 export const getArchiveBlockHash = asyncHandler(handler)
