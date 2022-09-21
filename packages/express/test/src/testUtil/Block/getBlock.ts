82aae962cd7a61c55358902958f12342a13ae794
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-21 09:02:34 -0500

    Match tests to new builder format

--- packages/express/test/src/testUtil/Block/getBlock.ts
+++ packages/express/test/src/testUtil/Block/getBlock.ts
@@ -1,11 +1,15 @@
 import { assertEx } from '@xylabs/assert'
-import { XyoBoundWitnessBuilder } from '@xyo-network/boundwitness'
+import { XyoPayloadWithPartialMeta } from '@xyo-network/archivist-model'
+import { XyoBoundWitness, XyoBoundWitnessBuilder } from '@xyo-network/boundwitness'
 import { XyoPayload } from '@xyo-network/payload'
 
 import { unitTestSigningAccount } from '../Account'
 import { getPayloads, knownPayload } from '../Payload'
 
-export const knownBlock = new XyoBoundWitnessBuilder({ inlinePayloads: true }).witness(unitTestSigningAccount).payload(knownPayload).build()
+export const knownBlock = new XyoBoundWitnessBuilder({ inlinePayloads: true })
+  .witness(unitTestSigningAccount)
+  .payload(knownPayload)
+  .build() as XyoBoundWitness & XyoPayloadWithPartialMeta
 export const knownBlockHash = assertEx(knownBlock._hash)
 
 export const getBlock = (...payloads: XyoPayload[]) => {
