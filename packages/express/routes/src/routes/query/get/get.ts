e71bd1f9aadadd18644a08a924f57ad1237029f0
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-21 13:07:08 -0500

    Get response queue from server dependencies

--- packages/express/routes/src/routes/query/get/get.ts
+++ packages/express/routes/src/routes/query/get/get.ts
@@ -1,7 +1,4 @@
 import { asyncHandler, NoReqBody, NoReqQuery } from '@xylabs/sdk-api-express-ecs'
-import { dependencies } from '@xyo-network/archivist-dependencies'
-import { IdentifiableHuri, Queue } from '@xyo-network/archivist-queue'
-import { TYPES } from '@xyo-network/archivist-types'
 import { XyoPayload } from '@xyo-network/payload'
 import { RequestHandler } from 'express'
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
@@ -11,7 +8,8 @@ export type QueryPathParams = {
 }
 
 const handler: RequestHandler<QueryPathParams, XyoPayload, NoReqBody, NoReqQuery> = async (req, res, next) => {
-  const result = await dependencies.get<Queue<IdentifiableHuri>>(TYPES.ResponseQueue).get(req.params.id)
+  const { responseQueue } = req.app
+  const result = await responseQueue.get(req.params.id)
   if (result?.huri?.hash) {
     res.redirect(`/${result.huri?.hash}`)
     return
