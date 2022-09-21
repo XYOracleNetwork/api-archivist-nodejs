04c4181a5e53fe7628d40048050d4b2595538d22
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-21 15:49:14 -0500

    Update query handler to use module query

--- packages/core/model/src/Archivist/ArchivePermissionsArchivist.ts
+++ packages/core/model/src/Archivist/ArchivePermissionsArchivist.ts
@@ -1,7 +1,9 @@
-import { Archivist } from '@xyo-network/archivist'
+import { Archivist, XyoArchivistQuery } from '@xyo-network/archivist'
 import { XyoBoundWitness } from '@xyo-network/boundwitness'
+import { Module } from '@xyo-network/module'
 
 import { XyoPayloadWithPartialMeta } from '../Payload'
 import { SetArchivePermissions } from '../Query'
 
-export type ArchivePermissionsArchivist = Archivist<SetArchivePermissions, XyoBoundWitness | null, XyoPayloadWithPartialMeta<SetArchivePermissions>>
+export type ArchivePermissionsArchivist = Archivist<SetArchivePermissions, XyoBoundWitness | null, XyoPayloadWithPartialMeta<SetArchivePermissions>> &
+  Module<XyoArchivistQuery>
