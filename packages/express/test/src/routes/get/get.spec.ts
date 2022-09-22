46040fd5d28c6e06f5bbf256c9b2e9977aaa3307
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-21 20:00:11 -0500

    Return module info on root GET

--- packages/express/test/src/routes/get/get.spec.ts
+++ packages/express/test/src/routes/get/get.spec.ts
@@ -1,10 +1,26 @@
-import { StatusCodes } from 'http-status-codes'
+import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 
 import { request } from '../../testUtil'
 
 describe('/', () => {
-  it('returns addresses on Node', async () => {
+  it(`returns ${ReasonPhrases.OK}`, async () => {
     const result = await (await request()).get('/')
-    expect(result.status).toBe(StatusCodes.NOT_IMPLEMENTED)
+    expect(result.status).toBe(StatusCodes.OK)
+  })
+  it('returns the address for the default module', async () => {
+    const result = await (await request()).get('/')
+    const { address } = result.body.data
+    expect(address).toBeDefined()
+    expect(address).toBeString()
+  })
+  it('returns the supported queries for the default module', async () => {
+    const result = await (await request()).get('/')
+    const { queries } = result.body.data
+    expect(queries).toBeDefined()
+    expect(queries).toBeArray()
+    const expected = queries as string[]
+    expected.map((query) => {
+      expect(query).toBeString()
+    })
   })
 })
