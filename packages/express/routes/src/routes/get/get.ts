0f025779fb6da1c49923407c016fcebda1a70c3f
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-22 00:07:55 -0500

    update root to return node modules

--- packages/express/routes/src/routes/get/get.ts
+++ packages/express/routes/src/routes/get/get.ts
@@ -2,22 +2,54 @@
 import { NoReqParams } from '@xylabs/sdk-api-express-ecs'
 // import { setRawResponseFormat } from '@xyo-network/archivist-middleware'
 import { Module } from '@xyo-network/module'
-import { RequestHandler } from 'express'
+import { Request, RequestHandler } from 'express'
 
 interface NodeInfo {
   address: string
+  href: string
   queries: string[]
+  type: string
 }
 
-const infoFromModule = (module: Module<never>): NodeInfo => {
+const nodeInfoFromModule = (module: Module<never>, type: string): NodeInfo => {
   const { address, queries: getQueries } = module
   const queries = getQueries()
-  return { address, queries }
+  const href = `/${address}`
+  return { address, href, queries, type }
 }
 
-const handler: RequestHandler<NoReqParams, NodeInfo> = (req, res) => {
+type ActiveModules = Record<string, NodeInfo>
+
+interface NodeDescription {
+  archivists: ActiveModules
+  diviners: ActiveModules
+}
+
+const nodeDescription: NodeDescription = {
+  archivists: {},
+  diviners: {},
+}
+
+let populated = false
+
+const populateNodeDescription = (req: Request) => {
+  const { payloadsArchivist, boundWitnessesArchivist, schemaStatsDiviner, payloadStatsDiviner, boundWitnessStatsDiviner } = req.app
+  nodeDescription.archivists = {
+    boundWitnessesArchivist: nodeInfoFromModule(boundWitnessesArchivist, 'archivist'),
+    payloadsArchivist: nodeInfoFromModule(payloadsArchivist, 'archivist'),
+  }
+  nodeDescription.diviners = {
+    boundWitnessStatsDiviner: nodeInfoFromModule(boundWitnessStatsDiviner, 'diviner'),
+    payloadStatsDiviner: nodeInfoFromModule(payloadStatsDiviner, 'diviner'),
+    schemaStatsDiviner: nodeInfoFromModule(schemaStatsDiviner, 'diviner'),
+  }
+  populated = true
+}
+
+const handler: RequestHandler<NoReqParams> = (req, res) => {
   // setRawResponseFormat(res)
-  res.json(infoFromModule(req.app.payloadsArchivist))
+  if (!populated) populateNodeDescription(req)
+  res.json(nodeDescription)
 }
 
 // export const getNode = asyncHandler(handler)
