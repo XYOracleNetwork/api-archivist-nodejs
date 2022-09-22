086c7ff535c4db957bbdd00fa478c9608d614c87
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-21 20:00:11 -0500

    Return module info on root GET

--- packages/express/routes/src/routes/get/get.ts
+++ packages/express/routes/src/routes/get/get.ts
@@ -1,9 +1,21 @@
 // import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
+import { NoReqParams } from '@xylabs/sdk-api-express-ecs'
+import { Module } from '@xyo-network/module'
 import { RequestHandler } from 'express'
-import { StatusCodes } from 'http-status-codes'
 
-const handler: RequestHandler = (_req, res) => {
-  res.sendStatus(StatusCodes.NOT_IMPLEMENTED)
+interface NodeInfo {
+  address: string
+  queries: string[]
+}
+
+const infoFromModule = (module: Module<never>): NodeInfo => {
+  const { address, queries: getQueries } = module
+  const queries = getQueries()
+  return { address, queries }
+}
+
+const handler: RequestHandler<NoReqParams, NodeInfo> = (req, res) => {
+  res.json(infoFromModule(req.app.payloadsArchivist))
 }
 
 // export const getNode = asyncHandler(handler)
