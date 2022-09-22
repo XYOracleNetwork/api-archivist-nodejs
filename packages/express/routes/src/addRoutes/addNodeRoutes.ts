65276669de25102c2e3a582fbb0d44e5346d28be
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-22 13:10:20 -0500

    Remove spacing

--- packages/express/routes/src/addRoutes/addNodeRoutes.ts
+++ packages/express/routes/src/addRoutes/addNodeRoutes.ts
@@ -7,11 +7,9 @@ export const addNodeRoutes = (app: Express) => {
   app.get(
     '/',
     allowAnonymous,
-    getNode,
-    /* #swagger.tags = ['Node'] */
+    getNode /* #swagger.tags = ['Node'] */,
     /* #swagger.summary = 'Gets modules on the Node' */
   )
-
   app.post(
     '/:archive',
     archiveLocals,
@@ -20,7 +18,6 @@ export const addNodeRoutes = (app: Express) => {
     /* #swagger.tags = ['Node'] */
     /* #swagger.summary = 'Execute the supplied queries, contained as Payloads in one or more Bound Witnesses. Implementation is specific to the supplied payload schemas.' */
   )
-
   app.get(
     '/:address',
     allowAnonymous,
@@ -28,7 +25,6 @@ export const addNodeRoutes = (app: Express) => {
     /* #swagger.tags = ['Node'] */
     /* #swagger.summary = 'Get the module info for the supplied address' */
   )
-
   app.get(
     '/:hash',
     allowAnonymous,
@@ -36,7 +32,6 @@ export const addNodeRoutes = (app: Express) => {
     /* #swagger.tags = ['Node'] */
     /* #swagger.summary = 'Get the HURI from the archivist' */
   )
-
   app.get(
     '/query/:id',
     allowAnonymous,
