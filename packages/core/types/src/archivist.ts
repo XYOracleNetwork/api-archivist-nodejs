6c667eb70b087fd6c3da9bc7ae3c214373fddfc9
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-27 11:50:47 -0500

    Add archivist factories

--- packages/core/types/src/archivist.ts
+++ packages/core/types/src/archivist.ts
@@ -1,8 +1,10 @@
 export const ARCHIVIST_TYPES = {
   ArchiveArchivist: Symbol('ArchiveArchivist'),
   ArchiveBoundWitnessesArchivist: Symbol('ArchiveBoundWitnessesArchivist'),
+  ArchiveBoundWitnessesArchivistFactory: Symbol('ArchiveBoundWitnessesArchivistFactory'),
   ArchiveKeyArchivist: Symbol('ArchiveKeyArchivist'),
   ArchivePayloadsArchivist: Symbol('ArchivePayloadsArchivist'),
+  ArchivePayloadsArchivistFactory: Symbol('ArchivePayloadsArchivistFactory'),
   ArchivePermissionsArchivist: Symbol('ArchivePermissionsArchivist'),
   BoundWitnessesArchivist: Symbol('BoundWitnessesArchivist'),
   PayloadsArchivist: Symbol('PayloadsArchivist'),
