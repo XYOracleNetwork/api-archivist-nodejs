205b28a511d4a56a5771e3bf1d834fee1259b3f7
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-27 17:06:39 -0500

    Conform methods to SDK shape

--- packages/core/model/src/Archivist/XyoBoundWitnessFilterPredicate.ts
+++ packages/core/model/src/Archivist/XyoBoundWitnessFilterPredicate.ts
@@ -1,10 +1,8 @@
 import { XyoBoundWitness } from '@xyo-network/boundwitness'
 
-import { XyoArchivePayloadFilterPredicate, XyoPayloadFilterPredicate } from './XyoPayloadFilterPredicate'
+import { XyoPayloadFilterPredicate } from './XyoPayloadFilterPredicate'
 
 type WithoutSchema<T> = Omit<Omit<T, 'schema'>, 'schemas'>
 
 // TODO: Should we just accept "schema"/"schemas" here and infer that they mean "payload_schemas"?
 export type XyoBoundWitnessFilterPredicate = WithoutSchema<XyoPayloadFilterPredicate> & Partial<XyoBoundWitness>
-
-export type XyoArchiveBoundWitnessFilterPredicate = WithoutSchema<XyoArchivePayloadFilterPredicate> & Partial<XyoBoundWitness>
