03f5c0f792b6aec00fe5c3e6e093593b35b4f947
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-09-29 14:17:02 -0500

    Sample elevation diviner skeleton

--- packages/core/model/src/Diviner/index.ts
+++ packages/core/model/src/Diviner/index.ts
@@ -1,4 +1,5 @@
 export * from './BoundWitnessStatsDiviner'
+export * from './ElevationDiviner'
 export * from './PayloadStatsDiviner'
 export * from './SchemaStatsDiviner'
 export * from './StatsPayload'
