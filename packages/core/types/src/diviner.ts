77c23d883b2daca36382725ca75a402d33704210
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-03 07:45:07 -0500

    Revert "Remove in-memory module"
    This reverts commit c093c3ea14638154b50f43be9f0e3750d00326ff.

--- packages/core/types/src/diviner.ts
+++ packages/core/types/src/diviner.ts
@@ -1,7 +1,6 @@
 export const DIVINER_TYPES = {
   BoundWitnessStatsDiviner: Symbol('BoundWitnessStatsDiviner'),
   ElevationDiviner: Symbol('ElevationDiviner'),
-  ModuleAddressDiviner: Symbol('ModuleAddressDiviner'),
   PayloadStatsDiviner: Symbol('PayloadStatsDiviner'),
   SchemaStatsDiviner: Symbol('SchemaStatsDiviner'),
 }
