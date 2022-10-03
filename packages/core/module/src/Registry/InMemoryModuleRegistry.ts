f5142f994d01597f10eeefded1cd48e5f6fdb537
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-03 14:18:35 -0500

    Add additional methods to Module registry

--- packages/core/module/src/Registry/InMemoryModuleRegistry.ts
+++ packages/core/module/src/Registry/InMemoryModuleRegistry.ts
@@ -4,6 +4,7 @@ import LruCache from 'lru-cache'
 
 // TODO: Modules shouldn't age out but memory is a finite resource. Could
 // do something like re-initialize LRU modules.
+// TODO: Call destroy query on un-initialize
 /**
  * The number of registered modules to keep
  * in the cache
@@ -12,7 +13,6 @@ const max = 10000
 
 export class InMemoryModuleRegistry implements ModuleRegistry {
   public onModuleAdded?: (address: string, module: Module) => Promise<void>
-
   protected registry = new LruCache<string, Module>({ max })
 
   public async addModule(address: string, module: Module): Promise<void> {
@@ -25,5 +25,9 @@ export class InMemoryModuleRegistry implements ModuleRegistry {
   public getModule(address: string): Promise<Module | undefined> {
     const module = this.registry.get(address)
     return Promise.resolve(module)
+  }
+
+  public hasModule(address: string): Promise<boolean> {
+    return Promise.resolve(this.registry.has(address))
   }
 }
