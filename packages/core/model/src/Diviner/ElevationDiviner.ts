fa4ebb80259356719b4cc809c514c282305aa1ba
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-03 09:29:40 -0500

    Correct naming for Elevation diviner types

--- packages/core/model/src/Diviner/ElevationDiviner.ts
+++ packages/core/model/src/Diviner/ElevationDiviner.ts
@@ -5,11 +5,10 @@ import { XyoPayload } from '@xyo-network/payload'
 export type ElevationSchema = 'network.xyo.location.elevation'
 export const ElevationSchema: ElevationSchema = 'network.xyo.location.elevation'
 
-// TODO: Remove Archivist from name
-export type ArchivistElevationDivinerConfigSchema = 'network.xyo.location.elevation.config'
-export const ArchivistElevationDivinerConfigSchema: ArchivistElevationDivinerConfigSchema = 'network.xyo.location.elevation.config'
+export type ElevationDivinerConfigSchema = 'network.xyo.location.elevation.config'
+export const ElevationDivinerConfigSchema: ElevationDivinerConfigSchema = 'network.xyo.location.elevation.config'
 
-export type ArchivistElevationDivinerConfig<S extends string = string, T extends XyoPayload = XyoPayload> = XyoDivinerConfig<
+export type ElevationDivinerConfig<S extends string = string, T extends XyoPayload = XyoPayload> = XyoDivinerConfig<
   T & {
     schema: S
   }
