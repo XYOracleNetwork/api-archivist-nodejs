f717881ba39f03f2960aa72ef9c0281d6e71054b
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-13 17:28:59 -0500

    Convert Module Initialization to ContainerModule

--- packages/core/mongo/src/Initializable/addInitializables.ts
+++ packages/core/mongo/src/Initializable/addInitializables.ts
@@ -1,6 +1,6 @@
 import { exists } from '@xylabs/sdk-js'
 import { TYPES } from '@xyo-network/archivist-types'
-import { Container } from 'inversify'
+import { Container, ContainerModule, interfaces } from 'inversify'
 
 export const addInitializables = (container: Container) => {
   container.bind(TYPES.Initializable).toDynamicValue((context) => {
@@ -14,3 +14,16 @@ export const addInitializables = (container: Container) => {
     return initializables.filter(exists)
   })
 }
+
+export const initializables = new ContainerModule((bind: interfaces.Bind, _unbind: interfaces.Unbind) => {
+  bind(TYPES.Initializable).toDynamicValue((context) => {
+    const initializables = [
+      context.container.get(TYPES.BoundWitnessStatsDiviner),
+      context.container.get(TYPES.ElevationDiviner),
+      context.container.get(TYPES.ModuleAddressDiviner),
+      context.container.get(TYPES.PayloadStatsDiviner),
+      context.container.get(TYPES.SchemaStatsDiviner),
+    ]
+    return initializables.filter(exists)
+  })
+})
