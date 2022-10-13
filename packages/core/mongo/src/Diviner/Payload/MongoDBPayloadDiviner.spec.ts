70ba94bed105f9d97bc0ee60984114e70b7c1ebf
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-13 13:01:35 -0500

    Add Payload Diviner

---
+++ packages/core/mongo/src/Diviner/Payload/MongoDBPayloadDiviner.spec.ts
@@ -0,0 +1,39 @@
+import { XyoAccount } from '@xyo-network/account'
+import {
+  BoundWitnessQueryPayload,
+  BoundWitnessQuerySchema,
+  XyoBoundWitnessWithMeta,
+  XyoBoundWitnessWithPartialMeta,
+} from '@xyo-network/archivist-model'
+import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
+import { Logger } from '@xyo-network/shared'
+import { mock, MockProxy } from 'jest-mock-extended'
+
+import { COLLECTIONS } from '../../collections'
+import { getBaseMongoSdk } from '../../Mongo'
+import { MongoDBPayloadDiviner } from './MongoDBPayloadDiviner'
+
+describe('MongoDBPayloadDiviner', () => {
+  let logger: MockProxy<Logger>
+  let account: XyoAccount
+  let sdk: BaseMongoSdk<XyoBoundWitnessWithMeta>
+  let sut: MongoDBPayloadDiviner
+  beforeEach(() => {
+    logger = mock<Logger>()
+    account = XyoAccount.random()
+    sdk = getBaseMongoSdk<XyoBoundWitnessWithMeta>(COLLECTIONS.BoundWitnesses)
+    sut = new MongoDBPayloadDiviner(logger, account, sdk)
+  })
+  describe('divine', () => {
+    describe('with valid query', () => {
+      it('divines', async () => {
+        const query: BoundWitnessQueryPayload = { limit: 1, schema: BoundWitnessQuerySchema }
+        const result = await sut.divine([query])
+        expect(result).toBeArrayOfSize(1)
+        const actual = result[0] as XyoBoundWitnessWithPartialMeta
+        expect(actual).toBeObject()
+        expect(actual.schema).toBe('network.xyo.boundwitness')
+      })
+    })
+  })
+})
