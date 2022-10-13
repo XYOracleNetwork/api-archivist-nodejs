6f03b063658987e993f66d4e2885c1df4f123f61
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-13 08:05:28 -0500

    Notes on refactoring

--- packages/core/mongo/src/Job/getJobs.ts
+++ packages/core/mongo/src/Job/getJobs.ts
@@ -3,6 +3,8 @@ import { TYPES } from '@xyo-network/archivist-types'
 import { Job, JobProvider } from '@xyo-network/shared'
 import { Container } from 'inversify'
 
+// TODO: This is no longer Mongo-specific and can be
+// exposed in a non-DB module
 export const getJobs = (container: Container): Job[] => {
   return container
     .getAll<JobProvider>(TYPES.JobProvider)
