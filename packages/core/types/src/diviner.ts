ec56b12c18cc706eb0909b1ee94196a500c66316
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-13 06:47:51 -0500

    Add DI symbols for diviner

--- packages/core/types/src/diviner.ts
+++ packages/core/types/src/diviner.ts
@@ -1,4 +1,5 @@
 export const DIVINER_TYPES = {
+  BoundWitnessDiviner: Symbol('BoundWitnessDiviner'),
   BoundWitnessStatsDiviner: Symbol('BoundWitnessStatsDiviner'),
   ElevationDiviner: Symbol('ElevationDiviner'),
   ModuleAddressDiviner: Symbol('ModuleAddressDiviner'),
