846e8376ebdbb1226e57f7f7ca4c8d63b325d3bf
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-13 13:09:31 -0500

    Add PayloadDiviner types

--- packages/core/mongo/src/Diviner/Payload/MongoDBPayloadDiviner.ts
+++ packages/core/mongo/src/Diviner/Payload/MongoDBPayloadDiviner.ts
@@ -1,11 +1,10 @@
 import 'reflect-metadata'
 
 import { XyoAccount } from '@xyo-network/account'
-import { BoundWitnessDiviner, BoundWitnessQueryPayload, isBoundWitnessQueryPayload, XyoBoundWitnessWithMeta } from '@xyo-network/archivist-model'
+import { isPayloadQueryPayload, PayloadDiviner, PayloadQueryPayload, XyoPayloadWithMeta } from '@xyo-network/archivist-model'
 import { TYPES } from '@xyo-network/archivist-types'
-import { XyoBoundWitness } from '@xyo-network/boundwitness'
 import { XyoArchivistPayloadDivinerConfigSchema, XyoDiviner } from '@xyo-network/diviner'
-import { XyoPayloads } from '@xyo-network/payload'
+import { XyoPayload, XyoPayloads } from '@xyo-network/payload'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { Job, JobProvider, Logger } from '@xyo-network/shared'
 import { inject, injectable } from 'inversify'
@@ -16,11 +15,11 @@ import { removeId } from '../../Mongo'
 import { MONGO_TYPES } from '../../types'
 
 @injectable()
-export class MongoDBPayloadDiviner extends XyoDiviner implements BoundWitnessDiviner, JobProvider {
+export class MongoDBPayloadDiviner extends XyoDiviner implements PayloadDiviner, JobProvider {
   constructor(
     @inject(TYPES.Logger) protected logger: Logger,
     @inject(TYPES.Account) account: XyoAccount,
-    @inject(MONGO_TYPES.BoundWitnessSdkMongo) protected readonly sdk: BaseMongoSdk<XyoBoundWitnessWithMeta>,
+    @inject(MONGO_TYPES.PayloadSdkMongo) protected readonly sdk: BaseMongoSdk<XyoPayloadWithMeta>,
   ) {
     super({ schema: XyoArchivistPayloadDivinerConfigSchema }, account)
   }
@@ -35,28 +34,15 @@ export class MongoDBPayloadDiviner extends XyoDiviner implements BoundWitnessDiv
     ]
   }
 
-  override async divine(payloads?: XyoPayloads): Promise<XyoPayloads<XyoBoundWitness>> {
-    const query = payloads?.find<BoundWitnessQueryPayload>(isBoundWitnessQueryPayload)
+  override async divine(payloads?: XyoPayloads): Promise<XyoPayloads<XyoPayload>> {
+    const query = payloads?.find<PayloadQueryPayload>(isPayloadQueryPayload)
     // TODO: Support multiple queries
     if (!query) return []
-    const {
-      archive,
-      archives,
-      addresses,
-      hash,
-      limit,
-      order,
-      payload_hashes,
-      payload_schemas,
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      schema,
-      timestamp,
-      ...props
-    } = query
+    const { archive, archives, hash, limit, order, schema, timestamp, ...props } = query
     const parsedLimit = limit || DefaultLimit
     const parsedOrder = order || DefaultOrder
     const sort: { [key: string]: SortDirection } = { _timestamp: parsedOrder === 'asc' ? 1 : -1 }
-    const filter: Filter<XyoBoundWitnessWithMeta> = { ...props }
+    const filter: Filter<XyoPayloadWithMeta> = { ...props }
     if (timestamp) {
       const parsedTimestamp = timestamp ? timestamp : parsedOrder === 'desc' ? Date.now() : 0
       filter._timestamp = parsedOrder === 'desc' ? { $lt: parsedTimestamp } : { $gt: parsedTimestamp }
@@ -64,13 +50,7 @@ export class MongoDBPayloadDiviner extends XyoDiviner implements BoundWitnessDiv
     if (archive) filter._archive = archive
     if (archives?.length) filter._archive = { $in: archives }
     if (hash) filter._hash = hash
-    // NOTE: Defaulting to $all since it makes the most sense when singing addresses are supplied
-    // but based on how MongoDB implements multi-key indexes $in might be much faster and we could
-    // solve the multi-sig problem via multiple API calls when multi-sig is desired instead of
-    // potentially impacting performance for all single-address queries
-    if (addresses?.length) filter.addresses = { $all: addresses }
-    if (payload_hashes?.length) filter.payload_hashes = { $in: payload_hashes }
-    if (payload_schemas?.length) filter.payload_schemas = { $in: payload_schemas }
+    if (schema) filter.schema = schema
     return (await (await this.sdk.find(filter)).sort(sort).limit(parsedLimit).maxTimeMS(DefaultMaxTimeMS).toArray()).map(removeId)
   }
 
