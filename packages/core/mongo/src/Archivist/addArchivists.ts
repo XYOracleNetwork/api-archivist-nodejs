c6484c872dd1825a2bb88f79d9ebbe0fe7273c39
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-13 14:50:51 -0500

    Remove obviated types

--- packages/core/mongo/src/Archivist/addArchivists.ts
+++ packages/core/mongo/src/Archivist/addArchivists.ts
@@ -7,9 +7,8 @@ import {
   WitnessedPayloadArchivist,
 } from '@xyo-network/archivist-model'
 import { TYPES } from '@xyo-network/archivist-types'
-import { Container, ContainerModule, interfaces } from 'inversify'
+import { ContainerModule, interfaces } from 'inversify'
 
-import { MONGO_TYPES } from '../types'
 import { MongoDBArchiveArchivist } from './Archive'
 import { MongoDBArchiveKeyArchivist } from './ArchiveKey'
 import { MongoDBBoundWitnessArchivist } from './BoundWitness'
@@ -17,26 +16,22 @@ import { MongoDBPayloadArchivist } from './Payload'
 import { MongoDBUserArchivist } from './User'
 import { MongoDBArchivistWitnessedPayloadArchivist } from './WitnessedPayload'
 
-export const addArchivists = (container: Container) => {
-  container.bind<ArchiveArchivist>(TYPES.ArchiveArchivist).to(MongoDBArchiveArchivist).inSingletonScope()
-  container.bind<ArchiveKeyArchivist>(TYPES.ArchiveKeyArchivist).to(MongoDBArchiveKeyArchivist).inSingletonScope()
-  container.bind<BoundWitnessesArchivist>(TYPES.BoundWitnessArchivist).to(MongoDBBoundWitnessArchivist).inSingletonScope()
-  container.bind<PayloadArchivist>(TYPES.PayloadArchivist).to(MongoDBPayloadArchivist).inSingletonScope()
-  container.bind<UserArchivist>(TYPES.UserArchivist).to(MongoDBUserArchivist).inSingletonScope()
-  container.bind<WitnessedPayloadArchivist>(TYPES.WitnessedPayloadArchivist).to(MongoDBArchivistWitnessedPayloadArchivist).inSingletonScope()
-}
-
 export const archivists = new ContainerModule((bind: interfaces.Bind, _unbind: interfaces.Unbind) => {
-  bind(MongoDBUserArchivist).to(MongoDBUserArchivist).inSingletonScope()
-  bind<MongoDBUserArchivist>(MONGO_TYPES.UserArchivistMongoDb).toService(MongoDBUserArchivist)
-  bind<UserArchivist>(TYPES.UserArchivist).toService(MongoDBUserArchivist)
-
   bind(MongoDBArchiveArchivist).to(MongoDBArchiveArchivist).inSingletonScope()
-  bind<MongoDBArchiveArchivist>(MONGO_TYPES.ArchiveArchivistMongoDb).toService(MongoDBArchiveArchivist)
   bind<ArchiveArchivist>(TYPES.ArchiveArchivist).toService(MongoDBArchiveArchivist)
 
-  bind<ArchiveKeyArchivist>(TYPES.ArchiveKeyArchivist).to(MongoDBArchiveKeyArchivist).inSingletonScope()
-  bind<BoundWitnessesArchivist>(TYPES.BoundWitnessArchivist).to(MongoDBBoundWitnessArchivist).inSingletonScope()
-  bind<PayloadArchivist>(TYPES.PayloadArchivist).to(MongoDBPayloadArchivist).inSingletonScope()
-  bind<WitnessedPayloadArchivist>(TYPES.WitnessedPayloadArchivist).to(MongoDBArchivistWitnessedPayloadArchivist).inSingletonScope()
+  bind(MongoDBArchiveKeyArchivist).to(MongoDBArchiveKeyArchivist).inSingletonScope()
+  bind<ArchiveKeyArchivist>(TYPES.ArchiveKeyArchivist).toService(MongoDBArchiveKeyArchivist)
+
+  bind(MongoDBBoundWitnessArchivist).to(MongoDBBoundWitnessArchivist).inSingletonScope()
+  bind<BoundWitnessesArchivist>(TYPES.BoundWitnessArchivist).toService(MongoDBBoundWitnessArchivist)
+
+  bind(MongoDBPayloadArchivist).to(MongoDBPayloadArchivist).inSingletonScope()
+  bind<PayloadArchivist>(TYPES.PayloadArchivist).toService(MongoDBPayloadArchivist)
+
+  bind(MongoDBUserArchivist).to(MongoDBUserArchivist).inSingletonScope()
+  bind<UserArchivist>(TYPES.UserArchivist).toService(MongoDBUserArchivist)
+
+  bind(MongoDBArchivistWitnessedPayloadArchivist).to(MongoDBArchivistWitnessedPayloadArchivist).inSingletonScope()
+  bind<WitnessedPayloadArchivist>(TYPES.WitnessedPayloadArchivist).toService(MongoDBArchivistWitnessedPayloadArchivist)
 })
--- packages/core/mongo/src/types.ts
+++ packages/core/mongo/src/types.ts
@@ -1,10 +1,8 @@
 export const MONGO_TYPES = {
-  ArchiveArchivistMongoDb: Symbol('ArchiveArchivistMongoDb'),
   ArchiveKeySdkMongo: Symbol('ArchiveKeySdkMongo'),
   ArchiveSdkMongo: Symbol('ArchiveSdkMongo'),
   BoundWitnessSdkMongo: Symbol('BoundWitnessSdkMongo'),
   PayloadSdkMongo: Symbol('PayloadSdkMongo'),
-  UserArchivistMongoDb: Symbol('UserArchivistMongoDb'),
   UserManagerMongoDb: Symbol('UserManagerMongoDb'),
   UserSdkMongo: Symbol('UserSdkMongo'),
   XyoArchiveKeySdkMongo: Symbol('XyoArchiveKeySdkMongo'),
