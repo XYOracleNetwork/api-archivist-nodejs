210362ebe9522c83e43126dd04ef63805114eb29
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-13 17:40:20 -0500

    Export ArchivistFactoryContainerModule

--- packages/core/mongo/src/Archivist/index.ts
+++ packages/core/mongo/src/Archivist/index.ts
@@ -1,4 +1,4 @@
 export * from './AbstractArchivist'
-export * from './addArchivistFactories'
 export * from './ArchivistContainerModule'
+export * from './ArchivistFactoryContainerModule'
 export * from './User'
