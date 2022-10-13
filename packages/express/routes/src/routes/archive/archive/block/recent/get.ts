5f5f7398129121382fe78126f8802e4f1b648a82
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-13 11:10:42 -0500

    Use diviner for BW latest

--- packages/express/routes/src/routes/archive/archive/block/recent/get.ts
+++ packages/express/routes/src/routes/archive/archive/block/recent/get.ts
@@ -2,26 +2,19 @@ import 'source-map-support/register'
 
 import { assertEx } from '@xylabs/assert'
 import { asyncHandler, tryParseInt } from '@xylabs/sdk-api-express-ecs'
-import { XyoArchivistWrapper } from '@xyo-network/archivist'
-import { ArchiveBoundWitnessArchivist, XyoBoundWitnessFilterPredicate } from '@xyo-network/archivist-model'
+import { BoundWitnessQueryPayload, BoundWitnessQuerySchema } from '@xyo-network/archivist-model'
 import { XyoBoundWitness } from '@xyo-network/boundwitness'
 import { RequestHandler } from 'express'
 
 import { BlockRecentPathParams } from './BlockRecentPathParams'
 
-const getBoundWitnesses = async (archivist: ArchiveBoundWitnessArchivist, limit: number) => {
-  const filter: XyoBoundWitnessFilterPredicate = { limit }
-
-  const wrapper = new XyoArchivistWrapper(archivist)
-  return await wrapper.find(filter)
-}
-
 const handler: RequestHandler<BlockRecentPathParams, (XyoBoundWitness | null)[]> = async (req, res) => {
   const { archive, limit } = req.params
-  const { archiveBoundWitnessArchivistFactory } = req.app
+  const { boundWitnessDiviner } = req.app
   const limitNumber = tryParseInt(limit) ?? 20
   assertEx(limitNumber > 0 && limitNumber <= 100, 'limit must be between 1 and 100')
-  const boundWitnesses = (await getBoundWitnesses(archiveBoundWitnessArchivistFactory(archive), limitNumber)) as (XyoBoundWitness | null)[]
+  const query: BoundWitnessQueryPayload = { archive, limit: limitNumber, schema: BoundWitnessQuerySchema }
+  const boundWitnesses = (await boundWitnessDiviner.divine([query])) as (XyoBoundWitness | null)[]
   res.json(boundWitnesses)
 }
 
