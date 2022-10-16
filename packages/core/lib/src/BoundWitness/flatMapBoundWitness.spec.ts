768249f0b204732dba287d03a869d65f655b3772
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-16 15:51:23 -0500

    Handle nested Bound Witnesses

--- packages/core/lib/src/BoundWitness/flatMapBoundWitness.spec.ts
+++ packages/core/lib/src/BoundWitness/flatMapBoundWitness.spec.ts
@@ -7,15 +7,22 @@ import { flatMapBoundWitness } from './flatMapBoundWitness'
 const config: BoundWitnessBuilderConfig = { inlinePayloads: true }
 
 describe('flatMapBoundWitness', () => {
-  const payload1: XyoPayload = new XyoPayloadBuilder<DebugPayload>({ schema: DebugSchema }).fields({ nonce: '1' }).build()
-  const payload2: XyoPayload = new XyoPayloadBuilder<DebugPayload>({ schema: DebugSchema }).fields({ nonce: '2' }).build()
-  it('with nested BoundWitnesses', () => {
+  describe('with nested BoundWitnesses', () => {
+    const payload1: XyoPayload = new XyoPayloadBuilder<DebugPayload>({ schema: DebugSchema }).fields({ nonce: '1' }).build()
+    const payload2: XyoPayload = new XyoPayloadBuilder<DebugPayload>({ schema: DebugSchema }).fields({ nonce: '2' }).build()
     const inner = new BoundWitnessBuilder(config).payload(payload2).build()
     const outer = new BoundWitnessBuilder(config).payloads([payload1, inner[0]]).build()
     const result = flatMapBoundWitness(outer[0])
-    expect(result).toBeObject()
-    expect(result[0]).toContain(outer)
-    expect(result[1]).toContain(payload1)
-    expect(result[1]).toContain(payload2)
+    it('extracts the bound witnesses', () => {
+      expect(result).toBeArray()
+      expect(result[0]).toBeArray()
+      expect(result[0].length).toBe(2)
+      expect(result[0]).toContainValues([outer[0], inner[0]])
+    })
+    it('extracts the payloads', () => {
+      expect(result[1]).toBeArray()
+      expect(result[1].length).toBe(2)
+      expect(result[1]).toContainValues([payload1, payload2])
+    })
   })
 })
