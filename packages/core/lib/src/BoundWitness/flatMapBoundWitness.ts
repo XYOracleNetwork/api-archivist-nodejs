cd30aee9b709a2446940ca998634fe02e0772605
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-16 15:51:23 -0500

    Handle nested Bound Witnesses

--- packages/core/lib/src/BoundWitness/flatMapBoundWitness.ts
+++ packages/core/lib/src/BoundWitness/flatMapBoundWitness.ts
@@ -5,10 +5,18 @@ import { XyoPayload } from '@xyo-network/payload'
 export type BoundWitnessMapResult = [Array<XyoBoundWitness>, Array<XyoPayload>]
 
 export const flatMapBoundWitness = (boundWitness: XyoBoundWitnessWithPartialMeta): BoundWitnessMapResult => {
-  const boundWitnesses: XyoBoundWitness[] = []
-  const payloads: XyoPayload[] = []
-  boundWitness._payloads?.map((payload) => {
-    payloads.push(payload)
-  })
+  const all = flatten(boundWitness)
+  const boundWitnesses: XyoBoundWitness[] = all.filter((x) => x.schema === 'network.xyo.boundwitness') as XyoBoundWitness[]
+  const payloads: XyoPayload[] = all.filter((x) => x.schema !== 'network.xyo.boundwitness')
   return [boundWitnesses, payloads]
+}
+
+const flatten = (boundWitness: XyoBoundWitnessWithPartialMeta): XyoPayload[] => {
+  const payloads: XyoPayload[] =
+    boundWitness?._payloads
+      ?.map((payload) => {
+        return payload.schema === 'network.xyo.boundwitness' ? flatten(payload as XyoBoundWitnessWithPartialMeta) : payload
+      })
+      .flatMap((x) => x) || []
+  return ([boundWitness] as XyoPayload[]).concat(payloads)
 }
