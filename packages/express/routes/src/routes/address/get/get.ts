eebccca113e9b975025c0382eaeb81e2682a909c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-17 17:37:43 -0500

    Normalize address (trim prefix, lowercase)

--- packages/express/routes/src/routes/address/get/get.ts
+++ packages/express/routes/src/routes/address/get/get.ts
@@ -1,5 +1,5 @@
 // import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
-import { getModuleDescription } from '@xyo-network/archivist-lib'
+import { getModuleDescription, trimAddressPrefix } from '@xyo-network/archivist-lib'
 import { ModuleDescription } from '@xyo-network/archivist-model'
 import { Module } from '@xyo-network/module'
 import { Request, RequestHandler } from 'express'
@@ -24,8 +24,8 @@ const handler: RequestHandler<AddressPathParams, ModuleDescription> = (req, res,
   if (!populated) populateStaticModules(req)
   const { address } = req.params
   if (address) {
-    // TODO: ToLower and remove hex prefix
-    const mod = activeModules[address]
+    const normalizedAddress = trimAddressPrefix(address).toLowerCase()
+    const mod = activeModules[normalizedAddress]
     if (mod) {
       res.json(getModuleDescription(mod))
       return
