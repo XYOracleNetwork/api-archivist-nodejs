b0090477a50620b98df754a88ef2940cb1521bd4
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-17 13:30:56 -0500

    Move transport to Memory Module

--- packages/core/module/src/Memory/Queue/InMemoryQueue.ts
+++ packages/core/module/src/Memory/Queue/InMemoryQueue.ts
@@ -2,7 +2,7 @@ import { Identifiable } from '@xyo-network/archivist-model'
 import { Queue } from '@xyo-network/archivist-model/src/Queue'
 import { Transport } from '@xyo-network/archivist-model/src/Transport/Transport'
 
-import { InMemoryTransport } from './InMemoryTransport'
+import { InMemoryTransport } from '../Transport/InMemoryTransport'
 
 export class InMemoryQueue<T extends Identifiable> extends Queue<T> {
   constructor(protected transport: Transport<T> = new InMemoryTransport<T>()) {
