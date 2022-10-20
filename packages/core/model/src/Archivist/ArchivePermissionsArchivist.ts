f20d38f54d2bf32e75f7e5cfb38b5c425ff5971c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-20 12:29:13 -0500

    Implement XyoModule over Module

--- packages/core/model/src/Archivist/ArchivePermissionsArchivist.ts
+++ packages/core/model/src/Archivist/ArchivePermissionsArchivist.ts
@@ -1,11 +1,11 @@
 import { Archivist } from '@xyo-network/archivist'
 import { XyoBoundWitness } from '@xyo-network/boundwitness'
-import { Module } from '@xyo-network/module'
+import { XyoModule } from '@xyo-network/module'
 
 import { XyoPayloadWithPartialMeta } from '../Payload'
 import { SetArchivePermissions } from '../Query'
 
 export type ArchivePermissionsArchivist = Archivist<SetArchivePermissions, XyoBoundWitness | null, XyoPayloadWithPartialMeta<SetArchivePermissions>> &
-  Module
+  XyoModule
 
 export type ArchivePermissionsArchivistFactory = (archive: string) => ArchivePermissionsArchivist
