1170483c4eb664cead670fd6b4ee20b062db0d8a
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-08-02 17:43:41 -0500

    Ignore coverage directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import { config as xylabsConfig } from '@xylabs/eslint-config-flat'
 
 export default [
   {
-    ignores: ['.yarn/**', 'jest.config.cjs', '**/dist/**', 'dist', 'build/**', 'node_modules/**'],
+    ignores: ['.yarn/**', 'jest.config.cjs', '**/dist/**', 'dist', 'build/**', 'coverage', 'node_modules/**'],
   },
   ...xylabsConfig,
   {
